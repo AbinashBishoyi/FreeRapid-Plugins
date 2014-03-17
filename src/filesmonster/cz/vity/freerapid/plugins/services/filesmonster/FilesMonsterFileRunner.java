a0eda0f8217a3fb904c358f92634bd98b3525f09
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 11:39:35 +0530

    SVN Update# 3080 by Stan: Filesmonster.com - new version of plugin

--- src/filesmonster/cz/vity/freerapid/plugins/services/filesmonster/FilesMonsterFileRunner.java
+++ src/filesmonster/cz/vity/freerapid/plugins/services/filesmonster/FilesMonsterFileRunner.java
@@ -1,5 +1,6 @@
 package cz.vity.freerapid.plugins.services.filesmonster;
 
+import cz.vity.freerapid.plugins.container.FileInfo;
 import cz.vity.freerapid.plugins.exceptions.*;
 import cz.vity.freerapid.plugins.services.recaptcha.ReCaptcha;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
@@ -9,6 +10,11 @@ import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -16,10 +22,15 @@ import java.util.regex.Matcher;
 /**
  * Class which contains main code
  *
- * @author Lukiz, ntoskrnl
+ * @author Lukiz
+ * @author ntoskrnl
+ * @author Stan
  */
 class FilesMonsterFileRunner extends AbstractRunner {
     private final static Logger logger = Logger.getLogger(FilesMonsterFileRunner.class.getName());
+
+    private static final String AJAX_HEADER_FIELD = "X-Requested-With";
+    private static final String AJAX_HEADER_VALUE = "XMLHttpRequest";
 
     @Override
     public void runCheck() throws Exception { //this method validates file
@@ -29,17 +40,20 @@ class FilesMonsterFileRunner extends AbstractRunner {
             checkProblems();
             checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
         } else {
-            checkProblems();
-            throw new ServiceConnectionProblemException();
+            checkContentOrConnectionProblems();
         }
     }
 
     private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
-        PlugUtils.checkName(httpFile, content, "File name: <span class=\"em\">", "</span>");
-        PlugUtils.checkFileSize(httpFile, content, "File size: <span class=\"em\">", "</span>");
+        boolean partOfFile = fileURL.contains("free/2");
+        if (!partOfFile) {
+            PlugUtils.checkName(httpFile, content, "File name:</td>\n<td>", "</td>");
+            PlugUtils.checkFileSize(httpFile, content, "File size:</td>\n<td>", "</td>");
+        }
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
-        if (!content.contains("slowdownload"))
+        if (!content.contains("slowdownload") && !partOfFile) {
             throw new NotRecoverableDownloadException("Only Premium download available");
+        }
     }
 
     @Override
@@ -47,61 +61,107 @@ class FilesMonsterFileRunner extends AbstractRunner {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
         final GetMethod method = getGetMethod(fileURL); //create GET request
-        if (makeRedirectedRequest(method)) { //we make the main request
-            final String contentAsString = getContentAsString();//check for response
-            checkProblems();//check problems
-            checkNameAndSize(contentAsString);//extract file name and size from the page
-
-            final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setActionFromFormByName("slowdownload", true).toPostMethod();
-            final String refer = "http://filesmonster.com" + httpMethod.getPath();
-            if (!makeRedirectedRequest(httpMethod)) {
-                checkProblems();
-                throw new ServiceConnectionProblemException();
-            }
-
-            while (getContentAsString().contains("recaptcha")) {
-                if (!makeRedirectedRequest(stepCaptcha(refer))) {
-                    checkProblems();
-                    throw new ServiceConnectionProblemException();
-                }
-            }
-
-            downloadTask.sleep(PlugUtils.getNumberBetween(getContentAsString(), "var timeout='", "';") + 1);
-
-            final HttpMethod ticketMethod = getMethodBuilder().setReferer(refer).setActionFromFormByName("rtForm", true).setAction("http://filesmonster.com/ajax.php").toHttpMethod();
-            ticketMethod.addRequestHeader("X-Requested-With", "XMLHttpRequest");//use AJAX
-            if (!makeRedirectedRequest(ticketMethod)) {
-                checkProblems();
-                throw new ServiceConnectionProblemException();
-            }
-            checkJSONProblems(getContentAsString());
-
-            String data = PlugUtils.getStringBetween(getContentAsString(), "\"text\":\"", "\",\"");
-            final HttpMethod getUrlMethod = getMethodBuilder().setReferer(refer).setAction("http://filesmonster.com/ajax.php").setParameter("act", "getdl").setParameter("data", data).toPostMethod();
-            getUrlMethod.addRequestHeader("X-Requested-With", "XMLHttpRequest");//use AJAX
-            if (!makeRedirectedRequest(getUrlMethod)) {
-                checkProblems();
-                throw new ServiceConnectionProblemException();
-            }
-            checkJSONProblems(getContentAsString());
-
-            String finalUrl = PlugUtils.getStringBetween(getContentAsString(), "\"url\":\"", "\",\"file_request");
-            String request = PlugUtils.getStringBetween(getContentAsString(), "file_request\":\"", "\",\"err");
-            finalUrl = finalUrl.replace("\\", "");
-
-            final HttpMethod finalMethod = getMethodBuilder().setReferer(refer).setAction(finalUrl).setParameter("X-File-Request", request).toPostMethod();
-            finalMethod.addRequestHeader("X-Requested-With", "XMLHttpRequest");//use AJAX
-            //here is the download link extraction
-            if (!tryDownloadAndSaveFile(finalMethod)) {
-                checkJSONProblems(getContentAsString());
-                checkProblems();//if downloading failed
-                logger.warning(getContentAsString());//log the info
-                throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
-            }
-        } else {
-            checkProblems();
-            throw new ServiceConnectionProblemException();
+        if (!makeRedirectedRequest(method)) { //we make the main request
+            checkContentOrConnectionProblems();
         }
+        if (!fileURL.contains("free/2")) { // main file
+            processMainFile();
+        } else { // part of main file
+            processPartOfFile();
+        }
+    }
+
+    private void processMainFile() throws ErrorDuringDownloadingException, IOException {
+        final String contentAsString = getContentAsString();//check for response
+        checkProblems();//check problems
+        checkNameAndSize(contentAsString);//extract file name and size from the page
+
+        final HttpMethod postMethodForTicket = getMethodBuilder()
+                .setReferer(fileURL).setActionFromFormByName("slowdownload", true).toPostMethod();
+        if (!makeRedirectedRequest(postMethodForTicket)) {
+            checkContentOrConnectionProblems();
+        }
+        String reserveTicket = PlugUtils.getStringBetween(getContentAsString(),
+                "reserve_ticket('", "'),");
+
+        final String referer = "http://filesmonster.com" + postMethodForTicket.getPath();
+        final String dlPrefix = postMethodForTicket.getPath()
+                .substring(0, postMethodForTicket.getPath().indexOf("free/") + "free".length())
+                + "/";
+
+        final HttpMethod getMethodForFiles = getMethodBuilder()
+                .setAction(reserveTicket).setReferer(referer).toGetMethod();
+        getMethodForFiles.addRequestHeader(AJAX_HEADER_FIELD, AJAX_HEADER_VALUE); // send as AJAX
+        if (!makeRequest(getMethodForFiles)) {
+            checkContentOrConnectionProblems();
+        }
+        List<FileInfo> files = getFilesFromJson(
+                getContentAsString(), "http://filesmonster.com" + dlPrefix, referer);
+        getPluginService().getPluginContext().getQueueSupport()
+                .addLinksToQueueFromContainer(httpFile, files);
+    }
+
+    private void processPartOfFile() throws Exception {
+        final HttpMethod getMethodForCaptcha = getMethodBuilder()
+                .setAction(fileURL).setReferer(httpFile.getDescription()).toGetMethod();
+        getMethodForCaptcha.addRequestHeader(AJAX_HEADER_FIELD, AJAX_HEADER_VALUE); // send as AJAX
+        if (!makeRedirectedRequest(getMethodForCaptcha)) {
+            checkContentOrConnectionProblems();
+        }
+        while (getContentAsString().contains("recaptcha")) {
+            if (!makeRedirectedRequest(stepCaptcha(fileURL))) {
+                checkContentOrConnectionProblems();
+            }
+        }
+        if (!getContentAsString().contains("get_link('")) {
+            checkContentOrConnectionProblems();
+        }
+
+        final HttpMethod getMethodForLink = getMethodBuilder()
+                .setAction(PlugUtils.getStringBetween(getContentAsString(), "get_link('", "');"))
+                .setReferer(fileURL).toGetMethod();
+        getMethodForLink.addRequestHeader(AJAX_HEADER_FIELD, AJAX_HEADER_VALUE); // send as AJAX
+        if (!makeRequest(getMethodForLink)) checkContentOrConnectionProblems();
+        if (!getContentAsString().contains("url\":\"")) checkJSONProblems(getContentAsString());
+        String finalLink = PlugUtils.getStringBetween(getContentAsString(),
+                "url\":\"", "\",").replace("\\", "");
+        String fileRequest = PlugUtils.getStringBetween(getContentAsString(),
+                "file_request\":\"", "\"");
+
+        final HttpMethod postMethodForDownload = getMethodBuilder()
+                .setAction(finalLink).setReferer(fileURL)
+                .setParameter("X-File-Request", fileRequest)
+                .setParameter("x", "688").setParameter("y", "258").toPostMethod(); // mouse position when clicked to download button - not necessary but what if...
+        postMethodForDownload.addRequestHeader(AJAX_HEADER_FIELD, AJAX_HEADER_VALUE); // send as AJAX
+        if (!tryDownloadAndSaveFile(postMethodForDownload)) {
+            checkJSONProblems(getContentAsString());
+            checkProblems();//if downloading failed
+            logger.warning(getContentAsString());//log the info
+            throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+        }
+    }
+
+    private List<FileInfo> getFilesFromJson(String json, String urlPrefix, String referer) throws MalformedURLException {
+        List<FileInfo> files = new ArrayList<FileInfo>();
+        for (int i = 1; true; i++) {
+            try {
+                String fileString = PlugUtils.getStringBetween(json, "{", "}", i);
+                FileInfo fileInfo = new FileInfo(new URL(urlPrefix + 2 + "/" +
+                        PlugUtils.getStringBetween(fileString, "dlcode\":\"", "\"")));
+                fileInfo.setFileName(PlugUtils.getStringBetween(fileString, "name\":\"", "\""));
+                fileInfo.setFileSize(PlugUtils.getNumberBetween(fileString, "size\":", ","));
+                fileInfo.setDescription(referer);
+                files.add(fileInfo);
+            } catch (PluginImplementationException e) {
+                break;
+            }
+        }
+        return files;
+    }
+
+    private void checkContentOrConnectionProblems() throws ErrorDuringDownloadingException {
+        checkProblems();
+        throw new ServiceConnectionProblemException();
     }
 
     private void checkProblems() throws ErrorDuringDownloadingException {
@@ -115,6 +175,12 @@ class FilesMonsterFileRunner extends AbstractRunner {
 
         if (contentAsString.contains("There are no free download slots available"))
             throw new ServiceConnectionProblemException("No more free download slots");
+
+        if (contentAsString.contains("avaliable for free download in")) {
+            int time = PlugUtils.getWaitTimeBetween(contentAsString,
+                    "avaliable for free download in", "min", TimeUnit.MINUTES);
+            throw new YouHaveToWaitException("Waiting for next file", time);
+        }
     }
 
     private void checkJSONProblems(final String content) throws ErrorDuringDownloadingException {
@@ -124,14 +190,23 @@ class FilesMonsterFileRunner extends AbstractRunner {
         } catch (PluginImplementationException e) {
             return;//no error
         }
-        if (error == null || error.isEmpty()) return;
-
-        if (error.contains("404"))
+        if (error == null || error.isEmpty()) {
+            return;
+        }
+        if (error.contains("404")) {
             throw new ServiceConnectionProblemException("AJAX server response: There is no such file");
-        if (error.contains("500"))
+        }
+        if (error.contains("500")) {
             throw new ServiceConnectionProblemException("AJAX server response: Internal error");
-        //else
-        throw new ServiceConnectionProblemException("AJAX server response: No free download slots available");
+        }
+        if (error.contains("Your IP address has been changed")) {
+            throw new NotRecoverableDownloadException("Link expired - Start main file download again for new link to this part");
+        }
+        if (error.contains("downloaded file recently")) {
+            throw new NotRecoverableDownloadException("Link probably expired - Try restart main download file for new link to this part");
+        }
+
+        throw new ServiceConnectionProblemException("Unspecified server response");
     }
 
     private HttpMethod stepCaptcha(final String referer) throws Exception {
@@ -150,7 +225,7 @@ class FilesMonsterFileRunner extends AbstractRunner {
         if (captcha == null) throw new CaptchaEntryInputMismatchException();
         r.setRecognized(captcha);
 
-        return r.modifyResponseMethod(getMethodBuilder(content).setReferer(referer).setActionFromFormWhereTagContains("recaptcha", true).setAction("http://filesmonster.com/get/free/")).toPostMethod();
+        return r.modifyResponseMethod(getMethodBuilder(content)
+                .setAction(referer).setReferer(referer)).toPostMethod(); // action is same as referer
     }
-
 }

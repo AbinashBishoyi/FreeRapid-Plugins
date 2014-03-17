15f62ed49b5fd03b51c47ed08c8334ffd98a45ef
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:41:49 +0530

    SVN Update# 3921 by tong2shot: ApunKaBollywood.net - new plugin.

---
+++ src/apunkabollywood/cz/vity/freerapid/plugins/services/apunkabollywood/TestApp.java
@@ -0,0 +1,40 @@
+package cz.vity.freerapid.plugins.services.apunkabollywood;
+
+import cz.vity.freerapid.plugins.dev.PluginDevApplication;
+import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import org.jdesktop.application.Application;
+
+import java.net.URL;
+
+/**
+ * @author tong2shot
+ */
+public class TestApp extends PluginDevApplication {
+    @Override
+    protected void startup() {
+        final HttpFile httpFile = getHttpFile();
+        try {
+            //httpFile.setNewURL(new URL("http://www.apunkabollywood.net/browser/category/view/7532/ek-tha-tiger-(2012)"));
+            //httpFile.setNewURL(new URL("http://www.apunkabollywood.net/browser/download/get/71512/01%20-%20Mashallah%20-%20Wajid%20&%20Shreya%20Ghoshal%20(ApunKaBollywood.com).html"));
+            httpFile.setNewURL(new URL("http://www.apunkabollywood.net/browser/category/view/1476/arth-(1982)"));
+            final ConnectionSettings connectionSettings = new ConnectionSettings();
+            //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
+            final ApunKaBollywoodServiceImpl service = new ApunKaBollywoodServiceImpl();
+            testRun(service, httpFile, connectionSettings);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.exit();
+    }
+
+    /**
+     * Main start method for running this application
+     * Called from IDE
+     *
+     * @param args arguments for application
+     */
+    public static void main(String[] args) {
+        Application.launch(TestApp.class, args);
+    }
+}

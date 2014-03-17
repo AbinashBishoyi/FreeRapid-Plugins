39f5b7ace392f19c603ce40a8f701fa6f8c32530
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:08:41 +0530

    SVN Update# 1385 by Vity: filesend plugin final

---
+++ src/filesend/cz/vity/freerapid/plugins/services/filesend/FileSendServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.filesend;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Vity
+ *         Class that provides basic info about plugin
+ */
+public class FileSendServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "filesend.net";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 8;//i checked that before
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new FileSendFileRunner();
+    }
+
+}

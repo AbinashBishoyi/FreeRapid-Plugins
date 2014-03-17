c6aec3b44d6763182c04a668af473f36ce064682
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:58:40 +0530

    SVN Update# 2411 by ntoskrnl: automatic refactorings f'd up the new plugin templates, fixed

--- dev/newplugin/ServiceImpl.java
+++ dev/newplugin/ServiceImpl.java
@@ -1,6 +1,4 @@
-package cz.vity.freerapid.plugins.services.
-
-#shortsmall#;
+package cz.vity.freerapid.plugins.services.#shortsmall#;
 
 import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
 import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
@@ -9,27 +7,27 @@ import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
  * Class that provides basic info about plugin
  * @author #author#
  */
-public class#fullWithoutDot#ServiceImpl extends AbstractFileShareService{
+public class #fullWithoutDot#ServiceImpl extends AbstractFileShareService {
 
-@Override
-public String getName(){
-        return"#fulllower#";
-}
+    @Override
+    public String getName() {
+        return "#fulllower#";
+    }
 
-@Override
-public int getMaxDownloadsFromOneIP(){
+    @Override
+    public int getMaxDownloadsFromOneIP() {
         //TODO don't forget to update this value, in plugin.xml don't forget to update this value too
         return 1;
-}
+    }
 
-@Override
-public boolean supportsRunCheck(){
+    @Override
+    public boolean supportsRunCheck() {
         return true;//ok
-}
+    }
 
-@Override
-protected PluginRunner getPluginRunnerInstance(){
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
         return new #fullWithoutDot#FileRunner();
-}
+    }
 
-        }
+}

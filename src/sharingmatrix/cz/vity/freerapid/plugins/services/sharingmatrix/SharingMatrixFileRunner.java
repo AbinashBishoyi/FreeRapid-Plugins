546168f434a66e6d1b013edc45653a0324959114
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:57:37 +0530

    SVN Update# 2402 by ntoskrnl: SharingMatrix.com - fixed by codeboy2k

--- src/sharingmatrix/cz/vity/freerapid/plugins/services/sharingmatrix/SharingMatrixFileRunner.java
+++ src/sharingmatrix/cz/vity/freerapid/plugins/services/sharingmatrix/SharingMatrixFileRunner.java
@@ -5,33 +5,33 @@ import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.hoster.CaptchaSupport;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import cz.vity.freerapid.utilities.LogUtils;
 import org.apache.commons.httpclient.HttpMethod;
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 /**
- * @author Kajda, JPEXS, RickCL
+ * @author Kajda, JPEXS, RickCL, codeboy2k
  * @since 0.82
  */
 class SharingMatrixFileRunner extends AbstractRunner {
-
     private final static Logger logger = Logger.getLogger(SharingMatrixFileRunner.class.getName());
 
     @Override
     public void runCheck() throws Exception {
         super.runCheck();
-        final HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
+        final HttpMethod httpMethod = getGetMethod(fileURL);
 
         if (makeRedirectedRequest(httpMethod)) {
-            checkSeriousProblems();
+            checkProblems();
             checkNameAndSize();
         } else {
-            throw new InvalidURLOrServiceProblemException("Invalid URL or service problem");
+            checkProblems();
+            throw new ServiceConnectionProblemException();
         }
     }
 
@@ -39,137 +39,165 @@ class SharingMatrixFileRunner extends AbstractRunner {
     public void run() throws Exception {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
-        HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
+        HttpMethod httpMethod = getGetMethod(fileURL);
 
         if (makeRedirectedRequest(httpMethod)) {
-            checkAllProblems();
+            checkProblems();
             checkNameAndSize();
             String contentAsString = getContentAsString();
             final String rootURL = PlugUtils.getStringBetween(contentAsString, "URL_ROOT = '", "'");
             final String linkId = PlugUtils.getStringBetween(contentAsString, "link_id = '", "'");
             final String linkName = PlugUtils.getStringBetween(contentAsString, "link_name = '", "'");
             String captchaURL = rootURL + "/images/captcha/";
+            boolean waitTimer;
+            String dlID = "";
+            int forcedWaitTime = 0;
 
-            httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/download.php?type_membership=free&link_id=" + linkId).toHttpMethod();
-            if (makeRedirectedRequest(httpMethod)) {
-                int ctjv = PlugUtils.getWaitTimeBetween(getContentAsString(), "ctjv = '", "';", TimeUnit.SECONDS);
-                downloadTask.sleep(ctjv);
-                String dlID = "";
-                try {
-                    captchaURL = PlugUtils.getStringBetween(getContentAsString(), "CAPTCHA_IMAGE_URL = '", "'");
-                } catch(PluginImplementationException e) {
-                    logger.warning(e.getMessage());
-                }
-                if (getContentAsString().contains("_get2.php")) {
-                    dlID = PlugUtils.getStringBetween(getContentAsString(), "showLink(", ", '');");
-                    httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/_get2.php?link_id=" + linkId + "&link_name=" + linkName + "&dl_id=" + dlID + "&password=").toHttpMethod();
-                } else {
-                    boolean refreshCaptcha = false;
-                    InputStream is;
-                    BufferedImage captchaImage = null;
-                    CaptchaSupport captchaSupport = getCaptchaSupport();
-                    if( getContentAsString().contains("check_timer.php") ) {
-                        httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/check_timer.php?tmp=" + (new Random()).nextInt() ).toGetMethod();
-                        if(!makeRequest(httpMethod)) {
-                            throw new PluginImplementationException();
-                        }
-                        //Matcher matcher = getMatcherAgainstContent("img:([^$]*)");
-                        refreshCaptcha = true;
+            do {
+                waitTimer = false;
+                httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/download.php?type_membership=free&link_id=" + linkId).toGetMethod();
+                if (makeRedirectedRequest(httpMethod)) {
+                    // wait time can be 1,5,10,15 or 45 mins (in seconds). After 45 mins, free users must wait a whole day before they can download again
+                    int ctjv = PlugUtils.getWaitTimeBetween(getContentAsString(), "ctjv = '", "';", TimeUnit.SECONDS);
+                    if (forcedWaitTime != 0) {
+                        // when sharing matrix is busy, it tells us to wait 60 seconds, but if we do that, we have to
+                        // refresh the captcha, ask for user to input the captcha, and then it fails again
+                        // and tells us to wait another 60 seconds.  It's annoying to enter the captcha every 60
+                        // seconds for 3 hours or more. This forcedWaitTime recognizes this situation and
+                        // forces a 1 hour wait when the sharingmatrix servers are busy.  Then you only
+                        // have to enter a captcha every hour (servers may still be busy, but it's far less annoying)
+                        downloadTask.sleep(forcedWaitTime);
+                        forcedWaitTime = 0;
                     } else {
-                        httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/include/crypt/cryptographp.php?cfg=0&").toHttpMethod();
-                        httpMethod.setFollowRedirects(true);
-                        is = client.makeRequestForFile(httpMethod);
-
-                        if (is == null) {
-                            throw new PluginImplementationException();
-                        }
-
-                        captchaImage = captchaSupport.loadCaptcha(is);
+                        downloadTask.sleep(ctjv);
                     }
-                    String captchaR = null;
-                    do {
-                        if( refreshCaptcha ) {
-                            httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/reload_captcha.php").toHttpMethod();
-                            if(!makeRequest(httpMethod)) {
+                    try {
+                        captchaURL = PlugUtils.getStringBetween(getContentAsString(), "CAPTCHA_IMAGE_URL = '", "'");
+                    } catch (PluginImplementationException e) {
+                        LogUtils.processException(logger, e);
+                    }
+                    if (getContentAsString().contains("_get2.php")) {
+                        dlID = PlugUtils.getStringBetween(getContentAsString(), "showLink(", ", '');");
+                        httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/_get2.php?link_id=" + linkId + "&link_name=" + linkName + "&dl_id=" + dlID + "&password=").toGetMethod();
+                    } else {
+                        boolean refreshCaptcha = false;
+                        InputStream is;
+                        BufferedImage captchaImage = null;
+                        CaptchaSupport captchaSupport = getCaptchaSupport();
+                        if (getContentAsString().contains("check_timer.php")) {
+                            httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/check_timer.php?tmp=" + (new Random()).nextInt()).toGetMethod();
+                            if (!makeRedirectedRequest(httpMethod)) {
+                                throw new ServiceConnectionProblemException();
+                            }
+                            refreshCaptcha = true;
+                        } else {
+                            httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/include/crypt/cryptographp.php?cfg=0&").toGetMethod();
+                            httpMethod.setFollowRedirects(true);
+                            is = client.makeRequestForFile(httpMethod);
+
+                            if (is == null) {
                                 throw new PluginImplementationException();
                             }
-                            httpMethod = getMethodBuilder().setReferer(fileURL).setAction(captchaURL + getContentAsString().trim()+".jpg").toHttpMethod();
-                            is = client.makeRequestForFile(httpMethod);
-                            if (is == null) {
-                                throw new PluginImplementationException(httpMethod.getURI().toString());
-                            }
+
                             captchaImage = captchaSupport.loadCaptcha(is);
                         }
-                        //System.out.println( getContentAsString() );
-                        captchaR = captchaSupport.askForCaptcha(captchaImage);
-                        if (captchaR == null) {
-                            throw new CaptchaEntryInputMismatchException();
-                        }
+                        String captchaR;
+                        do {
+                            if (refreshCaptcha) {
+                                httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/reload_captcha.php").toGetMethod();
+                                if (!makeRedirectedRequest(httpMethod)) {
+                                    throw new PluginImplementationException();
+                                }
+                                httpMethod = getMethodBuilder().setReferer(fileURL).setAction(captchaURL + getContentAsString().trim() + ".jpg").toGetMethod();
+                                is = client.makeRequestForFile(httpMethod);
+                                if (is == null) {
+                                    throw new PluginImplementationException(httpMethod.getURI().toString());
+                                }
+                                captchaImage = captchaSupport.loadCaptcha(is);
+                            }
+                            captchaR = captchaSupport.askForCaptcha(captchaImage);
+                            if (captchaR == null) {
+                                throw new CaptchaEntryInputMismatchException();
+                            }
 
-                        httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/verifier.php").setParameter("code", captchaR).toPostMethod();
-                        if (!makeRedirectedRequest(httpMethod)) {
-                            throw new ServiceConnectionProblemException();
-                        }
+                            httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/verifier.php").setParameter("code", captchaR).toPostMethod();
+                            if (!makeRedirectedRequest(httpMethod)) {
+                                throw new ServiceConnectionProblemException();
+                            }
 
-                    } while (!getContentAsString().trim().equals("1"));
-                    httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/dl.php").toHttpMethod();
-                    if (makeRedirectedRequest(httpMethod)) {
-                        dlID=getContentAsString();
+                        } while (!getContentAsString().trim().equals("1"));
+                        httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/dl.php").toGetMethod();
+                        if (makeRedirectedRequest(httpMethod)) {
+                            dlID = getContentAsString();
+                        }
+                        httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/_get.php?link_id=" + linkId + "&link_name=" + linkName + "&dl_id=" + dlID + "&password=").toGetMethod();
                     }
-                    httpMethod = getMethodBuilder().setReferer(fileURL).setAction(rootURL + "/ajax_scripts/_get.php?link_id=" + linkId + "&link_name=" + linkName + "&dl_id=" + dlID + "&password=").toHttpMethod();
-                }
-                if (makeRedirectedRequest(httpMethod)) {
-                    contentAsString = getContentAsString();
-                    httpMethod = getMethodBuilder().setReferer(fileURL).setAction("/download/" + PlugUtils.getStringBetween(contentAsString, "hash:\"", "\"") + "/" + dlID + "/").setBaseURL(PlugUtils.getStringBetween(contentAsString, "{serv:\"", "\"")).toHttpMethod();
-
-                    if (!tryDownloadAndSaveFile(httpMethod)) {
-                        checkAllProblems();
-                        logger.warning(getContentAsString());
-                        throw new IOException("File input stream is empty");
+                    if (makeRedirectedRequest(httpMethod)) {
+                        contentAsString = getContentAsString();
+                        if (contentAsString.contains("Wait timer!")) {
+                            waitTimer = true;
+                        }
+                        if (contentAsString.contains("You can not download now") ||
+                                contentAsString.contains("quota has been reached")) {
+                            waitTimer = true;
+                            forcedWaitTime = 3600;
+                        }
+                    } else {
+                        throw new ServiceConnectionProblemException();
                     }
                 } else {
                     throw new ServiceConnectionProblemException();
                 }
-            } else {
-                throw new ServiceConnectionProblemException();
+            } while (waitTimer);
+
+            httpMethod = getMethodBuilder()
+                    .setReferer(fileURL)
+                    .setAction("/download/" + PlugUtils.getStringBetween(contentAsString, "hash:\"", "\"") + "/" + dlID + "/")
+                    .setBaseURL(PlugUtils.getStringBetween(contentAsString, "{serv:\"", "\""))
+                    .toGetMethod();
+
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkProblems();
+                logger.warning(getContentAsString());
+                throw new ServiceConnectionProblemException("Error starting download");
             }
         } else {
-            throw new InvalidURLOrServiceProblemException("Invalid URL or service problem");
+            checkProblems();
+            throw new ServiceConnectionProblemException();
         }
     }
 
-    private void checkSeriousProblems() throws ErrorDuringDownloadingException {
+    private void checkProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
 
         if (contentAsString.contains("File not found")) {
             throw new URLNotAvailableAnymoreException("File not found");
         }
-        if (contentAsString.contains("no available free download slots left for your country")) {
-            throw new URLNotAvailableAnymoreException("We are very sorry, but for this file no available free download slots left for your country today");
+        if (contentAsString.contains("File has been deleted")) {
+            throw new URLNotAvailableAnymoreException("File has been deleted");
         }
-    }
-
-    private void checkAllProblems() throws ErrorDuringDownloadingException {
-        checkSeriousProblems();
+        if (contentAsString.contains("no available free download slots left for your country")) {
+            throw new ServiceConnectionProblemException("There are no available free download slots left for your country today");
+        }
     }
 
     private void checkNameAndSize() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString().replaceAll("&nbsp;", " ");
         try {
             PlugUtils.checkName(httpFile, contentAsString, "Filename:</th>\n<td>", "<");
-        } catch(PluginImplementationException e) {
+        } catch (PluginImplementationException e) {
             try {
                 PlugUtils.checkName(httpFile, contentAsString, "Filename: <strong>", "</strong>");
-            } catch(PluginImplementationException e2) {
+            } catch (PluginImplementationException e2) {
                 PlugUtils.checkName(httpFile, contentAsString, "<div id=\"fname\">", "</div>");
             }
         }
         try {
             PlugUtils.checkFileSize(httpFile, contentAsString, "Size:</th>\n<td>", "<");
-        } catch(Exception e) {
+        } catch (Exception e) {
             try {
                 PlugUtils.checkFileSize(httpFile, contentAsString, "File size: <strong>", "</strong>");
-            } catch(Exception e2) {
+            } catch (Exception e2) {
                 PlugUtils.checkFileSize(httpFile, contentAsString, "<div id=\"fsize_div\">", "</div>");
             }
         }
--- src/sharingmatrix/cz/vity/freerapid/plugins/services/sharingmatrix/SharingMatrixServiceImpl.java
+++ src/sharingmatrix/cz/vity/freerapid/plugins/services/sharingmatrix/SharingMatrixServiceImpl.java
@@ -9,10 +9,12 @@ import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
 public class SharingMatrixServiceImpl extends AbstractFileShareService {
     private final static String SERVICE_NAME = "sharingmatrix.com";
 
+    @Override
     public String getName() {
         return SERVICE_NAME;
     }
 
+    @Override
     public int getMaxDownloadsFromOneIP() {
         return 1;
     }

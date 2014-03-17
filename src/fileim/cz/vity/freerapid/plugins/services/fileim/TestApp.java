21ae01d54b6e58532857d3c194daf376192ea545
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 12:55:29 +0530

    SVN Update# 3582 by tong2shot: FileIM.com - new plugin

---
+++ src/fileim/cz/vity/freerapid/plugins/services/fileim/TestApp.java
@@ -0,0 +1,38 @@
+package cz.vity.freerapid.plugins.services.fileim;
+
+import cz.vity.freerapid.plugins.dev.PluginDevApplication;
+import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import org.jdesktop.application.Application;
+
+import java.net.URL;
+
+/**
+ * @author tong2shot
+ */
+public class TestApp extends PluginDevApplication {
+    @Override
+    protected void startup() {
+        final HttpFile httpFile = getHttpFile();
+        try {
+            httpFile.setNewURL(new URL("http://www.fileim.com/file/4cc5a77532b3a561.html"));
+            final ConnectionSettings connectionSettings = new ConnectionSettings();
+            //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
+            final FileIMServiceImpl service = new FileIMServiceImpl();
+            testRun(service, httpFile, connectionSettings);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.exit();
+    }
+
+    /**
+     * Main start method for running this application
+     * Called from IDE
+     *
+     * @param args arguments for application
+     */
+    public static void main(String[] args) {
+        Application.launch(TestApp.class, args);
+    }
+}

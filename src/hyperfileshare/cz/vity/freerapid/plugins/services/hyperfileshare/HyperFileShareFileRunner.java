9d5f68a824716ea008b67f8904e7ca130f1b1e91
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:20:05 +0530

    SVN Update# 2072 by Vity: new plugin + asocciated plugins in build.xml + minor fix in plugin template

--- src/hyperfileshare/cz/vity/freerapid/plugins/services/hyperfileshare/HyperFileShareFileRunner.java
+++ src/hyperfileshare/cz/vity/freerapid/plugins/services/hyperfileshare/HyperFileShareFileRunner.java
@@ -1,9 +1,6 @@
-package cz.vity.freerapid.plugins.services.#shortsmall#;
+package cz.vity.freerapid.plugins.services.hyperfileshare;
 
-import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
-import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
-import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
-import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.exceptions.*;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
@@ -15,10 +12,10 @@ import java.util.logging.Logger;
 /**
  * Class which contains main code
  *
- * @author #author#
+ * @author Vity
  */
-class #fullWithoutDot#FileRunner extends AbstractRunner {
-    private final static Logger logger = Logger.getLogger(#fullWithoutDot#FileRunner.class.getName());
+class HyperFileShareFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(HyperFileShareFileRunner.class.getName());
 
 
     @Override
@@ -33,8 +30,8 @@ class #fullWithoutDot#FileRunner extends AbstractRunner {
     }
 
     private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
-        PlugUtils.checkName(httpFile, content, "FileNameLEFT", "FileNameRIGHT");//TODO
-        PlugUtils.checkFileSize(httpFile, content, "FillSizeLEFT", "FileSizeRIGHT");//TODO
+        PlugUtils.checkName(httpFile, content, "<span>Download ", "</span>");
+        PlugUtils.checkFileSize(httpFile, content, "File size:</div><div id=\"fileright\"><strong>", "</strong>");
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
@@ -47,14 +44,17 @@ class #fullWithoutDot#FileRunner extends AbstractRunner {
             final String contentAsString = getContentAsString();//check for response
             checkProblems();//check problems
             checkNameAndSize(contentAsString);//extract file name and size from the page
-            final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setActionFromFormWhereTagContains("Download!", true).toHttpMethod();//TODO
-
-            //here is the download link extraction
-            if (!tryDownloadAndSaveFile(httpMethod)) {
-                checkProblems();//if downloading failed
-                logger.warning(getContentAsString());//log the info
-                throw new PluginImplementationException();//some unknown problem
-            }
+            HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setActionFromAHrefWhereATagContains("downloadkn.gif").toHttpMethod();
+            if (makeRedirectedRequest(httpMethod)) {
+                final String url = PlugUtils.getStringBetween(getContentAsString(), "window.location = \"", "\";");
+                httpMethod = getMethodBuilder().setAction(url).toHttpMethod();
+                //here is the download link extraction
+                if (!tryDownloadAndSaveFile(httpMethod)) {
+                    checkProblems();//if downloading failed
+                    logger.warning(getContentAsString());//log the info
+                    throw new PluginImplementationException();//some unknown problem
+                }
+            } else throw new ServiceConnectionProblemException();
         } else {
             checkProblems();
             throw new ServiceConnectionProblemException();
@@ -63,9 +63,16 @@ class #fullWithoutDot#FileRunner extends AbstractRunner {
 
     private void checkProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
-        if (contentAsString.contains("File Not Found")) {//TODO
+        if (contentAsString.contains("Download URL is incorrect or")) {
             throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+        if (contentAsString.contains("You exceeded your download count limit")) {
+            throw new YouHaveToWaitException("You exceeded your download count limit", 120); //let to know user in FRD
         }
     }
 
+    @Override
+    protected String getBaseURL() {
+        return "http://download.hyperfileshare.com/";
+    }
 }
---
+++ src/hyperfileshare/cz/vity/freerapid/plugins/services/hyperfileshare/HyperFileShareServiceImpl.java
@@ -0,0 +1,32 @@
+package cz.vity.freerapid.plugins.services.hyperfileshare;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author Vity
+ */
+public class HyperFileShareServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "hyperfileshare.com";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        //don't forget to update this value, in plugin.xml don't forget to update this value too
+        return 3;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new HyperFileShareFileRunner();
+    }
+
+}

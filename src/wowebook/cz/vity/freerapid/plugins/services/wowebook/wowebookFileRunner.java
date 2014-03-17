ec2380745789b0c1111e37a9c88ed3e5e1b3eaab
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:08:38 +0530

    SVN Update# 3685 by Abinash: wowebook.org - new plugin

---
+++ src/wowebook/cz/vity/freerapid/plugins/services/wowebook/wowebookFileRunner.java
@@ -0,0 +1,63 @@
+package cz.vity.freerapid.plugins.services.wowebook;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.DownloadState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.net.URL;
+import java.util.logging.Logger;
+
+/**
+ * Class which contains main code
+ *
+ * @author Abinash Bishoyi
+ */
+class wowebookFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(wowebookFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
+        String url = "";
+        if (makeRedirectedRequest(method)) { //we make the main request
+            checkProblems();
+            if (fileURL.contains("/download/")) {
+                url = PlugUtils.getStringBetween(getContentAsString(), "urls=", "'});");
+            } else {
+                url = PlugUtils.getStringBetween(getContentAsString(), "Link: <a href=\"", "\"  target");
+            }
+            httpFile.setNewURL(new URL(url));
+            httpFile.setPluginID("");
+            httpFile.setState(DownloadState.QUEUED);
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("File Not Found")) {//TODO
+            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+    }
+
+}
---
+++ src/wowebook/cz/vity/freerapid/plugins/services/wowebook/wowebookServiceImpl.java
@@ -0,0 +1,28 @@
+package cz.vity.freerapid.plugins.services.wowebook;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author Abinash Bishoyi
+ */
+public class wowebookServiceImpl extends AbstractFileShareService {
+
+    @Override
+    public String getName() {
+        return "wowebook.org";
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new wowebookFileRunner();
+    }
+
+}

9eb7f7049c9ee93bedced1299db557cf2fd920c0
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 15:39:50 +0530

    SVN Update# 4854 by birchie: Fixed:  LinkBucks.com  -- both xxxxx.domain + domain\xxxxx  now supported, link generation code updated

--- src/linkbucks/cz/vity/freerapid/plugins/services/linkbucks/LinkBucksRunner.java
+++ src/linkbucks/cz/vity/freerapid/plugins/services/linkbucks/LinkBucksRunner.java
@@ -12,9 +12,10 @@ import org.apache.commons.httpclient.methods.GetMethod;
 
 import java.net.URL;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
 
 /**
- * @author Alex, ntoskrnl
+ * @author Alex, ntoskrnl, birchie
  */
 class LinkBucksRunner extends AbstractRunner {
     private final static Logger logger = Logger.getLogger(LinkBucksRunner.class.getName());
@@ -36,13 +37,28 @@ class LinkBucksRunner extends AbstractRunner {
                 s = getMethodBuilder().setActionFromIFrameSrcWhereTagContains("id=\"content\"").getAction();
             }
             if ((s.equals("") || s.equals("about:blank")) && (content.contains("linkbucksmedia.com/director/?t="))) {
-                final String token = PlugUtils.getStringBetween(content, "linkbucksmedia.com/director/?t=", "';");
+                final Matcher matchT = PlugUtils.matcher("linkbucksmedia.com/director/\\?t=(.+?)[\"']", content);
+                if (!matchT.find())
+                    throw new PluginImplementationException("linkbucks token not found");
+                final String token = matchT.group(1);
+
+                // valid authkey is from script function containing uncommented line "var f = window['init' + 'Lb' + 'js' + ''];" ("initLbjs" only, no trailing chars)
+                final String authKeyMatchStr = "A(?:'\\s?\\+\\s?')?u(?:'\\s?\\+\\s?')?t(?:'\\s?\\+\\s?')?h(?:'\\s?\\+\\s?')?K(?:'\\s?\\+\\s?')?e(?:'\\s?\\+\\s?')?y";
+                final String aKey1MatchStr = "\\s\\sparams\\['" + authKeyMatchStr + "'\\]\\s?=\\s?(\\d+?);";
+                final String aKey2MatchStr = "\\s\\sparams\\['" + authKeyMatchStr + "'\\]\\s?=\\s?params\\['" + authKeyMatchStr + "'\\]\\s?\\+\\s?(\\d+?);";
+                final String initLbjsMatchStr = "\\s\\s\\w[^/]+?js'\\s?\\+\\s?''\\];";
+                final Matcher matchKey = PlugUtils.matcher(aKey1MatchStr + "[^<]+?" + aKey2MatchStr + "[^<]+?" + initLbjsMatchStr, content.replaceAll("\\s", " "));
+                if (!matchKey.find())
+                    throw new PluginImplementationException("linkbucks authkey not found");
+                final String authKey = "" + (Long.parseLong(matchKey.group(1)) + Long.parseLong(matchKey.group(2)));
+
                 final HttpMethod httpMethod = getMethodBuilder().setAjax()
                         .setAction("/intermission/loadTargetUrl")
                         .setParameter("t", token)
+                        .setParameter("ak", authKey)
                         .setReferer(fileURL)
                         .toGetMethod();
-                final int wait = PlugUtils.getNumberBetween(content, "Countdown : ", ",");
+                final int wait = PlugUtils.getNumberBetween(content, "Countdown: ", ",");
                 downloadTask.sleep(wait + 1);
                 if (!makeRedirectedRequest(httpMethod)) {
                     checkProblems();

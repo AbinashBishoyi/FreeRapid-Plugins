f3d25cf099caa800fa24465c40314e73c735bb5d
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:53:04 +0530

    SVN Update# 4016 by birchie: New XFS Plugin : IGetFile.com

---
+++ src/igetfile/cz/vity/freerapid/plugins/services/igetfile/IGetFileFileNameHandler.java
@@ -0,0 +1,20 @@
+package cz.vity.freerapid.plugins.services.igetfile;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileNameHandler;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+
+import java.util.regex.Matcher;
+
+public class IGetFileFileNameHandler implements FileNameHandler {
+
+    @Override
+    public void checkFileName(final HttpFile httpFile, final String content) throws ErrorDuringDownloadingException {
+        Matcher match = PlugUtils.matcher("Downloading:\\s+?(.+?)\\s+?\\(", content);
+        if (!match.find())
+            throw new PluginImplementationException("File name not found");
+        httpFile.setFileName(match.group(1));
+    }
+}

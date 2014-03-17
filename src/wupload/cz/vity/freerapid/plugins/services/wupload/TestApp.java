ba7d36d843d691265949609e6a9681cb316238f0
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 11:44:06 +0530

    SVN Update# 3122 by wordrider: wupload plugin initial working release 0.1 by Coloss

---
+++ src/wupload/cz/vity/freerapid/plugins/services/wupload/TestApp.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.wupload;
+
+import cz.vity.freerapid.plugins.dev.PluginDevApplication;
+import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import org.jdesktop.application.Application;
+
+import java.net.URL;
+
+/**
+ * @author Coloss
+ */
+public class TestApp extends PluginDevApplication {
+    @Override
+    protected void startup() {
+        final HttpFile httpFile = getHttpFile();
+        try {
+            httpFile.setNewURL(new URL("http://www.wupload.com/file/6212977/testPNG.png"));
+            final ConnectionSettings connectionSettings = new ConnectionSettings();
+            // connectionSettings.setProxy("localhost", 8081);
+            testRun(new WuploadShareServiceImpl(), httpFile, connectionSettings);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.exit();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(TestApp.class, args);
+    }
+}

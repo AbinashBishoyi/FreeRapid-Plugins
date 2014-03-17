d6726c8846a17597a3b72c2c672e36c455bc4816
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:13:36 +0530

    SVN Update# 1429 by Thonda:

---
+++ src/filesend/cz/vity/freerapid/plugins/services/nahrajcz/NahrajCzServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.nahrajcz;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Vity
+ *         Class that provides basic info about plugin
+ */
+public class NahrajCzServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "nahraj.cz";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 9;//TO DO
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new NahrajCzFileRunner();
+    }
+
+}

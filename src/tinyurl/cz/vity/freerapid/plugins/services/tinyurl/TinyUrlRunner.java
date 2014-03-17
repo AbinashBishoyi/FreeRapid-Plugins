692eccaedf3c067bbad6a97f6f3fb170a6ed9c47
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 06:55:28 +0530

    SVN Update# 1228 by Vity: fixed - getResponseHeaders != getResponseHeader

--- src/tinyurl/cz/vity/freerapid/plugins/services/tinyurl/TinyUrlRunner.java
+++ src/tinyurl/cz/vity/freerapid/plugins/services/tinyurl/TinyUrlRunner.java
@@ -1,23 +1,17 @@
 package cz.vity.freerapid.plugins.services.tinyurl;
 
-import cz.vity.freerapid.plugins.exceptions.InvalidURLOrServiceProblemException;
-import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
-import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
-
 import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
-
-
-
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
 import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.DownloadState;
-import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.methods.GetMethod;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
 
 /**
  * @author Alex
@@ -32,29 +26,29 @@ class TinyUrlRunner extends AbstractRunner {
         final GetMethod method = getGetMethod(fileURL);
         logger.info(fileURL);
 
-       
- 
+
         //it works to me .... compilable
-            if (client.makeRequest(method, false) == HttpStatus.SC_MOVED_PERMANENTLY) {
+        if (client.makeRequest(method, false) == HttpStatus.SC_MOVED_PERMANENTLY) {
+            final Header locationHeader = method.getResponseHeader("Location");
+            if (locationHeader == null)
+                throw new PluginImplementationException();
+            String s = locationHeader.getValue();
+            logger.info(s);
+            this.httpFile.setNewURL(new URL(s));
+            this.httpFile.setPluginID("");
+            this.httpFile.setState(DownloadState.QUEUED);
 
-         
-                String s = method.getResponseHeaders("Location").toString();
-                logger.info(s);
-                this.httpFile.setNewURL(new URL(s));
-                this.httpFile.setPluginID("");
-                this.httpFile.setState(DownloadState.QUEUED);
 
-          
-            } else {
+        } else {
             checkProblems();
             throw new ServiceConnectionProblemException();
-            }
-        } 
+        }
+    }
 
     private void checkProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
-        if (contentAsString.contains("was not found")) {
-            throw new URLNotAvailableAnymoreException("The page you requested was not found in our database.");
+        if (contentAsString.contains("Unable to find site")) {
+            throw new URLNotAvailableAnymoreException("Unable to find site's URL to redirect to.");
         }
     }
 

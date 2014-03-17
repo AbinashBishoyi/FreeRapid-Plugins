682b0de0a9fc0f1e55cdee014791f2c3a11e2057
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:09:57 +0530

    SVN Update# 1397 by Vity: czshare free by smisek + minor changes - code optimization

---
+++ src/czshare/cz/vity/freerapid/plugins/services/czshare/CzshareServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.czshare;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Ladislav Vitasek, Ludek Zika
+ */
+public class CzshareServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "czshare.com";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new CzshareRunner();
+    }
+
+}

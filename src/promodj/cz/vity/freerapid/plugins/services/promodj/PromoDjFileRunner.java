ddd1440ff3eacaa8d7dd6bd9f109fb4a5d207aac
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:45:01 +0530

    SVN Update# 3945 by Abinash: promodj.com - new plugin

---
+++ src/promodj/cz/vity/freerapid/plugins/services/promodj/PromoDjFileRunner.java
@@ -0,0 +1,75 @@
+package cz.vity.freerapid.plugins.services.promodj;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+
+/**
+ * Class which contains main code
+ *
+ * @author Abinash Bishoyi
+ */
+class PromoDjFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(PromoDjFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
+        final Matcher matcher = getMatcherAgainstContent("Format:</b> <a href=\"http://promodj.com/source/(.+?)/(.+?)\">");
+        if (!matcher.find()) {
+            throw new PluginImplementationException("File name/size not found");
+        }
+        httpFile.setFileName(PlugUtils.unescapeHtml(matcher.group(2)));
+        PlugUtils.checkFileSize(httpFile, content, "Size:</b>", "<br />");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
+        if (makeRedirectedRequest(method)) { //we make the main request
+            final String contentAsString = getContentAsString();//check for response
+            checkProblems();//check problems
+            checkNameAndSize(contentAsString);//extract file name and size from the page
+            final HttpMethod httpMethod = getMethodBuilder().setAction(PlugUtils.getStringBetween(getContentAsString(), "Format:</b> <a href=\"", "\">")).toHttpMethod();
+            //here is the download link extraction
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkProblems();//if downloading failed
+                throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("File Not Found")) {
+            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+    }
+
+}

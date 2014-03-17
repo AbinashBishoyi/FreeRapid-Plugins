5701abd98126a046db23c9a1a43933e48507fbe9
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:56:40 +0530

    SVN Update# 1864 by JPEXS: Added uploadspace.eu plugin in version 1.0

---
+++ src/uploadspace/cz/vity/freerapid/plugins/services/uploadspace/UploadSpaceServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.uploadspace;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ * @author JPEXS
+ */
+public class UploadSpaceServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "uploadspace.eu";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new UploadSpaceFileRunner();
+    }
+
+}

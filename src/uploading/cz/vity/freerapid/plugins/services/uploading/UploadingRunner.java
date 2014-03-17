f5f4cfbc62642e6374b4e9ff771b273ce910709d
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:02:54 +0530

    SVN Update# 1323 by Lukiz: init uploading.com

---
+++ src/uploading/cz/vity/freerapid/plugins/services/uploading/UploadingRunner.java
@@ -0,0 +1,120 @@
+package cz.vity.freerapid.plugins.services.uploading;
+
+import cz.vity.freerapid.plugins.exceptions.*;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+
+/**
+ * @author Ladislav Vitasek, Ludek Zika
+ */
+class UploadingRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(UploadingRunner.class.getName());
+
+    public UploadingRunner() {
+        super();
+    }
+
+    public void runCheck() throws Exception {
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);
+        if (makeRedirectedRequest(getMethod)) {
+            checkNameAndSize(getContentAsString());
+        } else
+            throw new PluginImplementationException();
+    }
+
+    public void run() throws Exception {
+        super.run();
+
+        final GetMethod getMethod = getGetMethod(fileURL);
+        getMethod.setFollowRedirects(true);
+        if (makeRedirectedRequest(getMethod)) {
+            if (getContentAsString().contains("downloadform")) {
+                checkNameAndSize(getContentAsString());
+                PostMethod method = getPostMethod(fileURL);
+                method.addParameter("free", "1");
+                if (makeRedirectedRequest(method)) {
+                    if (!getContentAsString().contains("name=\"x\"")) {
+                        logger.info(getContentAsString());
+                        throw new PluginImplementationException();
+                    }
+
+                    downloadTask.sleep(5);
+                    PostMethod method2 = getPostMethod(fileURL);
+                    method2.addParameter("free", "1");
+                    method2.addParameter("x", "1");
+                    if (!tryDownloadAndSaveFile(method2)) {
+                        checkProblems();
+                        logger.warning(getContentAsString());
+                        throw new IOException("File input stream is empty.");
+                    }
+
+                } else {
+                    logger.info(getContentAsString());
+                    throw new PluginImplementationException();
+                }
+            } else {
+                checkProblems();
+                logger.info(getContentAsString());
+                throw new PluginImplementationException();
+            }
+        } else
+            throw new PluginImplementationException();
+    }
+
+    private String sicherName(String s) throws UnsupportedEncodingException {
+        Matcher matcher = PlugUtils.matcher("(.*/)([^/]*)$", s);
+        if (matcher.find()) {
+            return matcher.group(2);
+        }
+        return "file01";
+    }
+
+    private void checkNameAndSize(String content) throws Exception {
+
+        if (!content.contains("uploading.com")) {
+            logger.warning(content);
+            throw new InvalidURLOrServiceProblemException("Invalid URL or unindentified service");
+        }
+        if (content.contains("FILE REMOVED")) {
+            throw new URLNotAvailableAnymoreException("FILE REMOVED BECAUSE OF ABUSE OR DELETED BY OWNER");
+        }
+
+        Matcher matcher = PlugUtils.matcher("Download file </h.> <b>([^<]+)", content);
+        // odebiram jmeno
+        String fn;
+        if (matcher.find()) {
+            fn = matcher.group(1);
+        } else fn = sicherName(fileURL);
+        logger.info("File name " + fn);
+        httpFile.setFileName(fn);
+        // konec odebirani jmena
+
+        matcher = PlugUtils.matcher("([0-9.]+ .B)<br/>", content);
+        if (matcher.find()) {
+            Long a = PlugUtils.getFileSizeFromString(matcher.group(1));
+            logger.info("File size " + a);
+            httpFile.setFileSize(a);
+            httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+        }
+
+    }
+
+
+    private void checkProblems() throws ServiceConnectionProblemException, YouHaveToWaitException, URLNotAvailableAnymoreException {
+        Matcher matcher;
+        matcher = getMatcherAgainstContent("FILE REMOVED");
+        if (matcher.find()) {
+            throw new URLNotAvailableAnymoreException("<b>FILE REMOVED BECAUSE OF ABUSE OR DELETED BY OWNER</b><br>");
+        }
+    }
+
+}
---
+++ src/uploading/cz/vity/freerapid/plugins/services/uploading/UploadingServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.uploading;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Ladislav Vitasek, Ludek Zika
+ */
+public class UploadingServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "uploading.com";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new UploadingRunner();
+    }
+
+
+}

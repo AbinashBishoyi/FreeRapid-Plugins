8c1c1e33c94309e166b92439bc3120cdb3fd3d59
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:47:50 +0530

    SVN Update# 1778 by Eterad: DiesperseiT Plugin - New

---
+++ src/disperseit/cz/vity/freerapid/plugins/services/disperseit/DisperseiTServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.disperseit;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ * @author Eterad
+ */
+public class DisperseiTServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "disperseit.com";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 9;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new DisperseiTFileRunner();
+    }
+
+}

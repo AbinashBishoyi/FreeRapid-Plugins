2c16314f4c62c1777df18b6a19e02c63d5d661c4
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:37:01 +0530

    SVN Update# 1687 by Kajda: Plugin uGotFile.com

---
+++ src/ugotfile/cz/vity/freerapid/plugins/services/ugotfile/UGotFileServiceImpl.java
@@ -0,0 +1,29 @@
+package cz.vity.freerapid.plugins.services.ugotfile;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Kajda
+ */
+public class UGotFileServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "ugotfile.com";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new UGotFileFileRunner();
+    }
+}

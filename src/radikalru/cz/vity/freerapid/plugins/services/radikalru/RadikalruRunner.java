0e245883145be6f0d2edbf155aef26fbd4115b3f
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 06:58:07 +0530

    SVN Update# 1261 by Vity: line formatting

--- src/radikalru/cz/vity/freerapid/plugins/services/radikalru/RadikalruRunner.java
+++ src/radikalru/cz/vity/freerapid/plugins/services/radikalru/RadikalruRunner.java
@@ -1,9 +1,6 @@
 package cz.vity.freerapid.plugins.services.radikalru;
 
-import cz.vity.freerapid.plugins.exceptions.InvalidURLOrServiceProblemException;
-import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
 import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
-import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
@@ -30,31 +27,27 @@ class RadikalruRunner extends AbstractRunner {
 
         GetMethod getMethod = getGetMethod(fileURL);
 
-                        if (!tryDownloadAndSaveFile(getMethod)) {
-                            checkProblems();
-                            logger.warning(getContentAsString());//something was really wrong, we will explore it from the logs :-)
-                            throw new IOException("File input stream is empty.");
-                        }
+        if (!tryDownloadAndSaveFile(getMethod)) {
+            checkProblems();
+            logger.warning(getContentAsString());//something was really wrong, we will explore it from the logs :-)
+            throw new IOException("File input stream is empty.");
+        }
 
     }
 
     private String processURL(String mURL) throws Exception {
         if (mURL.contains("t.jpg")) {
-            return mURL.replace("t.jpg",".jpg");
+            return mURL.replace("t.jpg", ".jpg");
         } else return mURL;
     }
-    
+
     private void checkName(String content) throws Exception {
-        Matcher matcher = PlugUtils.matcher("http://[0-9a-z]+.radikal.ru/(.+/)?(.+?.jpg)",content);
+        Matcher matcher = PlugUtils.matcher("http://[0-9a-z]+.radikal.ru/(.+/)?(.+?.jpg)", content);
         if (matcher.find()) {
             httpFile.setFileName(matcher.group(2));
-            logger.info("File Name: "+ matcher.group(2));
+            logger.info("File Name: " + matcher.group(2));
             httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
-
         }
-
-
-
     }
 
 

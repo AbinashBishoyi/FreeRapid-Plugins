83a8aa22d4e751ab83a10ba2a9a3b593a7d259cb
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 14:34:55 +0530

    SVN Update# 4388 by tong2shot: KeyCaptcha : - modify s_s_c_web_server_sign4. - fix mm content regex.

--- src/keycaptcha/cz/vity/freerapid/plugins/services/keycaptcha/KeyCaptchaClient.java
+++ src/keycaptcha/cz/vity/freerapid/plugins/services/keycaptcha/KeyCaptchaClient.java
@@ -32,24 +32,34 @@ class KeyCaptchaClient {
     private final Map<String, String> parameters = new HashMap<String, String>();
     private MethodBuilder responseMethodBuilder;
     private String responseUrl;
+    private String gjsContent;
 
     public KeyCaptchaClient(final HttpDownloadClient client) {
         this.client = client;
     }
 
+    /**
+     * Populate parameters and images
+     *
+     * @param pageContent Main page content
+     * @param pageUrl     Main page URL
+     * @return Populated images
+     * @throws Exception
+     */
     public KeyCaptchaImages getImages(final String pageContent, final String pageUrl) throws Exception {
         try {
             final String formContent = findFormContent(pageContent);
             loadPagesAndPopulateParameters(formContent, pageUrl);
 
+            //image url locations are in gjs
             final KeyCaptchaImages images = new KeyCaptchaImages();
-            Matcher matcher = PlugUtils.matcher("\\('([^']+?)','([^']+?)',[^,\\)]+?,false\\)", client.getContentAsString());
+            Matcher matcher = PlugUtils.matcher("\\('([^']+?)','([^']+?)',[^,\\)]+?,false\\)", gjsContent);
             if (!matcher.find()) {
                 throw new PluginImplementationException("KeyCaptcha image data not found");
             }
             getBackgroundImage(images, matcher.group(1), matcher.group(2));
 
-            matcher = PlugUtils.matcher("\\('([^']+?)','([^']+?)',[^,\\)]+?,true\\)", client.getContentAsString());
+            matcher = PlugUtils.matcher("\\('([^']+?)','([^']+?)',[^,\\)]+?,true\\)", gjsContent);
             if (!matcher.find()) {
                 throw new PluginImplementationException("KeyCaptcha image data not found");
             }
@@ -64,28 +74,31 @@ class KeyCaptchaClient {
     public HttpMethod getResult(final List<Point> pieceLocations, final List<Point> mouseLocations) throws Exception {
         try {
             final String webServerSignParam = parameters.get("s_s_c_web_server_sign");
-            final String js1Url = responseUrl + "/swfs/mm?pS="
+            final String mmUrl = responseUrl + "/swfs/mm?pS="
                     + KeyCaptchaCrypt.encrypt(webServerSignParam, webServerSignParam + "Khd21M47")
                     + "&cP=" + sscGetParams("")
                     + "&mms=" + Math.random()
                     + "&r=" + Math.random();
-            client.makeRequest(client.getGetMethod(js1Url), true);
-            Matcher matcher = PlugUtils.matcher("\\|(.+?)'\\.split", client.getContentAsString());
+            logger.info("mmUrl : " + mmUrl);
+            client.makeRequest(client.getGetMethod(mmUrl), true);
+            Matcher matcher = PlugUtils.matcher("\\|([A-Za-z0-9]+)'\\.split", client.getContentAsString());
             if (!matcher.find()) {
                 throw new PluginImplementationException("Error parsing KeyCaptcha server response (1)");
             }
 
             parameters.put("extra", "2813d567024c09716fa4b1244a98b7c7");
-            final String js2Url = responseUrl + "/swfs/cjs?pS=123&cOut="
+            final String cjsUrl = responseUrl + "/swfs/cjs?pS=123&cOut="
                     + KeyCaptchaCrypt.encrypt(createResponse(pieceLocations), matcher.group(1))
                     + "..." + createResponse(mouseLocations)
                     + "&cP=" + sscGetParams("");
-            client.makeRequest(client.getGetMethod(js2Url), true);
+            logger.info("cjsUrl : " + cjsUrl);
+            client.makeRequest(client.getGetMethod(cjsUrl), true);
             matcher = PlugUtils.matcher("s_s_c_setcapvalue\\(\\s*\"(.+?)\"", client.getContentAsString());
             if (!matcher.find()) {
                 throw new PluginImplementationException("Error parsing KeyCaptcha server response (2)");
             }
             final String result = matcher.group(1);
+            logger.info("result : " + result);
             checkResponse(result);
             return responseMethodBuilder.setParameter("capcode", result).toPostMethod();
         } catch (final PluginImplementationException e) {
@@ -116,6 +129,14 @@ class KeyCaptchaClient {
         }
     }
 
+    /**
+     * Get main page KeyCaptcha form content
+     *
+     * @param pageContent Main page content
+     * @return KeyCaptcha form content
+     * @throws ErrorDuringDownloadingException
+     *
+     */
     private String findFormContent(final String pageContent) throws ErrorDuringDownloadingException {
         final Matcher matcher = PlugUtils.matcher("(?s)(<form\\b.+?</form>)", pageContent);
         while (matcher.find()) {
@@ -131,30 +152,38 @@ class KeyCaptchaClient {
         findParameters(formContent);
         parameters.put("pageUrl", pageUrl.split("#")[0]);
 
-        final String js1Url = findString("<script [^<>]+?src=['\"](.+?)['\"]", "script 1 URL", formContent);
-        client.makeRequest(client.getGetMethod(js1Url), true);
-
-        final String js2Url = findString("var _13=\"(.+?)\"", "script 2 URL", client.getContentAsString())
+        //capJs (cap.js)
+        final String capJsUrl = findString("<script [^<>]+?src=['\"](.+?)['\"]", "script 1 URL", formContent);
+        logger.info("CapJsUrl : " + capJsUrl);
+        client.makeRequest(client.getGetMethod(capJsUrl), true);
+        final String capsJsUrl = findString("var _13=\"(.+?)\"", "script 2 URL", client.getContentAsString())
                 + parameters.get("s_s_c_user_id")
                 + "&u=" + URLEncoder.encode(parameters.get("pageUrl"), "UTF-8")
                 + "&r=" + Math.random();
-        client.makeRequest(client.getGetMethod(js2Url), true);
 
-        parameters.put("s_s_c_web_server_sign4",
-                findString("s_s_c_web_server_sign4=\"(.+?)\";", "parameter 4", client.getContentAsString()));
+        //capsJs (caps.js)
+        logger.info("CapsJsUrl : " + capsJsUrl);
+        client.makeRequest(client.getGetMethod(capsJsUrl), true);
+        String s_s_c_web_server_sign4 = findString("s_s_c_web_server_sign4=\"(.+?)\";", "parameter 4", client.getContentAsString());
+        s_s_c_web_server_sign4 = s_s_c_web_server_sign4.substring(0, 10) + "690" + s_s_c_web_server_sign4.substring(10);
+        parameters.put("s_s_c_web_server_sign4", s_s_c_web_server_sign4);
         parameters.put("extra",
                 findString("s_s_c_check_process\\)\\{\\s*return \"(.+?)\"", "extra parameter", client.getContentAsString()));
-
         final String extra2 = findString("dn3iufwwoi4=\"(.+?)\"", "js3 extra2 parameter", client.getContentAsString());
-        final String js3Url = findString("\"src\",\"(.+?)\"", "script 3 URL", client.getContentAsString())
+        final String gjsUrl = findString("\"src\",\"(.+?)\"", "script 3 URL", client.getContentAsString())
                 + sscGetParams("|" + extra2) + "&r=" + Math.random();
-        client.makeRequest(client.getGetMethod(js3Url), true);
 
+        //gjs
+        logger.info("GjsUrl : " + gjsUrl);
+        client.makeRequest(client.getGetMethod(gjsUrl), true);
+        gjsContent = client.getContentAsString();
         parameters.put("s_s_c_web_server_sign3",
-                findString("s_s_c_setnewws\\(\"(.+?)\"", "parameter 3", client.getContentAsString()));
-
-        responseUrl = findString("setAttribute\\('src','(.+?)'", "response URL", client.getContentAsString());
+                findString("s_s_c_setnewws\\(\"(.+?)\"", "parameter 3", gjsContent));
+        responseUrl = findString("setAttribute\\('src','(.+?)'", "response URL", gjsContent);
         responseUrl = responseUrl.substring(0, responseUrl.indexOf('/', "https://".length()));
+        logger.info("responseUrl : " + responseUrl);
+
+        //main page KeyCaptcha form methodBuilder
         responseMethodBuilder = new MethodBuilder(formContent, client)
                 .setReferer(pageUrl)
                 .setBaseURL(pageUrl.substring(0, pageUrl.indexOf('/', "https://".length()) + 1))

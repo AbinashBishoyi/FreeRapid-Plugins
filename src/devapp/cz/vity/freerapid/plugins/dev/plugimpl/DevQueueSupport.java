8a5d1543f9ab1d083d13f159a1c08754899aa0ce
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 09:35:09 +0530

    SVN Update# 2757 by ntoskrnl: fixed devapp

--- src/devapp/cz/vity/freerapid/plugins/dev/plugimpl/DevQueueSupport.java
+++ src/devapp/cz/vity/freerapid/plugins/dev/plugimpl/DevQueueSupport.java
@@ -1,5 +1,6 @@
 package cz.vity.freerapid.plugins.dev.plugimpl;
 
+import cz.vity.freerapid.plugins.container.FileInfo;
 import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
 import cz.vity.freerapid.plugins.webclient.interfaces.MaintainQueueSupport;
 
@@ -21,14 +22,24 @@ public class DevQueueSupport implements MaintainQueueSupport {
             builder.append(uri).append('\n');
         }
         logger.info(builder.toString());
-        return false;
+        return true;
     }
 
     @Override
     public boolean addLinksToQueue(HttpFile parentFile, String data) {
         StringBuilder builder = new StringBuilder().append("The following data will be parsed for links to be added into the queue:\n\n").append(data);
         logger.info(builder.toString());
-        return false;
+        return true;
+    }
+
+    @Override
+    public boolean addLinksToQueueFromContainer(HttpFile parentFile, List<FileInfo> infoList) {
+        StringBuilder builder = new StringBuilder().append("The following files will be added into the queue:\n");
+        for (FileInfo info : infoList) {
+            builder.append(info.getFileUrl()).append('\n');
+        }
+        logger.info(builder.toString());
+        return true;
     }
 
     @Override
@@ -38,15 +49,24 @@ public class DevQueueSupport implements MaintainQueueSupport {
             builder.append(uri).append('\n');
         }
         logger.info(builder.toString());
-        return false;
-
+        return true;
     }
 
     @Override
     public boolean addLinkToQueueUsingPriority(HttpFile parentFile, String data) throws Exception {
         StringBuilder builder = new StringBuilder().append("The following data will be parsed for links of which one will be chosen to be added into the queue:\n\n").append(data);
         logger.info(builder.toString());
-        return false;
+        return true;
+    }
+
+    @Override
+    public boolean addLinkToQueueFromContainerUsingPriority(HttpFile parentFile, List<FileInfo> infoList) {
+        StringBuilder builder = new StringBuilder().append("One of the following files will be added into the queue:\n");
+        for (FileInfo info : infoList) {
+            builder.append(info.getFileUrl()).append('\n');
+        }
+        logger.info(builder.toString());
+        return true;
     }
 
 }

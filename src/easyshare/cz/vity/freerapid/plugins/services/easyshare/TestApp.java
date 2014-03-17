005b677e6eec46e23e746905566190204d2c9e24
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:45:58 +0530

    SVN Update# 1762 by Vity: easyshare testapp fix

--- src/easyshare/cz/vity/freerapid/plugins/services/easyshare/TestApp.java
+++ src/easyshare/cz/vity/freerapid/plugins/services/easyshare/TestApp.java
@@ -1,4 +1,4 @@
-package cz.vity.freerapid.plugins.services.dailymotion;
+package cz.vity.freerapid.plugins.services.easyshare;
 
 import cz.vity.freerapid.plugins.dev.PluginDevApplication;
 import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
@@ -8,36 +8,23 @@ import org.jdesktop.application.Application;
 import java.net.URL;
 
 /**
- * @author Vity
+ * @author Ladislav Vitasek
  */
 public class TestApp extends PluginDevApplication {
-    @Override
     protected void startup() {
-        final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
+        final HttpFile httpFile = getHttpFile();
         try {
-            //we set file URL
-            httpFile.setNewURL(new URL("http://www.dailymotion.com/fr/featured/channel/videogames/video/x97z45_captivate-2009-tous-les-futurs-hits_videogames"));
-            //the way we connect to the internet
-            final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
-            //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
-            //then we tries to download
-            final DailymotionServiceImpl service = new DailymotionServiceImpl(); //instance of service - of our plugin
-            //runcheck makes the validation
-            testRun(service, httpFile, connectionSettings);//download file with service and its Runner
-            //all output goes to the console
-        } catch (Exception e) {//catch possible exception
-            e.printStackTrace(); //writes error output - stack trace to console
+            httpFile.setNewURL(new URL("http://w17.easy-share.com/1701853057.html"));
+            final ConnectionSettings connectionSettings = new ConnectionSettings();
+            connectionSettings.setProxy("localhost", 8081);
+            testRun(new EasyShareServiceImpl(), httpFile, connectionSettings);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        this.exit();//exit application
+        this.exit();
     }
 
-    /**
-     * Main start method for running this application
-     * Called from IDE
-     *
-     * @param args arguments for application
-     */
     public static void main(String[] args) {
-        Application.launch(TestApp.class, args);//starts the application - calls startup() internally
+        Application.launch(TestApp.class, args);
     }
 }

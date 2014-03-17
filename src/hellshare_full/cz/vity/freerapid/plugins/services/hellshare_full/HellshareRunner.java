70e1be52e5af38a8e119290f7e8e790ee0b80c12
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:58:40 +0530

    SVN Update# 1883 by Smisek:

--- src/hellshare_full/cz/vity/freerapid/plugins/services/hellshare_full/HellshareRunner.java
+++ src/hellshare_full/cz/vity/freerapid/plugins/services/hellshare_full/HellshareRunner.java
@@ -6,6 +6,7 @@ import cz.vity.freerapid.plugins.webclient.DownloadState;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 
@@ -22,7 +23,7 @@ class HellshareRunner extends AbstractRunner {
     @Override
     public void runCheck() throws Exception {
         super.runCheck();
-        final GetMethod getMethod = getGetMethod(fileURL);
+        final HttpMethod getMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
         if (makeRedirectedRequest(getMethod)) {
             checkNameAndSize(getContentAsString());
         } else
@@ -34,24 +35,22 @@ class HellshareRunner extends AbstractRunner {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
 
-        final GetMethod getMethod = getGetMethod(fileURL);
-        getMethod.setFollowRedirects(true);
-        if (makeRequest(getMethod)) {
+        final HttpMethod getMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
+        if (makeRedirectedRequest(getMethod)) {
+
             checkNameAndSize(getContentAsString());
 
-            client.setReferer(fileURL);
-
-            Matcher matcher = getMatcherAgainstContent("<p>U.ivatel nep.ihl..en.</p>|<p>Pou..vate. neprihl.sen..</p>|<p>A felhaszn.l. nincs bejelentkezve.</p>");
+            Matcher matcher = getMatcherAgainstContent("<h2 class=\"hidden\">P.ihl.si. s.</h2>|<h2 class=\"hidden\">BejelentkezÃ©s</h2>|<h2 class=\"hidden\">Sign In</h2>");
             if (matcher.find()) {
                 Login(getContentAsString());
             }
 
-            matcher = getMatcherAgainstContent("Bohu.el nem.te dostatek kredit. na sta.en. souboru|Nem.te dostato.n. kredit pre download s.boru|Nem rendelkezik elegend. kredittel a f.jl let.lt.s.hez");
+            matcher = getMatcherAgainstContent("<a id=\"button-download-full-nocredit\"");
             if (matcher.find()) {
-                throw new NotRecoverableDownloadException("No credit for download!");
+                 throw new NotRecoverableDownloadException("No credit for download!");
             }
 
-            matcher = getMatcherAgainstContent(", [0-9]+[)];\" href=\"([^\"]+)\" target=\"full-download-iframe\">Download</a>");
+            matcher = getMatcherAgainstContent(" href=\"([^\"]+)\" target=\"full-download-iframe\">[^<]+</a>");
             if (matcher.find()) {
                 String downURL = matcher.group(1);
                 final GetMethod getmethod = getGetMethod(downURL);
@@ -68,26 +67,13 @@ class HellshareRunner extends AbstractRunner {
             }
 
         } else
-            throw new PluginImplementationException();
+            throw new ServiceConnectionProblemException();
     }
 
     private void checkNameAndSize(String content) throws Exception {
-        if (getContentAsString().contains("FreeDownProgress")) {
-            Matcher matcher = PlugUtils.matcher("<table class=\"download-properties\">[^<]+<tr><th scope=\"row\" class=\"download-properties-label\">[^<]+</th><td><h2>([^<]+)</h2></td></tr>[^<]+<tr><th scope=\"row\" class=\"download-properties-label\">[^<]+</th><td>([^<]+ .B)</td></tr>", content);
-            if (matcher.find()) {
-                String fn = matcher.group(1);
-                logger.info("File name " + fn);
-                httpFile.setFileName(fn);
-                Long a = PlugUtils.getFileSizeFromString(matcher.group(2));
-                logger.info("File size " + a);
-                httpFile.setFileSize(a);
-            }
-            httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
-        } else {
-            checkProblems();
-            logger.info(getContentAsString());
-            throw new PluginImplementationException();
-        }
+        PlugUtils.checkName(httpFile, content, "Obsah ", "o velikosti");
+        PlugUtils.checkFileSize(httpFile, content, "o velikosti", "byl nahr");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
     private void Login(String content) throws Exception {
@@ -102,38 +88,29 @@ class HellshareRunner extends AbstractRunner {
                 }
                 badConfig = false;
             }
-
-            Matcher matcher = PlugUtils.matcher("<p><span>. <a href=\"([^\"]+)\">P.ihl..en.</a> .</span></p>|<p><span>. <a href=\"([^\"]+)\">Prihl.senie</a> .</span></p>|<p><span>. <a href=\"([^\"]+)\">Bejelentkez.s</a> .</span></p>", content);
+            Matcher matcher = PlugUtils.matcher("<form id=\"([^\"]+)\" class=\"[^\"]+\" method=\"post\" action=\"([^\"]+)\" enctype=\"multipart/form-data\"", getContentAsString());
             if (!matcher.find()) {
                 throw new PluginImplementationException();
             }
-            String loginURL = matcher.group(1) != null ? matcher.group(1) : (matcher.group(2) != null ? matcher.group(2) : matcher.group(3));
-            GetMethod loginMethod = getGetMethod(loginURL);
-            if (makeRequest(loginMethod)) {
-                matcher = PlugUtils.matcher("<form name=\"([^\"]+)\" method=\"post\" action=\"([^\"]+)\">", getContentAsString());
-                if (!matcher.find()) {
+            String formName = matcher.group(1);
+            String postURL = matcher.group(2);
+
+            PostMethod postmethod = getPostMethod(postURL);
+
+            postmethod.addParameter(formName + "_lg", pa.getUsername());
+            postmethod.addParameter(formName + "_psw", pa.getPassword());
+            postmethod.addParameter(formName + "_sbm", "Prihlasit");
+            postmethod.addParameter("DownloadRedirect", "");
+
+            if (makeRedirectedRequest(postmethod)) {
+                matcher = getMatcherAgainstContent("<h2 class=\"hidden\">P.ihl.si. s.</h2>|<h2 class=\"hidden\">BejelentkezÃ©s</h2>|<h2 class=\"hidden\">Sign In</h2>");
+                if (matcher.find()) {
+                    badConfig = true;
+                    throw new NotRecoverableDownloadException("Bad HellShare full account login information!");
+                }
+                GetMethod getMethod = getGetMethod(fileURL);
+                if (!makeRedirectedRequest(getMethod))
                     throw new PluginImplementationException();
-                }
-                String formName = matcher.group(1);
-                String postURL = matcher.group(2);
-
-                PostMethod postmethod = getPostMethod(postURL);
-
-                postmethod.addParameter(formName + "_lg", pa.getUsername());
-                postmethod.addParameter(formName + "_psw", pa.getPassword());
-                postmethod.addParameter(formName + "_sbm", "Prihlasit");
-                postmethod.addParameter("DownloadRedirect", "");
-
-                if (makeRedirectedRequest(postmethod)) {
-                    matcher = getMatcherAgainstContent("<h1>P.ihl..en.</h1>|<h1>Prihl.si.</h1>|<h1>Bejelentkez.s</h1>");
-                    if (matcher.find()) {
-                        badConfig = true;
-                        throw new NotRecoverableDownloadException("Bad HellShare full account login information!");
-                    }
-                    GetMethod getMethod = getGetMethod(fileURL);
-                    if (!makeRedirectedRequest(getMethod))
-                        throw new PluginImplementationException();
-                }
             } else {
                 throw new PluginImplementationException("Bad login URL");
             }
@@ -148,7 +125,7 @@ class HellshareRunner extends AbstractRunner {
         }
         matcher = getMatcherAgainstContent("Na serveru jsou .* free download|Na serveri s. vyu.it. v.etky free download sloty|A szerveren az .sszes free download slot ki van haszn.lva");
         if (matcher.find()) {
-            throw new YouHaveToWaitException("Na serveru jsou využity všechny free download sloty", 30);
+            throw new YouHaveToWaitException("Na serveru jsou vyuï¿½ity vï¿½echny free download sloty", 30);
         }
         if (badConfig || getContentAsString().equals("")) {
             throw new NotRecoverableDownloadException("Bad HellShare full account login information!");

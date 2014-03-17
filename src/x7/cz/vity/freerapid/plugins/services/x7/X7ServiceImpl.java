06bf39c7b525d14df43bd0e1ac2e40c4a5b6a60a
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:30:21 +0530

    SVN Update# 2160 by wordrider: new plugin - x7.to

---
+++ src/x7/cz/vity/freerapid/plugins/services/x7/X7ServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.x7;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author ntoskrnl
+ */
+public class X7ServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "x7.to";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new X7FileRunner();
+    }
+
+}

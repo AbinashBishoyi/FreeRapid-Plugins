df5968134d6576830248fb13316a14f1abe1a50c
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 06:22:03 +0530

    SVN Update# 817 by Vity: speed optimization

--- src/megaupload/cz/vity/freerapid/plugins/services/megaupload/EditImage.java
+++ src/megaupload/cz/vity/freerapid/plugins/services/megaupload/EditImage.java
@@ -1,40 +1,38 @@
 package cz.vity.freerapid.plugins.services.megaupload;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.util.*;
-import java.util.logging.Logger;
-import java.io.File;
-import java.io.IOException;
 
 /**
  * @author Ludek Zika
  */
 
 class EditImage {
-    private final static Logger logger = Logger.getLogger(EditImage.class.getName());
-    BufferedImage in;
-    BufferedImage out;
+    //private final static Logger logger = Logger.getLogger(EditImage.class.getName());
+    private BufferedImage in;
+    private BufferedImage out;
 
     EditImage(BufferedImage in) {
         this.in = in;
         out = new BufferedImage(in.getWidth(), in.getHeight(), BufferedImage.TYPE_INT_RGB);
 
-    /*        File fo = new File("D:\\capgenIn.bmp");
-       try {
-           ImageIO.write(in, "bmp", fo);
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
-              */
+        /*        File fo = new File("D:\\capgenIn.bmp");
+ try {
+     ImageIO.write(in, "bmp", fo);
+ } catch (IOException e) {
+     e.printStackTrace();
+ }
+        */
     }
 
     BufferedImage separate() {
-        HashMap<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+        final HashMap<Integer, Integer> histogram = new HashMap<Integer, Integer>();
         int rgb;
         int count;
-        for (int x = 0; x < in.getWidth(); x++) {
-            for (int y = 0; y < in.getHeight(); y++) {
+        final int width = in.getWidth();
+        final int height = in.getHeight();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 out.setRGB(x, y, 0xFFFFFF);
                 rgb = in.getRGB(x, y);
                 if (histogram.get(rgb) == null) {
@@ -46,33 +44,32 @@ class EditImage {
             }
         }
         List<Integer> keys = new ArrayList<Integer>(histogram.keySet());
-        final Map<Integer, Integer> mapForComp = histogram;
         Collections.sort(keys,
                 new Comparator<Integer>() {
                     public int compare(Integer left, Integer right) {
-                        Integer leftValue = mapForComp.get(left);
-                        Integer rightValue = mapForComp.get(right);
+                        Integer leftValue = histogram.get(left);
+                        Integer rightValue = histogram.get(right);
 
                         return rightValue.compareTo(leftValue);
                     }
 
                 });
-        ArrayList<Integer> mostColor = new ArrayList<Integer>();
+        final ArrayList<Integer> mostColor = new ArrayList<Integer>();
 
         mostColor.add(keys.get(1));
         mostColor.add(keys.get(2));
         mostColor.add(keys.get(3));
 
-        HashMap<Integer, Integer> shiftMap = new HashMap<Integer, Integer>();
+        final HashMap<Integer, Integer> shiftMap = new HashMap<Integer, Integer>();
 
-        for (int x = 0; x < in.getWidth(); x++) {
-            for (int y = 0; y < in.getHeight(); y++) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 int color = in.getRGB(x, y);
                 if (mostColor.contains(color)) {
                     if (shiftMap.containsKey(color)) {
                         int shift = shiftMap.get(color);
                         int shiftX = x + shift;
-                        if (shiftX >= 0 && shiftX < in.getWidth()) out.setRGB(shiftX, y, color);
+                        if (shiftX >= 0 && shiftX < width) out.setRGB(shiftX, y, color);
 
                     } else shiftMap.put(color, countShift(shiftMap.size(), x));
 
@@ -84,7 +81,7 @@ class EditImage {
         return out;
     }
 
-    private Integer countShift(int size, int x) {
+    private static Integer countShift(int size, int x) {
         if (size == 0) {
             if (x < 5) return -x;
             return -5;

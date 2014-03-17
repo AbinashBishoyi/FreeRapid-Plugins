ac6657896d2a8c53c4160c35b0c1fa3110b60ce0
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 11:30:54 +0530

    SVN Update# 3021 by RickCL: Novo plugin Unibytes

---
+++ src/unibytes/cz/vity/freerapid/plugins/services/unibytes/UnibytesFileRunner.java
@@ -0,0 +1,145 @@
+package cz.vity.freerapid.plugins.services.unibytes;
+
+import java.awt.image.BufferedImage;
+import java.net.URLDecoder;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import cz.vity.freerapid.plugins.exceptions.CaptchaEntryInputMismatchException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.exceptions.YouHaveToWaitException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+
+/**
+ * Class which contains main code
+ *
+ * @author RickCL
+ */
+class UnibytesFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(UnibytesFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception {
+        super.runCheck();
+
+        HttpMethod httpMethod = getMethodBuilder()
+            .setAction(fileURL)
+            .toGetMethod();
+        if (!makeRedirectedRequest(httpMethod)) {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+
+        try {
+            //Matcher matcher = getMatcherAgainstContent("You are trying to download file:(?:<[^>]*>)*([^<]*)");
+            String content = getContentAsString();
+            String fileName = PlugUtils.getStringBetween(content, "You are trying to download file:", "</span><br/>").replaceAll("<[^>]*>", "");
+            String fileSize=null;// = PlugUtils.getStringBetween(content, "You are trying to download file:[^\\(]*\\(", "\\)", 1);
+            Matcher matcher = Pattern.compile("You are trying to download file:[^\n]*[^\\(]*\\(([^\\)]*)\\)", Pattern.MULTILINE).matcher(content);
+            if(matcher.find()) {
+                fileSize=matcher.group(1);
+            } else {
+                checkProblems();
+            }
+
+            final long lsize = PlugUtils.getFileSizeFromString( fileSize );
+
+            httpFile.setFileName( URLDecoder.decode(fileName,"UTF-8") );
+            httpFile.setFileSize( lsize );
+
+            httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+        } catch(PluginImplementationException e) {
+            checkProblems();
+        }
+
+    }
+
+    private void checkProblems() throws Exception {
+        if (getContentAsString().contains("File not found")) {
+            throw new URLNotAvailableAnymoreException("File not found or removed");
+        }
+        if (getContentAsString().contains("Somebody else is already downloading")) {
+            throw new ServiceConnectionProblemException("Somebody else is already downloading using your IP-address");
+        }
+
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+
+        HttpMethod httpMethod = getMethodBuilder()
+            .setAction(fileURL)
+            .toGetMethod();
+        System.out.println( httpMethod.getURI() );
+        if (!makeRedirectedRequest(httpMethod)) {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+
+        httpMethod = getMethodBuilder().setActionFromFormWhereTagContains("startForm", true).toPostMethod();
+        System.out.println( httpMethod.getURI() );
+        if (!makeRedirectedRequest(httpMethod)) {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+
+        checkProblems();
+
+        int waitTime = PlugUtils.getWaitTimeBetween(getContentAsString(), "var timerRest = ", ";", TimeUnit.SECONDS);
+        downloadTask.sleep(waitTime);
+
+        httpMethod = getMethodBuilder().setActionFromFormWhereTagContains("stepForm", true).toPostMethod();
+        System.out.println( httpMethod.getURI() );
+        if (!makeRedirectedRequest(httpMethod)) {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+
+        httpMethod = getMethodBuilder().setActionFromFormWhereTagContains("stepForm", true).toPostMethod();
+        System.out.println( httpMethod.getURI() );
+        if (!makeRedirectedRequest(httpMethod)) {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+
+        while( getContentAsString().contains("name=\"captcha\"") ) {
+            String captcha="http://www.unibytes.com/captcha.jpg";
+            final BufferedImage captchaImage = getCaptchaSupport().getCaptchaImage(captcha);
+            captcha = getCaptchaSupport().askForCaptcha(captchaImage);
+            if( captcha == null || captcha.isEmpty() ) {
+                throw new CaptchaEntryInputMismatchException("Can't be null");
+            }
+
+            System.out.println( getMethodBuilder().setActionFromFormWhereTagContains("stepForm", true).getParameters() );
+
+            httpMethod = getMethodBuilder().setActionFromFormWhereTagContains("stepForm", true)
+                .removeParameter("captcha")
+                .setParameter("captcha", captcha)
+                .toPostMethod();
+            System.out.println( httpMethod.getURI() );
+            if (!makeRedirectedRequest(httpMethod)) {
+                checkProblems();
+                throw new ServiceConnectionProblemException();
+            }
+        }
+        httpMethod = getMethodBuilder().setActionFromTextBetween("<div style=\"width: 650px; margin: 40px auto; text-align: center; font-size: 2em;\"><a href=\"", "\">Download</a>").toGetMethod();
+        System.out.println( httpMethod.getURI() );
+        if (!tryDownloadAndSaveFile(httpMethod)) {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+
+   }
+
+}
---
+++ src/unibytes/cz/vity/freerapid/plugins/services/unibytes/UnibytesServiceImpl.java
@@ -0,0 +1,33 @@
+package cz.vity.freerapid.plugins.services.unibytes;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author RickCL
+ */
+public class UnibytesServiceImpl extends AbstractFileShareService {
+
+    @Override
+    public String getName() {
+        return "unibytes.com";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new UnibytesFileRunner();
+    }
+
+}

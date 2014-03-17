0790df8bce15ab1d9f3f0f5461fd51710e06c08f
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 12:12:26 +0530

    SVN Update# 3396 by tong2shot: fixing to suit current state of site, ajax request+response

--- src/tube8/cz/vity/freerapid/plugins/services/tube8/Tube8FileRunner.java
+++ src/tube8/cz/vity/freerapid/plugins/services/tube8/Tube8FileRunner.java
@@ -5,6 +5,7 @@ import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
 import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.MethodBuilder;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -33,8 +34,7 @@ class Tube8FileRunner extends AbstractRunner {
     }
 
     private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
-        PlugUtils.checkName(httpFile, content, "class=\"main-title main-sprite-img\">", "</");
-        //PlugUtils.checkFileSize(httpFile, content, "class=\"btn-01 main-sprite-img relative\">[^<]?<span.*?<span class=\"video-data absolute\">(", ")</span>");
+        PlugUtils.checkName(httpFile, content, "var videotitle=\"", "\";");
         //remove old extension if it exists
         String name = httpFile.getFileName();
         final int pos = name.lastIndexOf('.');
@@ -49,17 +49,39 @@ class Tube8FileRunner extends AbstractRunner {
         logger.info("Starting download in TASK " + fileURL);
         final GetMethod method = getGetMethod(fileURL); //create GET request
         if (makeRedirectedRequest(method)) { //we make the main request
-            final String contentAsString = getContentAsString();//check for response
+            String contentAsString = getContentAsString();//check for response
             checkProblems();//check problems
             checkNameAndSize(contentAsString);//extract file name and size from the page
+
             client.getHTTPClient().getParams().setParameter("dontUseHeaderFilename", true);
-            final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setActionFromTextBetween("videourl=\"", "\"").toGetMethod();
-            // alternate:
-            // final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setActionFromTextBetween("flashvars.video_url = '", "'").toGetMethod();
-            if (!tryDownloadAndSaveFile(httpMethod)) {
-                checkProblems();//if downloading failed
-                throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+            client.getHTTPClient().getParams().setParameter("X-Requested-With", "XMLHttpRequest");
+
+            final String hash = PlugUtils.getStringBetween(contentAsString, "var hash = \"", "\";");
+            final String videoId = PlugUtils.getStringBetween(contentAsString, "var videoId = ", ";");
+            final MethodBuilder ajaxMethodBuilder = getMethodBuilder()
+                    .setAction("http://www.tube8.com/ajax/getVideoDownloadURL.php")
+                    .setParameter("hash", hash)
+                    .setParameter("video", videoId);
+            final HttpMethod ajaxMethod = ajaxMethodBuilder.toGetMethod();
+
+            logger.info("making ajax request..");
+            if (makeRequest(ajaxMethod)) {
+                contentAsString = getContentAsString();
+                logger.info("Ajax response : " + contentAsString);
+                //there are 2 options for video to download : standard and mobile
+                //we choose standard
+                final String videoURL = PlugUtils.getStringBetween(contentAsString, "\"standard_url\":\"", "\",").replace("\\", "");
+                final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setAction(videoURL).toGetMethod();
+
+                if (!tryDownloadAndSaveFile(httpMethod)) {
+                    checkProblems();//if downloading failed
+                    throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+                }
+            } else {
+                checkProblems();
+                throw new ServiceConnectionProblemException();
             }
+
         } else {
             checkProblems();
             throw new ServiceConnectionProblemException();
@@ -67,9 +89,16 @@ class Tube8FileRunner extends AbstractRunner {
     }
 
     private void checkProblems() throws ErrorDuringDownloadingException {
-        if (getContentAsString().contains(">Newest Videos<")) {
+        final String contentAsString = getContentAsString();
+
+        if (contentAsString.contains(">Newest Videos<")) {
+            throw new URLNotAvailableAnymoreException("File not found");
+        } else if (contentAsString.contains("This video is deleted")) {
+            throw new URLNotAvailableAnymoreException("This video is deleted");
+        } else if (!PlugUtils.matcher("var videoId", contentAsString).find()) {
             throw new URLNotAvailableAnymoreException("File not found");
         }
+
     }
 
 }

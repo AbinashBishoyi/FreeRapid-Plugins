f019578fd60c7ba8383ac763593d4950c1368644
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:14:01 +0530

    SVN Update# 2020 by Vity: novaup

---
+++ src/novaup/cz/vity/freerapid/plugins/services/novaup/NovaupServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.novaup;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author Vity
+ */
+public class NovaupServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "novaup.com";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new NovaupFileRunner();
+    }
+
+}

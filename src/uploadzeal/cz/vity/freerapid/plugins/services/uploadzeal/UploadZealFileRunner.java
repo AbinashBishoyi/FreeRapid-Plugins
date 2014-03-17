d73941cc1f7a0a2258f6f6f0071f5a31ee4fe563
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 14:21:26 +0530

    SVN Update# 4287 by birchie: New XFS Plugin : UploadZeal.com

---
+++ src/uploadzeal/cz/vity/freerapid/plugins/services/uploadzeal/UploadZealFileRunner.java
@@ -0,0 +1,11 @@
+package cz.vity.freerapid.plugins.services.uploadzeal;
+
+import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+
+/**
+ * Class which contains main code
+ *
+ * @author birchie
+ */
+class UploadZealFileRunner extends XFileSharingRunner {
+}

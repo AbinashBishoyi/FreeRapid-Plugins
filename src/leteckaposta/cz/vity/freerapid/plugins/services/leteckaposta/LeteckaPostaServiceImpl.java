817032398e7b5bb5d148ac7d668ac5dbba95d858
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:06:40 +0530

    SVN Update# 1366 by Vity: letecka posta

---
+++ src/leteckaposta/cz/vity/freerapid/plugins/services/leteckaposta/LeteckaPostaServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.leteckaposta;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Ladislav Vitasek
+ */
+public class LeteckaPostaServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "leteckaposta.cz";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 3;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new LeteckaPostaFileRunner();
+    }
+
+}

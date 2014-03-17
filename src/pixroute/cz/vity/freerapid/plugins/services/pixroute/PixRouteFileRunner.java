dd7f691976a55693fbb26cc0c6f28d107a134511
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 12:10:58 +0530

    SVN Update# 3386 by tong2shot:

---
+++ src/pixroute/cz/vity/freerapid/plugins/services/pixroute/PixRouteFileRunner.java
@@ -0,0 +1,79 @@
+package cz.vity.freerapid.plugins.services.pixroute;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.util.logging.Logger;
+import java.net.URI;
+import static java.net.URLEncoder.encode;
+
+/**
+ * Class which contains main code
+ *
+ * @author tong2shot
+ */
+class PixRouteFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(PixRouteFileRunner.class.getName());
+
+
+
+    /*
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+    */
+    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
+        PlugUtils.checkName(httpFile, content, "<td nowrap>", "</");
+        PlugUtils.checkFileSize(httpFile, content, "<td>", "<small>");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
+        if (makeRedirectedRequest(method)) { //we make the main request
+            final String contentAsString = getContentAsString();//check for response
+            checkProblems();//check problems
+            checkNameAndSize(contentAsString);//extract file name and size from the page
+            final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setActionFromImgSrcWhereTagContains("ujhz1oo").toGetMethod();
+
+            //here is the download link extraction
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkProblems();//if downloading failed
+                logger.warning(getContentAsString());//log the info
+                throw new PluginImplementationException();//some unknown problem
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("No such file with this filename")) {
+            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+    }
+
+}

ec0e4d3acd890ad4e2c90fab73b8dfd6347b0950
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:04:09 +0530

    SVN Update# 1938 by Lukiz: MU update

--- src/megaupload/cz/vity/freerapid/plugins/services/megaupload/MegauploadRunner.java
+++ src/megaupload/cz/vity/freerapid/plugins/services/megaupload/MegauploadRunner.java
@@ -74,7 +74,9 @@ class MegauploadRunner extends AbstractRunner {
                     httpFile.setFileName(PlugUtils.unescapeHtml(toEncode));
                 }
                 downloadURL = encodeURL(downloadURL);
-                final GetMethod method = getGetMethod(downloadURL);
+                 final HttpMethod method = getMethodBuilder().setAction(downloadURL).setReferer(httpFile.getFileUrl().toString()).toHttpMethod();
+               // final GetMethod method = getGetMethod(downloadURL);
+                 downloadTask.sleep(45);
                 if (!tryDownloadAndSaveFile(method)) {
                     checkProblems();
                     logger.warning(getContentAsString());

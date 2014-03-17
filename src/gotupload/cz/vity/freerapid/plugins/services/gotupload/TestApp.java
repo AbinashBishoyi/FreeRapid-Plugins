45cb3fa50a87976972887fe64544ae4972fac25f
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:27:58 +0530

    SVN Update# 2138 by Javi: Gotupload.com plugin

---
+++ src/gotupload/cz/vity/freerapid/plugins/services/gotupload/TestApp.java
@@ -0,0 +1,43 @@
+package cz.vity.freerapid.plugins.services.gotupload;
+
+import cz.vity.freerapid.plugins.dev.PluginDevApplication;
+import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import org.jdesktop.application.Application;
+
+import java.net.URL;
+
+/**
+ * @author Javi
+ */
+public class TestApp extends PluginDevApplication {
+    @Override
+    protected void startup() {
+        final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
+        try {
+            //we set file URL
+            httpFile.setNewURL(new URL("http://www.gotupload.com/z9a0jn5jor43/Mx_Vs_Atv_Reflex_XBOX360-SPARE.sfv"));
+            //the way we connect to the internet
+            final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
+            //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
+            //then we tries to download
+            final GotUploadServiceImpl service = new GotUploadServiceImpl(); //instance of service - of our plugin
+            //runcheck makes the validation
+            testRun(service, httpFile, connectionSettings);//download file with service and its Runner
+            //all output goes to the console
+        } catch (Exception e) {//catch possible exception
+            e.printStackTrace(); //writes error output - stack trace to console
+        }
+        this.exit();//exit application
+    }
+
+    /**
+     * Main start method for running this application
+     * Called from IDE
+     *
+     * @param args arguments for application
+     */
+    public static void main(String[] args) {
+        Application.launch(TestApp.class, args);//starts the application - calls startup() internally
+    }
+}

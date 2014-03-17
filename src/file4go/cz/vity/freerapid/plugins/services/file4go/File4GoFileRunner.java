2299ed06b9eea9c66d0d81bba6f653e32ffdf327
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:49:12 +0530

    SVN Update# 3986 by birchie:  New Plugin : File4Go.com      New XFS Plugin : Fsakura.com

---
+++ src/file4go/cz/vity/freerapid/plugins/services/file4go/File4GoFileRunner.java
@@ -0,0 +1,68 @@
+package cz.vity.freerapid.plugins.services.file4go;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.util.logging.Logger;
+
+/**
+ * Class which contains main code
+ *
+ * @author birchie
+ */
+class File4GoFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(File4GoFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
+        PlugUtils.checkName(httpFile, content, "<span class=\"name\"><b>Nome do arquivo:</b>", "</span>");
+        PlugUtils.checkFileSize(httpFile, content, "<span class=\"size\"><b>Tamanho:</b>", "</span>");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
+        if (makeRedirectedRequest(method)) { //we make the main request
+            final String contentAsString = getContentAsString();//check for response
+            checkProblems();//check problems
+            checkNameAndSize(contentAsString);//extract file name and size from the page
+            final HttpMethod httpMethod = getGetMethod(PlugUtils.getStringBetween(getContentAsString(), "window.location.href='", "'\">Download</button>"));
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkProblems();//if downloading failed
+                throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("Not Found") || contentAsString.contains("Arquivo Temporariamente Indisponivel")) {
+            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+    }
+
+}
---
+++ src/file4go/cz/vity/freerapid/plugins/services/file4go/File4GoServiceImpl.java
@@ -0,0 +1,28 @@
+package cz.vity.freerapid.plugins.services.file4go;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author birchie
+ */
+public class File4GoServiceImpl extends AbstractFileShareService {
+
+    @Override
+    public String getName() {
+        return "file4go.com";
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new File4GoFileRunner();
+    }
+
+}

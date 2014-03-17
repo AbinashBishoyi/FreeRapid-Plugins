17febcd33fb594019716a50713b2d468021e618e
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 12:48:51 +0530

    SVN Update# 3531 by ntoskrnl: new plugin - Uploaded.to_premium

---
+++ src/uploadedto_premium/cz/vity/freerapid/plugins/services/uploadedto_premium/UploadedtoFileRunner.java
@@ -0,0 +1,101 @@
+package cz.vity.freerapid.plugins.services.uploadedto_premium;
+
+import cz.vity.freerapid.plugins.exceptions.*;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.Cookie;
+import org.apache.commons.httpclient.HttpMethod;
+
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+
+/**
+ * Class which contains main code
+ *
+ * @author ntoskrnl
+ */
+class UploadedtoFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(UploadedtoFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception {
+        super.runCheck();
+        addCookie(new Cookie(".uploaded.to", "lang", "en", "/", 86400, false));
+        addCookie(new Cookie(".ul.to", "lang", "en", "/", 86400, false));
+        final HttpMethod method = getGetMethod(fileURL);
+        if (makeRedirectedRequest(method)) {
+            checkProblems();
+            checkNameAndSize();
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkNameAndSize() throws ErrorDuringDownloadingException {
+        final Matcher matcher = getMatcherAgainstContent("<title>(.+?) \\((.+?)\\) \\- uploaded\\.to</title>");
+        if (!matcher.find()) {
+            throw new PluginImplementationException("File name/size not found");
+        }
+        httpFile.setFileName(matcher.group(1));
+        httpFile.setFileSize(PlugUtils.getFileSizeFromString(matcher.group(2)));
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        addCookie(new Cookie(".uploaded.to", "lang", "en", "/", 86400, false));
+        addCookie(new Cookie(".ul.to", "lang", "en", "/", 86400, false));
+        login();
+        HttpMethod method = getGetMethod(fileURL);
+        if (!tryDownloadAndSaveFile(method)) {
+            checkProblems();
+            checkNameAndSize();
+            method = getMethodBuilder().setActionFromFormWhereTagContains("Premium Download", true).toGetMethod();
+            if (!tryDownloadAndSaveFile(method)) {
+                checkProblems();
+                throw new ServiceConnectionProblemException("Error starting download");
+            }
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        if (getContentAsString().contains("Page not found") || getContentAsString().contains("The requested file isn't available anymore")) {
+            throw new URLNotAvailableAnymoreException("File not found");
+        }
+        if (getContentAsString().contains("Our service is currently unavailable in your country")) {
+            throw new NotRecoverableDownloadException("Our service is currently unavailable in your country");
+        }
+    }
+
+    private void login() throws Exception {
+        synchronized (UploadedtoFileRunner.class) {
+            UploadedtoServiceImpl service = (UploadedtoServiceImpl) getPluginService();
+            PremiumAccount pa = service.getConfig();
+            if (!pa.isSet()) {
+                pa = service.showConfigDialog();
+                if (pa == null || !pa.isSet()) {
+                    throw new BadLoginException("No Uploaded account login information!");
+                }
+            }
+            final HttpMethod method = getMethodBuilder()
+                    .setAction("http://uploaded.to/io/login")
+                    .setParameter("id", pa.getUsername())
+                    .setParameter("pw", pa.getPassword())
+                    .toPostMethod();
+            method.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            setFileStreamContentTypes(new String[0], new String[]{"application/javascript"});
+            if (!makeRedirectedRequest(method)) {
+                throw new ServiceConnectionProblemException("Error posting login info");
+            }
+            if (getContentAsString().contains("err")) {
+                throw new BadLoginException("Invalid Uploaded account login information!");
+            }
+        }
+    }
+
+}
---
+++ src/uploadedto_premium/cz/vity/freerapid/plugins/services/uploadedto_premium/UploadedtoServiceImpl.java
@@ -0,0 +1,54 @@
+package cz.vity.freerapid.plugins.services.uploadedto_premium;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author ntoskrnl
+ */
+public class UploadedtoServiceImpl extends AbstractFileShareService {
+    private static final String PLUGIN_CONFIG_FILE = "plugin_UploadedPremium.xml";
+    private volatile PremiumAccount config;
+
+    @Override
+    public String getName() {
+        return "uploaded.to_premium";
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new UploadedtoFileRunner();
+    }
+
+    @Override
+    public void showOptions() throws Exception {
+        PremiumAccount pa = showConfigDialog();
+        if (pa != null) config = pa;
+    }
+
+    PremiumAccount showConfigDialog() throws Exception {
+        return showAccountDialog(getConfig(), "Uploaded", PLUGIN_CONFIG_FILE);
+    }
+
+    PremiumAccount getConfig() throws Exception {
+        synchronized (UploadedtoServiceImpl.class) {
+            if (config == null) {
+                config = getAccountConfigFromFile(PLUGIN_CONFIG_FILE);
+            }
+        }
+        return config;
+    }
+
+    void setConfig(final PremiumAccount config) {
+        this.config = config;
+    }
+
+}

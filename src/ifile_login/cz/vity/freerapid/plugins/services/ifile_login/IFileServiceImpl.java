c202f3da25c51ebce61882e802e353fbe0c88d7d
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:07:55 +0530

    SVN Update# 1981 by JPEXS: Added IFile.it plugin with login support version 0.7.2

---
+++ src/ifile_login/cz/vity/freerapid/plugins/services/ifile_login/IFileServiceImpl.java
@@ -0,0 +1,54 @@
+package cz.vity.freerapid.plugins.services.ifile_login;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author JPEXS
+ */
+public class IFileServiceImpl extends AbstractFileShareService {
+    private final static String SERVICE_NAME = "ifile.it (login)";
+    private static final String PLUGIN_CONFIG_FILE = "plugin_IFileItLogin.xml";
+
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 2;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new IFileFileRunner();
+    }
+
+    @Override
+    public void showOptions() throws Exception {
+        PremiumAccount pa = showConfigDialog();
+        if (pa != null) config = pa;
+    }
+
+    public PremiumAccount showConfigDialog() throws Exception {
+        return showAccountDialog(getConfig(), "IFile", PLUGIN_CONFIG_FILE);
+    }
+
+    PremiumAccount getConfig() throws Exception {
+        if (config == null) {
+            synchronized (IFileServiceImpl.class) {
+                config = getAccountConfigFromFile(PLUGIN_CONFIG_FILE);
+            }
+        }
+
+        return config;
+    }
+
+    private volatile PremiumAccount config;
+}

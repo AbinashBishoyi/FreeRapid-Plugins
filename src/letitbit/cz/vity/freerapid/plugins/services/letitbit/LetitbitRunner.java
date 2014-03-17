dd5e9e9127ff18c330b0c053680429724ea0e4d6
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:09:50 +0530

    SVN Update# 1396 by Lukiz: update letitbit

--- src/letitbit/cz/vity/freerapid/plugins/services/letitbit/LetitbitRunner.java
+++ src/letitbit/cz/vity/freerapid/plugins/services/letitbit/LetitbitRunner.java
@@ -8,6 +8,7 @@ import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -61,52 +62,79 @@ class LetitbitRunner extends AbstractRunner {
         getMethod.setFollowRedirects(true);
         if (makeRequest(getMethod)) {
             checkNameAndSize(getContentAsString());
-            Matcher matcher = Pattern.compile("form action=\"([^\"]*download[^\"]*)\"(.*)</form>", Pattern.MULTILINE | Pattern.DOTALL).matcher(getContentAsString());
-            if (!matcher.find()) {
-                checkProblems();
-                logger.warning(getContentAsString());
-                throw new InvalidURLOrServiceProblemException("Free download link not found");
-            }
-            String s = matcher.group(1);
-            String form = matcher.group(2);
+            int captchas = 0;
+            while (captchas++ < 5) {
+                Matcher matcher = Pattern.compile("form action=\"([^\"]*download[^\"]*)\"(.*)</form>", Pattern.MULTILINE | Pattern.DOTALL).matcher(getContentAsString());
+                if (!matcher.find()) {
+                    checkProblems();
+                    logger.warning(getContentAsString());
+                    throw new InvalidURLOrServiceProblemException("Free download link not found");
+                }
+                String s = matcher.group(1);
+                String form = matcher.group(2);
+                logger.info("Submit form to - " + s);
+                client.setReferer(fileURL);
+                final PostMethod postMethod = getPostMethod(s);
+                PlugUtils.addParameters(postMethod, form, new String[]{"uid", "frameset", "uid2"});
+                postMethod.addParameter("cap", readCaptchaImage());
+                postMethod.addParameter("fix", "1");
 
-            String uid = PlugUtils.getParameter("uid", form);
-            String frameset = PlugUtils.getParameter("frameset", form);
-
-            logger.info("Submit form to - " + s);
-            client.setReferer(fileURL);
-            final PostMethod postMethod = getPostMethod(s);
-
-            postMethod.addParameter("uid", uid);
-            postMethod.addParameter("frameset", frameset);
-            postMethod.addParameter("fix", "1");
-
-            if (!makeRequest(postMethod)) {
-                logger.info(getContentAsString());
-                throw new PluginImplementationException("Problem with a connection to service.\nCannot find requested page content");
-            }
-            matcher = PlugUtils.matcher("src=\"([^?]*)\\?link=([^\"]*)\"", getContentAsString());
-            if (matcher.find()) {
-                String t = matcher.group(2);
-                logger.info("Download URL: " + t);
-                downloadTask.sleep(4);
-                httpFile.setState(DownloadState.GETTING);
-                client.setReferer(matcher.group(1) + "?link=" + t);
-                final GetMethod method = getGetMethod(t);
-                method.setFollowRedirects(true);
-                if (!tryDownloadAndSaveFile(method)) {
+                if (!makeRequest(postMethod)) {
+                    logger.info(getContentAsString());
+                    throw new PluginImplementationException("Problem with a connection to service.\nCannot find requested page content");
+                }
+                matcher = PlugUtils.matcher("src=\"([^?]*)\\?link=([^\"]*)\"", getContentAsString());
+                if (matcher.find()) {
+                    String t = matcher.group(2);
+                    logger.info("Download URL: " + t);
+                    downloadTask.sleep(4);
+                    logger.info(getContentAsString());
+                    httpFile.setState(DownloadState.GETTING);
+                    client.setReferer(matcher.group(1) + "?link=" + t);
+                    final GetMethod method = getGetMethod(t);
+                    method.setFollowRedirects(true);
+                    if (!tryDownloadAndSaveFile(method)) {
+                        checkProblems();
+                        logger.info(getContentAsString());
+                        throw new IOException("File input stream is empty.");
+                    } else {
+                        break;
+                    }
+                } else {
+                    if (getContentAsString().contains("javascript:history.go(-1);")) {
+                        logger.info("Wrong captcha");
+                        if (captchas == 3) throw new PluginImplementationException("Too many captcha attempts");
+                        continue;
+                    }
                     checkProblems();
                     logger.info(getContentAsString());
-                    throw new IOException("File input stream is empty.");
+                    throw new PluginImplementationException("Problem with a connection to service.\nCannot find requested page content");
                 }
-            } else {
-                checkProblems();
-                logger.info(getContentAsString());
-                throw new PluginImplementationException("Problem with a connection to service.\nCannot find requested page content");
             }
-
         } else
             throw new PluginImplementationException("Problem with a connection to service.\nCannot find requested page content");
+    }
+
+    private String readCaptchaImage() throws Exception {
+        Matcher matcher = getMatcherAgainstContent("src='(.*?/cap.php[^']*)'");
+        if (matcher.find()) {
+            String s = PlugUtils.replaceEntities(matcher.group(1));
+            logger.info("Captcha - image " + s);
+            String captcha;
+            final BufferedImage captchaImage = getCaptchaSupport().getCaptchaImage(s);
+            final BufferedImage croppedCaptchaImage = captchaImage.getSubimage(1, 1, captchaImage.getWidth() - 2, captchaImage.getHeight() - 2);
+            captcha = PlugUtils.recognize(croppedCaptchaImage, "-C A-z-0-9");
+            if (captcha != null) {
+                logger.info("Captcha - OCR recognized " + captcha);
+                //    matcher = PlugUtils.matcher("[A-Z-a-z-0-9]{6}", captcha);
+                //   if (!matcher.find()) {
+                //       captcha = null;
+                //   }
+            } else captcha = "";
+            captchaImage.flush();//askForCaptcha uvolnuje ten obrazek, takze tady to udelame rucne
+            return captcha;
+        }
+        return "";
     }
 
     private void checkProblems() throws ServiceConnectionProblemException, YouHaveToWaitException, URLNotAvailableAnymoreException {

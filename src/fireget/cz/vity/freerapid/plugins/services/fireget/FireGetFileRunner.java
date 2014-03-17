bc6d25d4d27f786e195f5b0a3ce84059d7fb15c5
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 15:02:59 +0530

    SVN Update# 4614 by birchie: Fixed:  FireGet.com

--- src/fireget/cz/vity/freerapid/plugins/services/fireget/FireGetFileRunner.java
+++ src/fireget/cz/vity/freerapid/plugins/services/fireget/FireGetFileRunner.java
@@ -1,6 +1,12 @@
 package cz.vity.freerapid.plugins.services.fireget;
 
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
 import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileNameHandler;
+import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileSizeHandler;
+
+import java.util.List;
 
 /**
  * Class which contains main code
@@ -8,4 +14,27 @@ import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
  * @author birchie
  */
 class FireGetFileRunner extends XFileSharingRunner {
+
+    @Override
+    protected List<FileNameHandler> getFileNameHandlers() {
+        final List<FileNameHandler> fileNameHandlers = super.getFileNameHandlers();
+        fileNameHandlers.add(new FireGetFileNameHandler());   // 4 premium downloads
+        return fileNameHandlers;
+    }
+
+    @Override
+    protected List<FileSizeHandler> getFileSizeHandlers() {
+        final List<FileSizeHandler> fileSizeHandlers = super.getFileSizeHandlers();
+        fileSizeHandlers.add(new FireGetFileSizeHandler());   // 4 premium downloads
+        return fileSizeHandlers;
+    }
+
+    @Override
+    protected void checkDownloadProblems() throws ErrorDuringDownloadingException {
+        final String content = getContentAsString();
+        if (content.contains("Due the high load there're no free uploading slots for this file at the moment")) {
+            throw new ServiceConnectionProblemException("Due the high load the file has no free slots, wait or try premium");
+        }
+        super.checkDownloadProblems();
+    }
 }

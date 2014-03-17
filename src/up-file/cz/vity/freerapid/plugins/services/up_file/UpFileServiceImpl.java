42f98bb437a49ebc0e07443f4e9cc36812f9e217
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:05:20 +0530

    SVN Update# 1950 by wordrider: New plugin up-file.com

---
+++ src/up-file/cz/vity/freerapid/plugins/services/up_file/UpFileServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.up_file;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ * @author Thumb
+ */
+public class UpFileServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "up-file.com";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new UpFileFileRunner();
+    }
+
+}

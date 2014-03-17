075116e08a458b1f87478e9180c64d9e8145fbb4
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:23:58 +0530

    SVN Update# 3789 by birchie: Upload.ee - new plugin FileDefend.com - new XFS plugin

---
+++ src/filedefend/cz/vity/freerapid/plugins/services/filedefend/FileDefendFileRunner.java
@@ -0,0 +1,11 @@
+package cz.vity.freerapid.plugins.services.filedefend;
+
+import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+
+/**
+ * Class which contains main code
+ *
+ * @author birchie
+ */
+class FileDefendFileRunner extends XFileSharingRunner {
+}
---
+++ src/uploadee/cz/vity/freerapid/plugins/services/uploadee/UploadEEFileRunner.java
@@ -0,0 +1,69 @@
+package cz.vity.freerapid.plugins.services.uploadee;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.util.logging.Logger;
+
+/**
+ * Class which contains main code
+ *
+ * @author birchie
+ */
+class UploadEEFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(UploadEEFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
+        PlugUtils.checkName(httpFile, content, "File: <b>", "</b>");
+        PlugUtils.checkFileSize(httpFile, content, "Size: ", "<br");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
+        if (makeRedirectedRequest(method)) { //we make the main request
+            final String contentAsString = getContentAsString();//check for response
+            checkProblems();//check problems
+            checkNameAndSize(contentAsString);//extract file name and size from the page
+
+            final HttpMethod httpMethod = getGetMethod(PlugUtils.getStringBetween(contentAsString, "id=\"d_l\" href=\"", "\""));
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkProblems();
+                throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("There is no such file")) {
+            throw new URLNotAvailableAnymoreException("File not found");
+        }
+    }
+
+}

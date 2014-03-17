6788d4af957dd6fcf563d15fda9d20f59b930833
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:36:03 +0530

    SVN Update# 2210 by RickCL: Implemented support to Secured.in

---
+++ src/securedin/cz/vity/freerapid/plugins/services/securedin/SecuredinServiceImpl.java
@@ -0,0 +1,32 @@
+package cz.vity.freerapid.plugins.services.securedin;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author RickCL
+ */
+public class SecuredinServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "secured.in";
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new SecuredinRunner();
+    }
+
+    @Override
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return false;
+    }
+
+}

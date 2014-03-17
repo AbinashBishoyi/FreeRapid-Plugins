946d243d1919cb93be6649bcf49f90d200d1e51f
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:04:16 +0530

    SVN Update# 1939 by Arthur: files.fm plugins

---
+++ src/files/cz/vity/freerapid/plugins/services/files/FilesServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.files;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ * @author Arthur Gunawan
+ */
+public class FilesServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "files.fm";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new FilesFileRunner();
+    }
+
+}

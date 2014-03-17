43b31095d167c2118d5da5947230c19829478c1f
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 09:07:59 +0530

    SVN Update# 2486 by ntoskrnl: new plugin - DepositFiles.com_premium

---
+++ src/depositfiles_premium/cz/vity/freerapid/plugins/services/depositfiles_premium/DepositFilesServiceImpl.java
@@ -0,0 +1,56 @@
+package cz.vity.freerapid.plugins.services.depositfiles_premium;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author ntoskrnl
+ */
+public class DepositFilesServiceImpl extends AbstractFileShareService {
+    private static final String PLUGIN_CONFIG_FILE = "plugin_DepositFilesPremium.xml";
+    private volatile PremiumAccount config;
+
+    @Override
+    public String getName() {
+        return "depositfiles.com_premium";
+    }
+
+    @Override
+    public int getMaxDownloadsFromOneIP() {
+        return 100;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new DepositFilesFileRunner();
+    }
+
+    @Override
+    public void showOptions() throws Exception {
+        PremiumAccount pa = showConfigDialog();
+        if (pa != null) config = pa;
+    }
+
+    public PremiumAccount showConfigDialog() throws Exception {
+        return showAccountDialog(getConfig(), "DepositFiles", PLUGIN_CONFIG_FILE);
+    }
+
+    PremiumAccount getConfig() throws Exception {
+        if (config == null) {
+            synchronized (DepositFilesServiceImpl.class) {
+                config = getAccountConfigFromFile(PLUGIN_CONFIG_FILE);
+            }
+        }
+
+        return config;
+    }
+
+}

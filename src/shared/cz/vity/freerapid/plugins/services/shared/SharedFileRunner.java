4efb94f97f5e34310b7322f3273d4fa198e2d2fc
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 15:40:48 +0530

    SVN Update# 4861 by birchie: Updated:  Shared.com  - folder support added

--- src/shared/cz/vity/freerapid/plugins/services/shared/SharedFileRunner.java
+++ src/shared/cz/vity/freerapid/plugins/services/shared/SharedFileRunner.java
@@ -1,15 +1,21 @@
 package cz.vity.freerapid.plugins.services.shared;
 
 import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
 import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
 import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.DownloadState;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 
+import java.net.URI;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
 
 /**
  * Class which contains main code
@@ -34,7 +40,12 @@ class SharedFileRunner extends AbstractRunner {
 
     private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
         PlugUtils.checkName(httpFile, content, "<title>", " - Shared</title>");
-        PlugUtils.checkFileSize(httpFile, content, "div class=\"attachment-size\">", "</div>");
+        if (fileURL.contains("/f/")) {  // folder
+            httpFile.setFileName("Folder: " + httpFile.getFileName());
+            PlugUtils.checkFileSize(httpFile, content, "total_entries: ", "}");
+        } else {
+            PlugUtils.checkFileSize(httpFile, content, "div class=\"attachment-size\">", "</div>");
+        }
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
@@ -44,14 +55,27 @@ class SharedFileRunner extends AbstractRunner {
         logger.info("Starting download in TASK " + fileURL);
         final GetMethod method = getGetMethod(fileURL); //create GET request
         if (makeRedirectedRequest(method)) { //we make the main request
-            final String contentAsString = getContentAsString();//check for response
-            checkProblems();//check problems
-            checkNameAndSize(contentAsString);//extract file name and size from the page
+            if (fileURL.contains("/f/")) {  // folder
+                final List<URI> list = new LinkedList<URI>();
+                final Matcher match = PlugUtils.matcher("dl_url_direct\":\"(.+?)\",", getContentAsString());
+                while (match.find()) {
+                    list.add(new URI(match.group(1)));
+                }
+                if (list.isEmpty()) throw new PluginImplementationException("No links found");
+                getPluginService().getPluginContext().getQueueSupport().addLinksToQueue(httpFile, list);
+                httpFile.setFileName("Link(s) Extracted !");
+                httpFile.setState(DownloadState.COMPLETED);
+                httpFile.getProperties().put("removeCompleted", true);
+            } else {
+                final String contentAsString = getContentAsString();//check for response
+                checkProblems();//check problems
+                checkNameAndSize(contentAsString);//extract file name and size from the page
 
-            final HttpMethod httpMethod = getGetMethod(PlugUtils.getStringBetween(contentAsString, "href=\"", "\" id=\"download"));
-            if (!tryDownloadAndSaveFile(httpMethod)) {
-                checkProblems();//if downloading failed
-                throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+                final HttpMethod httpMethod = getGetMethod(PlugUtils.getStringBetween(contentAsString, "href=\"", "\" id=\"download"));
+                if (!tryDownloadAndSaveFile(httpMethod)) {
+                    checkProblems();//if downloading failed
+                    throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+                }
             }
         } else {
             checkProblems();

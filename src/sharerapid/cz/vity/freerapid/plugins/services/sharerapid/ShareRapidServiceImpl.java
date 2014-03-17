1ed5c3b30318d8958adb9a618f665f4eee6695ae
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:13:29 +0530

    SVN Update# 1428 by wordrider: ShareRapid plugin

---
+++ src/sharerapid/cz/vity/freerapid/plugins/services/sharerapid/ShareRapidServiceImpl.java
@@ -0,0 +1,53 @@
+package cz.vity.freerapid.plugins.services.sharerapid;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Ladislav Vitasek, Ludek Zika
+ */
+public class ShareRapidServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "share-rapid.com";
+    private static final String PLUGIN_CONFIG_FILE = "plugin_ShareRapid.xml";
+    private volatile PremiumAccount config;
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 3;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new ShareRapidRunner();
+    }
+
+    @Override
+    public void showOptions() throws Exception {
+        PremiumAccount pa = showConfigDialog();
+        if (pa != null) config = pa;
+    }
+
+    public PremiumAccount showConfigDialog() throws Exception {
+        return showAccountDialog(getConfig(), "CZshare", PLUGIN_CONFIG_FILE);
+    }
+
+    PremiumAccount getConfig() throws Exception {
+        if (config == null) {
+            synchronized (ShareRapidServiceImpl.class) {
+                config = getAccountConfigFromFile(PLUGIN_CONFIG_FILE);
+            }
+        }
+
+        return config;
+    }
+
+}

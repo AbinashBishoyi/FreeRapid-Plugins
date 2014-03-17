f01907c6fce8062e14e68aec3b3d65328aae6d9a
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 15:22:36 +0530

    SVN Update# 4750 by ntoskrnl: new plugin - AnonFiles.com

---
+++ src/anonfiles/cz/vity/freerapid/plugins/services/anonfiles/AnonFilesFileRunner.java
@@ -0,0 +1,76 @@
+package cz.vity.freerapid.plugins.services.anonfiles;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.DownloadClientConsts;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+
+/**
+ * Class which contains main code
+ *
+ * @author ntoskrnl
+ */
+class AnonFilesFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(AnonFilesFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception {
+        super.runCheck();
+        final HttpMethod getMethod = getGetMethod(fileURL);
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+            checkNameAndSize();
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkNameAndSize() throws ErrorDuringDownloadingException {
+        final Matcher matcher = getMatcherAgainstContent("<legend><b>(.+?)</b> \\((.+?)\\)</legend>");
+        if (!matcher.find()) {
+            throw new PluginImplementationException("File name/size not found");
+        }
+        httpFile.setFileName(matcher.group(1));
+        httpFile.setFileSize(PlugUtils.getFileSizeFromString(matcher.group(2)));
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        HttpMethod method = getGetMethod(fileURL);
+        if (makeRedirectedRequest(method)) {
+            checkProblems();
+            checkNameAndSize();
+            setClientParameter(DownloadClientConsts.DONT_USE_HEADER_FILENAME, true);
+            method = getMethodBuilder()
+                    .setReferer(fileURL)
+                    .setActionFromAHrefWhereATagContains("Download")
+                    .toGetMethod();
+            if (!tryDownloadAndSaveFile(method)) {
+                checkProblems();
+                throw new ServiceConnectionProblemException("Error starting download");
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        if (getContentAsString().contains("File does not exist")) {
+            throw new URLNotAvailableAnymoreException("File not found");
+        }
+    }
+
+}

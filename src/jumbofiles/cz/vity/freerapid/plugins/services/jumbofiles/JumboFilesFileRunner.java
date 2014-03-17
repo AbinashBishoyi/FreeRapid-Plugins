be257e821fa234824602d217ef620ecbd5fe45b7
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:31:09 +0530

    SVN Update# 3852 by tong2shot: JumboFiles.com - fixed

--- src/jumbofiles/cz/vity/freerapid/plugins/services/jumbofiles/JumboFilesFileRunner.java
+++ src/jumbofiles/cz/vity/freerapid/plugins/services/jumbofiles/JumboFilesFileRunner.java
@@ -2,10 +2,13 @@ package cz.vity.freerapid.plugins.services.jumbofiles;
 
 import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
 import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
 import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
 import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileNameHandler;
 import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileSizeHandler;
+import cz.vity.freerapid.plugins.webclient.MethodBuilder;
+import org.apache.commons.httpclient.HttpMethod;
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -50,8 +53,72 @@ class JumboFilesFileRunner extends XFileSharingRunner {
     @Override
     protected List<String> getDownloadLinkRegexes() {
         final List<String> downloadLinkRegexes = super.getDownloadLinkRegexes();
+        downloadLinkRegexes.add(0, "<FORM METHOD\\s*=\\s*\"LINK\" ACTION\\s*=\\s*\"".toLowerCase() + "(http.+?" + Pattern.quote(httpFile.getFileName()) + ")\">");
         downloadLinkRegexes.add(0, "<FORM METHOD\\s*=\\s*\"LINK\" ACTION\\s*=\\s*\"(http.+?" + Pattern.quote(httpFile.getFileName()) + ")\">");
+        downloadLinkRegexes.add(0, "<a href\\s?=\\s?(?:\"|')".toUpperCase() + "(http.+?" + Pattern.quote(httpFile.getFileName()) + ")(?:\"|')");
         return downloadLinkRegexes;
+    }
+
+    @Override
+    public void run() throws Exception {
+        setLanguageCookie();
+        login();
+        HttpMethod method = getGetMethod(fileURL);
+        if (!makeRedirectedRequest(method)) {
+            checkFileProblems();
+            throw new ServiceConnectionProblemException();
+        }
+        checkFileProblems();
+        checkNameAndSize();
+        checkDownloadProblems();
+        for (int loopCounter = 0; ; loopCounter++) {
+            if (loopCounter >= 8) {
+                //avoid infinite loops
+                throw new PluginImplementationException("Cannot proceed to download link");
+            }
+            final MethodBuilder methodBuilder;
+            if (getContentAsString().contains("method_free")) {
+                methodBuilder = getMethodBuilder()
+                        .setReferer(fileURL)
+                        .setActionFromFormWhereTagContains("method_free", true)
+                        .setAction(fileURL);
+            } else {
+                methodBuilder = getMethodBuilder()
+                        .setReferer(fileURL)
+                        .setActionFromFormWhereTagContains("F1", true)
+                        .setAction(fileURL);
+            }
+            if ((methodBuilder.getParameters().get("method_free") != null) && (!methodBuilder.getParameters().get("method_free").isEmpty())) {
+                methodBuilder.removeParameter("method_premium");
+            }
+            final int waitTime = getWaitTime();
+            final long startTime = System.currentTimeMillis();
+            stepPassword(methodBuilder);
+            if (!stepCaptcha(methodBuilder)) {                // skip the wait timer if its on the same page
+                sleepWaitTime(waitTime, startTime);           //   as a captcha of type ReCaptcha
+            }
+            method = methodBuilder.toPostMethod();
+            final int httpStatus = client.makeRequest(method, false);
+            if (httpStatus / 100 == 3) {
+                //redirect to download file location
+                method = stepRedirectToFileLocation(method);
+                break;
+            } else if (checkDownloadPageMarker()) {
+                //page containing download link
+                final String downloadLink = getDownloadLinkFromRegexes();
+                method = getMethodBuilder()
+                        .setReferer(fileURL)
+                        .setAction(downloadLink)
+                        .toGetMethod();
+                break;
+            }
+            checkDownloadProblems();
+        }
+        setFileStreamContentTypes("text/plain");
+        if (!tryDownloadAndSaveFile(method)) {
+            checkDownloadProblems();
+            throw new ServiceConnectionProblemException("Error starting download");
+        }
     }
 
     @Override

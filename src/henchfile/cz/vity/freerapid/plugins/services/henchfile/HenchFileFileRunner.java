929237a6eb63cd4268bbf9117f13eb766ebbc49e
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 14:07:35 +0530

    SVN Update# 4156 by birchie: Fixed : HenchFile.com

--- src/henchfile/cz/vity/freerapid/plugins/services/henchfile/HenchFileFileRunner.java
+++ src/henchfile/cz/vity/freerapid/plugins/services/henchfile/HenchFileFileRunner.java
@@ -1,6 +1,9 @@
 package cz.vity.freerapid.plugins.services.henchfile;
 
 import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileSizeHandler;
+
+import java.util.List;
 
 /**
  * Class which contains main code
@@ -8,4 +11,12 @@ import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
  * @author birchie
  */
 class HenchFileFileRunner extends XFileSharingRunner {
+
+    @Override
+    protected List<FileSizeHandler> getFileSizeHandlers() {
+        final List<FileSizeHandler> fileSizeHandlers = super.getFileSizeHandlers();
+        fileSizeHandlers.add(0, new HenchFileFileSizeHandler());
+        return fileSizeHandlers;
+    }
+
 }

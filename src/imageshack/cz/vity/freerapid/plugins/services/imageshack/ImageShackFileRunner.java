ea2c5e637a333bb837699d37055763b9523e8fb6
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 09:03:39 +0530

    SVN Update# 2451 by ntoskrnl: ImageShack.us - fixed + added yfrog.com mirror

--- src/imageshack/cz/vity/freerapid/plugins/services/imageshack/ImageShackFileRunner.java
+++ src/imageshack/cz/vity/freerapid/plugins/services/imageshack/ImageShackFileRunner.java
@@ -1,6 +1,6 @@
 package cz.vity.freerapid.plugins.services.imageshack;
 
-import cz.vity.freerapid.plugins.exceptions.BuildMethodException;
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
 import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
 import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
 import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
@@ -8,12 +8,10 @@ import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.URIException;
 import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.util.URIUtil;
 
-import java.net.URI;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
 
 /**
  * Class which contains main code
@@ -29,22 +27,28 @@ class ImageShackFileRunner extends AbstractRunner {
         final GetMethod getMethod = getGetMethod(fileURL);
         if (makeRedirectedRequest(getMethod)) {
             checkProblems();
-            checkNameAndSize(getContentAsString());
+            checkNameAndSize();
         } else {
             checkProblems();
-            throw new PluginImplementationException();
+            throw new ServiceConnectionProblemException();
         }
-
     }
 
-    private void checkNameAndSize(String contentAsString) throws PluginImplementationException {
-        PlugUtils.checkName(httpFile, contentAsString, "my.php?image=", "&quot;");
-        //name is automatically taken from the file URL, there is no available full file name on their page with extension
+    private void checkNameAndSize() throws ErrorDuringDownloadingException {
+        final Matcher name = getMatcherAgainstContent("Filename:.+?<td[^<>]*?>(.+?)</td>");
+        if (!name.find()) throw new PluginImplementationException("File name not found");
+        httpFile.setFileName(name.group(1));
+
+        final Matcher size = getMatcherAgainstContent("Size:.+?<td[^<>]*?>(.+?)</td>");
+        if (!size.find()) throw new PluginImplementationException("File size not found");
+        httpFile.setFileSize(PlugUtils.getFileSizeFromString(size.group(1)));
+
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
-    private void checkProblems() throws URLNotAvailableAnymoreException, ServiceConnectionProblemException {
-        if (getContentAsString().contains("404 - Not Found")) {
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        if (getContentAsString().contains("404 - Not Found")
+                || getContentAsString().contains("Welcome to ImageShack")) {
             throw new URLNotAvailableAnymoreException("File not found");
         }
     }
@@ -53,32 +57,18 @@ class ImageShackFileRunner extends AbstractRunner {
     public void run() throws Exception {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
-        final GetMethod method = getGetMethod(fileURL); //create GET request
+        final GetMethod method = getGetMethod(fileURL);
         if (makeRedirectedRequest(method)) {
             checkProblems();
-            checkNameAndSize(getContentAsString());
-
-            final String action = getMethodBuilder().setActionFromAHrefWhereATagContains("Full Size").getAction();
-            final String s = new URI(encode(fileURL)).resolve(action).toURL().toExternalForm();
-            logger.info("Link " + s);
-            final HttpMethod httpMethod = getMethodBuilder().setAction(s).toHttpMethod();
-            //here is the download link extraction
+            checkNameAndSize();
+            final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setActionFromAHrefWhereATagContains("blue/external.png").toGetMethod();
             if (!tryDownloadAndSaveFile(httpMethod)) {
                 checkProblems();
-                logger.warning(getContentAsString()); //log the info
-                throw new PluginImplementationException(); //unknown problem
+                throw new ServiceConnectionProblemException("Error starting download");
             }
         } else {
             checkProblems();
-            throw new PluginImplementationException();
-        }
-    }
-
-    private String encode(String s) throws BuildMethodException {
-        try {
-            return URIUtil.encodePathQuery(s, "UTF-8");
-        } catch (URIException e) {
-            throw new BuildMethodException("Cannot create URI");
+            throw new ServiceConnectionProblemException();
         }
     }
 

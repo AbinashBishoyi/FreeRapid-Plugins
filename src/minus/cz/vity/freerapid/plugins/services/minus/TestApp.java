e1beaed35b2bb05a9f1560334825d4fa402dfd90
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:37:45 +0530

    SVN Update# 3889 by tommy: Add new plugin for minus.com and min.us

---
+++ src/minus/cz/vity/freerapid/plugins/services/minus/TestApp.java
@@ -0,0 +1,44 @@
+package cz.vity.freerapid.plugins.services.minus;
+
+import cz.vity.freerapid.plugins.dev.PluginDevApplication;
+import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import org.jdesktop.application.Application;
+
+import java.net.URL;
+
+/**
+ * @author Tommy
+ */
+public class TestApp extends PluginDevApplication {
+    @Override
+    protected void startup() {
+        final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
+        try {
+            //we set file URL
+            httpFile.setNewURL(new URL("http://minus.com/lyGHOEFKYlVVa"));
+//            httpFile.setNewURL(new URL("http://min.us/mIIOD4rnI/1"));
+            //the way we connect to the internet
+            final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
+            connectionSettings.setProxy("127.0.0.1", 8098); //eg we can use local proxy to sniff HTTP communication
+            //then we tries to download
+            final minusServiceImpl service = new minusServiceImpl(); //instance of service - of our plugin
+            //runcheck makes the validation
+            testRun(service, httpFile, connectionSettings);//download file with service and its Runner
+            //all output goes to the console
+        } catch (Exception e) {//catch possible exception
+            e.printStackTrace(); //writes error output - stack trace to console
+        }
+        this.exit();//exit application
+    }
+
+    /**
+     * Main start method for running this application
+     * Called from IDE
+     *
+     * @param args arguments for application
+     */
+    public static void main(String[] args) {
+        Application.launch(TestApp.class, args);//starts the application - calls startup() internally
+    }
+}
---
+++ src/minus/cz/vity/freerapid/plugins/services/minus/minusServiceImpl.java
@@ -0,0 +1,33 @@
+package cz.vity.freerapid.plugins.services.minus;
+
+import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingServiceImpl;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author Tommy
+ */
+public class minusServiceImpl extends XFileSharingServiceImpl {
+
+    @Override
+    public String getName() {
+        return "minus.com";
+    }
+
+    @Override
+    public String getServiceTitle() {
+        return "Minus";
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new minusFileRunner();
+    }
+
+}

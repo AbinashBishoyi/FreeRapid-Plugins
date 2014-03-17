d7c17014619a60cae881021287af6e17b1143cdd
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:57:01 +0530

    SVN Update# 1867 by Vity: edisk test link

---
+++ src/edisk/cz/vity/freerapid/plugins/services/edisk/TestApp.java
@@ -0,0 +1,43 @@
+package cz.vity.freerapid.plugins.services.edisk;
+
+import cz.vity.freerapid.plugins.dev.PluginDevApplication;
+import cz.vity.freerapid.plugins.services.egoshare.EgoshareShareServiceImpl;
+import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import org.jdesktop.application.Application;
+
+import java.net.URL;
+
+/**
+ * @author Ladislav Vitasek
+ */
+public class TestApp extends PluginDevApplication {
+    @Override
+    protected void startup() {
+        final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
+        try {
+            //we set file URL
+            httpFile.setNewURL(new URL("http://www.edisk.cz/stahni/87158/Pit_K._-_I_Love_You_Vinyl.mp3_60.61MB.html"));
+            //the way we connect to the internet
+            final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
+            //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
+            //then we tries to download
+            final EgoshareShareServiceImpl service = new EgoshareShareServiceImpl(); //instance of service - of our plugin
+            testRun(service, httpFile, connectionSettings);//download file with service and its Runner
+            //all output goes to the console
+        } catch (Exception e) {//catch possible exception
+            e.printStackTrace(); //writes error output - stack trace to console
+        }
+        this.exit();//exit application
+    }
+
+    /**
+     * Main start method for running this application
+     * Called from IDE
+     *
+     * @param args arguments for application
+     */
+    public static void main(String[] args) {
+        Application.launch(TestApp.class, args);//starts the application - calls startup() internally
+    }
+}

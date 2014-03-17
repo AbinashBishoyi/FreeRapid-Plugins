d189b08e9445f93fb783d7be6efc1d4c2e5b09fc
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:59:10 +0530

    SVN Update# 2415 by ntoskrnl: FileServe.com - cleanup

--- src/fileserve/cz/vity/freerapid/plugins/services/fileserve/FileserveFilesShareServiceImpl.java
+++ src/fileserve/cz/vity/freerapid/plugins/services/fileserve/FileserveFilesShareServiceImpl.java
@@ -9,10 +9,12 @@ import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
 public class FileserveFilesShareServiceImpl extends AbstractFileShareService {
     private static final String SERVICE_NAME = "fileserve.com";
 
+    @Override
     public String getName() {
         return SERVICE_NAME;
     }
 
+    @Override
     public int getMaxDownloadsFromOneIP() {
         return 1;
     }

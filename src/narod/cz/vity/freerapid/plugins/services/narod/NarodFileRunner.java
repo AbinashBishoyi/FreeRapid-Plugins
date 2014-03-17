3c531406e0ed98ea4b7f21a843e11d0b5d0e2cc7
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:23:26 +0530

    SVN Update# 2099 by wordrider: Narod and TurboBit plugins seemed to have broken encoding. Now they are actually compilable.

--- src/narod/cz/vity/freerapid/plugins/services/narod/NarodFileRunner.java
+++ src/narod/cz/vity/freerapid/plugins/services/narod/NarodFileRunner.java
@@ -34,18 +34,18 @@ class NarodFileRunner extends AbstractRunner {
     }
 
     private void checkNameAndSize() throws ErrorDuringDownloadingException {
-    	Matcher nameMatcher=PlugUtils.matcher("<dt class=\"name\">(.*?)</dt>", getContentAsString());
-    	if(!nameMatcher.find())
-    		unimplemented();
-    	 Matcher namePartMatcher = PlugUtils.matcher("<[^>]*>|\\s", nameMatcher.group(1));
-    	 httpFile.setFileName(namePartMatcher.replaceAll(""));
-    	 
-    	 Matcher sizeMatcher=PlugUtils.matcher("Размер:(?:<[^>]*>|\\s)*(.*?)<", getContentAsString());
-    	 if(!sizeMatcher.find())
-    		 unimplemented();
-    	 httpFile.setFileSize(PlugUtils.getFileSizeFromString(sizeMatcher.group(1).replace('Б', 'B')));
-    	 
-    	 
+        Matcher nameMatcher = PlugUtils.matcher("<dt class=\"name\">(.*?)</dt>", getContentAsString());
+        if (!nameMatcher.find())
+            unimplemented();
+        Matcher namePartMatcher = PlugUtils.matcher("<[^>]*>|\\s", nameMatcher.group(1));
+        httpFile.setFileName(namePartMatcher.replaceAll(""));
+
+        Matcher sizeMatcher = PlugUtils.matcher("??????:(?:<[^>]*>|\\s)*(.*?)<", getContentAsString());
+        if (!sizeMatcher.find())
+            unimplemented();
+        httpFile.setFileSize(PlugUtils.getFileSizeFromString(sizeMatcher.group(1).replace('?', 'B')));
+
+
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
@@ -53,80 +53,71 @@ class NarodFileRunner extends AbstractRunner {
     public void run() throws Exception {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
-        
+
         runCheck();
         checkDownloadProblems();
-        
+
         HttpMethod method1 = addCaptchaPrameters(getMethodBuilder()
-        	.setActionFromFormByName("f-capchaform", true)
-        	.setAction(fileURL))
-        	.setParameter("action", "sendcapcha")
-        	.toPostMethod();
-        
-        if(!makeRequest(method1))
-        	throw new ServiceConnectionProblemException();
+                .setActionFromFormByName("f-capchaform", true)
+                .setAction(fileURL))
+                .setParameter("action", "sendcapcha")
+                .toPostMethod();
+
+        if (!makeRequest(method1))
+            throw new ServiceConnectionProblemException();
         checkProblems();
 
         HttpMethod method2 = getMethodBuilder()
-        	.setActionFromAHrefWhereATagContains("http://narod.ru")
-        	.setBaseURL("http://narod.ru")
-        	.toGetMethod();
-        
-            //here is the download link extraction
-            if (!tryDownloadAndSaveFile(method2)) {
-                checkProblems();//if downloading failed
-                unimplemented();
-            }
+                .setActionFromAHrefWhereATagContains("http://narod.ru")
+                .setBaseURL("http://narod.ru")
+                .toGetMethod();
+
+        //here is the download link extraction
+        if (!tryDownloadAndSaveFile(method2)) {
+            checkProblems();//if downloading failed
+            unimplemented();
+        }
     }
 
-		/**
-		 * @throws PluginImplementationException
-		 */
-		private void unimplemented() throws PluginImplementationException {
-			logger.warning(getContentAsString());//log the info
-			throw new PluginImplementationException();//some unknown problem
-		}
+    private void unimplemented() throws PluginImplementationException {
+        logger.warning(getContentAsString());//log the info
+        throw new PluginImplementationException();//some unknown problem
+    }
 
-    /**
-		 * @param setAction
-		 * @return
-     * @throws ErrorDuringDownloadingException 
-     * @throws IOException 
-		 */
-		private MethodBuilder addCaptchaPrameters(MethodBuilder builder) throws ErrorDuringDownloadingException, IOException {
-			HttpMethod keyRequest=getMethodBuilder()
-				.setAction("http://narod.ru/disk/getcapchaxml/?rnd="+(int)(Math.random()*777))
-				.toGetMethod();
-			
-			if(!makeRequest(keyRequest))
-				throw new ServiceConnectionProblemException();
-			
-			CaptchaSupport cs=getCaptchaSupport();
-			
-			Matcher numberMatcher=PlugUtils.matcher("<number\\s+url=\"([^\"]*)\"[^>]*>([^<]*)<", getContentAsString());
-			if(!numberMatcher.find())
-				unimplemented();
-			
-			return builder.setParameter("key", numberMatcher.group(2))
-				.setParameter("rep", cs.getCaptcha(numberMatcher.group(1)));
-			
-		}
+    private MethodBuilder addCaptchaPrameters(MethodBuilder builder) throws ErrorDuringDownloadingException, IOException {
+        HttpMethod keyRequest = getMethodBuilder()
+                .setAction("http://narod.ru/disk/getcapchaxml/?rnd=" + (int) (Math.random() * 777))
+                .toGetMethod();
 
-		private void checkFileProblems() throws ErrorDuringDownloadingException {
+        if (!makeRequest(keyRequest))
+            throw new ServiceConnectionProblemException();
+
+        CaptchaSupport cs = getCaptchaSupport();
+
+        Matcher numberMatcher = PlugUtils.matcher("<number\\s+url=\"([^\"]*)\"[^>]*>([^<]*)<", getContentAsString());
+        if (!numberMatcher.find())
+            unimplemented();
+
+        return builder.setParameter("key", numberMatcher.group(2))
+                .setParameter("rep", cs.getCaptcha(numberMatcher.group(1)));
+
+    }
+
+    private void checkFileProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
         if (contentAsString.contains("<td class=\"headCode\">404</td>")) {
             throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
         }
     }
-		
-		private void checkDownloadProblems() throws ErrorDuringDownloadingException {
-        if(getContentAsString().contains("<b class=\"error-msg\"><strong>Ошиблись?</strong> Попробуйте еще&nbsp;раз</b>"))
+
+    private void checkDownloadProblems() throws ErrorDuringDownloadingException {
+        if (getContentAsString().contains("<b class=\"error-msg\"><strong>?????????</strong> ?????????? ???&nbsp;???</b>"))
             throw new CaptchaEntryInputMismatchException();
-		}
-		
-		private void checkProblems() throws ErrorDuringDownloadingException {
-			checkDownloadProblems();
-			checkFileProblems();
-		}
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        checkDownloadProblems();
+        checkFileProblems();
+    }
 
 }
--- src/turbobit/cz/vity/freerapid/plugins/services/turbobit/TurboBitFileRunner.java
+++ src/turbobit/cz/vity/freerapid/plugins/services/turbobit/TurboBitFileRunner.java
@@ -8,8 +8,6 @@ import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 
-import com.sun.org.apache.xml.internal.utils.UnImplNode;
-
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 
@@ -34,15 +32,15 @@ class TurboBitFileRunner extends AbstractRunner {
     }
 
     private void checkNameAndSize() throws ErrorDuringDownloadingException {
-    	Matcher nameMatcher=PlugUtils.matcher("Скачать файл(?:.*?)<b>(.*?)</b>", getContentAsString());
-    	if(!nameMatcher.find())
-    		unimplemented();
-    	httpFile.setFileName(nameMatcher.group(1));
-    	
-    	Matcher sizeMatcher=PlugUtils.matcher("Размер файла:(?:\\s|<[^>]*>)*([^>]*)<", getContentAsString());
-    	if(!sizeMatcher.find())
-    		unimplemented();
-    	httpFile.setFileSize(PlugUtils.getFileSizeFromString(sizeMatcher.group(1)));
+        Matcher nameMatcher = PlugUtils.matcher("??????? ????(?:.*?)<b>(.*?)</b>", getContentAsString());
+        if (!nameMatcher.find())
+            unimplemented();
+        httpFile.setFileName(nameMatcher.group(1));
+
+        Matcher sizeMatcher = PlugUtils.matcher("?????? ?????:(?:\\s|<[^>]*>)*([^>]*)<", getContentAsString());
+        if (!sizeMatcher.find())
+            unimplemented();
+        httpFile.setFileSize(PlugUtils.getFileSizeFromString(sizeMatcher.group(1)));
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
@@ -50,109 +48,101 @@ class TurboBitFileRunner extends AbstractRunner {
     public void run() throws Exception {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
-        
+
         runCheck();
         checkDownloadProblems();
-        
+
         final HttpMethod method1 = getMethodBuilder()
-        	.setActionFromAHrefWhereATagContains("Скачать бесплатно")
-        	.setBaseURL("http://www.turbobit.net/")
-        	.toHttpMethod();
-        
-        if(!makeRequest(method1))
-        	throw new ServiceConnectionProblemException();
+                .setActionFromAHrefWhereATagContains("??????? ?????????")
+                .setBaseURL("http://www.turbobit.net/")
+                .toHttpMethod();
+
+        if (!makeRequest(method1))
+            throw new ServiceConnectionProblemException();
         checkProblems();
-        
+
         HttpMethod method2 = getMethodBuilder()
-        	.setActionFromFormWhereTagContains("Скачать", true)
-        	.setParameter("captcha_response", getCaptcha())
-        	.setBaseURL(method1.getURI().toString())
-        	.toHttpMethod();
-        
-        if(!makeRequest(method2))
-        	throw new ServiceConnectionProblemException();
+                .setActionFromFormWhereTagContains("???????", true)
+                .setParameter("captcha_response", getCaptcha())
+                .setBaseURL(method1.getURI().toString())
+                .toHttpMethod();
+
+        if (!makeRequest(method2))
+            throw new ServiceConnectionProblemException();
         checkProblems();
-        
-        Matcher actionMatcher=PlugUtils.matcher("\"(/download/timeout[^\"]*)\"", getContentAsString());
-        if(!actionMatcher.find())
-        	unimplemented();
-        
+
+        Matcher actionMatcher = PlugUtils.matcher("\"(/download/timeout[^\"]*)\"", getContentAsString());
+        if (!actionMatcher.find())
+            unimplemented();
+
         HttpMethod method3 = getMethodBuilder()
-        	.setAction(actionMatcher.group(1))
-        	.setBaseURL("http://www.turbobit.net")
-        	.toHttpMethod();
-        
+                .setAction(actionMatcher.group(1))
+                .setBaseURL("http://www.turbobit.net")
+                .toHttpMethod();
+
         downloadTask.sleep(62);
-        
-        if(!makeRequest(method3))
-        	throw new ServiceConnectionProblemException();
+
+        if (!makeRequest(method3))
+            throw new ServiceConnectionProblemException();
         checkProblems();
 
         HttpMethod method4 = getMethodBuilder()
-        	.setActionFromAHrefWhereATagContains("Скачать файл")
-        	.setBaseURL("http://www.turbobit.net")
-        	.toHttpMethod();
-        
+                .setActionFromAHrefWhereATagContains("??????? ????")
+                .setBaseURL("http://www.turbobit.net")
+                .toHttpMethod();
+
         //here is the download link extraction
         if (!tryDownloadAndSaveFile(method4)) {
-        	checkProblems();//if downloading failed
-        	unimplemented();
+            checkProblems();//if downloading failed
+            unimplemented();
         }
     }
 
-    /**
-		 * @return
-     * @throws ErrorDuringDownloadingException 
-		 */
-		private String getCaptcha() throws ErrorDuringDownloadingException {
-			CaptchaSupport cs=getCaptchaSupport();
-			Matcher captchaURLMatcher=PlugUtils.matcher("<img alt=\"Captcha\" src=\"([^\"]*)\"", getContentAsString());
-			if(!captchaURLMatcher.find())
-				unimplemented();
-			return cs.getCaptcha(captchaURLMatcher.group(1));
-		}
-
-		/**
-		 * @throws ErrorDuringDownloadingException 
-		 * 
-		 */
-		private void unimplemented() throws ErrorDuringDownloadingException {
-      logger.warning(getContentAsString());//log the info
-      throw new PluginImplementationException();//some unknown problem
-		}
-
-		private void checkFileProblems() throws ErrorDuringDownloadingException {
-			Matcher err404Matcher=PlugUtils.matcher("<div class=\"text-404\">(.*?)</div", getContentAsString());
-			if(err404Matcher.find()) {
-				if(err404Matcher.group(1).contains("Запрошенный документ не найден"))
-					throw new URLNotAvailableAnymoreException(err404Matcher.group(1));
-			}
-			if(getContentAsString().contains("Файл не найден"))
-				throw new URLNotAvailableAnymoreException();
+    private String getCaptcha() throws ErrorDuringDownloadingException {
+        CaptchaSupport cs = getCaptchaSupport();
+        Matcher captchaURLMatcher = PlugUtils.matcher("<img alt=\"Captcha\" src=\"([^\"]*)\"", getContentAsString());
+        if (!captchaURLMatcher.find())
+            unimplemented();
+        return cs.getCaptcha(captchaURLMatcher.group(1));
     }
-    
+
+    private void unimplemented() throws ErrorDuringDownloadingException {
+        logger.warning(getContentAsString());//log the info
+        throw new PluginImplementationException();//some unknown problem
+    }
+
+    private void checkFileProblems() throws ErrorDuringDownloadingException {
+        Matcher err404Matcher = PlugUtils.matcher("<div class=\"text-404\">(.*?)</div", getContentAsString());
+        if (err404Matcher.find()) {
+            if (err404Matcher.group(1).contains("??????????? ???????? ?? ??????"))
+                throw new URLNotAvailableAnymoreException(err404Matcher.group(1));
+        }
+        if (getContentAsString().contains("???? ?? ??????"))
+            throw new URLNotAvailableAnymoreException();
+    }
+
     private void checkDownloadProblems() throws ErrorDuringDownloadingException {
-  		try {
-    	Matcher waitMatcher=PlugUtils.matcher("Попробуйте\\s+повторить.*<span id='timeout'>([^>]*)<", getContentAsString());
-    	if(waitMatcher.find()) {
-    			throw new YouHaveToWaitException("You have to wait", Integer.valueOf(waitMatcher.group(1)));
-    	}
-    	Matcher errMatcher=PlugUtils.matcher("<div[^>]*class='error'[^>]*>([^<]*)<", getContentAsString());
-    	if(errMatcher.find() && !errMatcher.group(1).isEmpty()) {
-    		if(errMatcher.group(1).contains("Неверный ответ"))
-    			throw new CaptchaEntryInputMismatchException();
-    		unimplemented();
-    	}
-    	if(getContentAsString().contains("Ссылка просрочена")) // it's unlikely we get this...
-    		throw new YouHaveToWaitException("Trying again...", 10);
-  		} catch(NumberFormatException e) {
-  			unimplemented();
-  		}
+        try {
+            Matcher waitMatcher = PlugUtils.matcher("??????????\\s+?????????.*<span id='timeout'>([^>]*)<", getContentAsString());
+            if (waitMatcher.find()) {
+                throw new YouHaveToWaitException("You have to wait", Integer.valueOf(waitMatcher.group(1)));
+            }
+            Matcher errMatcher = PlugUtils.matcher("<div[^>]*class='error'[^>]*>([^<]*)<", getContentAsString());
+            if (errMatcher.find() && !errMatcher.group(1).isEmpty()) {
+                if (errMatcher.group(1).contains("???????? ?????"))
+                    throw new CaptchaEntryInputMismatchException();
+                unimplemented();
+            }
+            if (getContentAsString().contains("?????? ??????????")) // it's unlikely we get this...
+                throw new YouHaveToWaitException("Trying again...", 10);
+        } catch (NumberFormatException e) {
+            unimplemented();
+        }
     }
-    
+
     private void checkProblems() throws ErrorDuringDownloadingException {
-    	checkFileProblems();
-    	checkDownloadProblems();
+        checkFileProblems();
+        checkDownloadProblems();
     }
 
 }

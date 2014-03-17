78d60a5dad499ca66ad174e27f385d5b9c04d9af
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:50:32 +0530

    SVN Update# 2340 by Vity: dataport.cz plugin

---
+++ src/dataport/cz/vity/freerapid/plugins/services/dataport/DataPortServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.dataport;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ * @author Vity
+ */
+public class DataPortServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "dataport.cz";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 5;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new DataPortFileRunner();
+    }
+
+}

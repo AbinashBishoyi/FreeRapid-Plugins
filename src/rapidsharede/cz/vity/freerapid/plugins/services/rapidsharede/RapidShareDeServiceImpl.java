6fd94af6dd374bc35fdd41a932d43828bc04753e
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:16:34 +0530

    SVN Update# 1453 by Kajda: Plugin RapidShare.de

---
+++ src/rapidsharede/cz/vity/freerapid/plugins/services/rapidsharede/RapidShareDeServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.rapidsharede;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Kajda
+ */
+public class RapidShareDeServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "rapidshare.de";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 9;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new RapidShareDeFileRunner();
+    }
+
+}

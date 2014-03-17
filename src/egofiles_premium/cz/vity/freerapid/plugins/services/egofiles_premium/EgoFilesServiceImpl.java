e14a22a4dafb6278eddee66f810f1769cfba0cdc
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:58:35 +0530

    SVN Update# 4044 by ntoskrnl: EgoFiles.com_premium - new plugin they don't seem to allow free downloads

---
+++ src/egofiles_premium/cz/vity/freerapid/plugins/services/egofiles_premium/EgoFilesServiceImpl.java
@@ -0,0 +1,56 @@
+package cz.vity.freerapid.plugins.services.egofiles_premium;
+
+import cz.vity.freerapid.plugins.services.depositfiles_premium.DepositFilesServiceImpl;
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author ntoskrnl
+ */
+public class EgoFilesServiceImpl extends AbstractFileShareService {
+    private static final String PLUGIN_CONFIG_FILE = "plugin_EgoFilesPremium.xml";
+    private volatile PremiumAccount config;
+
+    @Override
+    public String getName() {
+        return "egofiles.com_premium";
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return false;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new EgoFilesFileRunner();
+    }
+
+    @Override
+    public void showOptions() throws Exception {
+        PremiumAccount pa = showConfigDialog();
+        if (pa != null) config = pa;
+    }
+
+    public PremiumAccount showConfigDialog() throws Exception {
+        return showAccountDialog(getConfig(), "EgoFiles", PLUGIN_CONFIG_FILE);
+    }
+
+    PremiumAccount getConfig() throws Exception {
+        if (config == null) {
+            synchronized (DepositFilesServiceImpl.class) {
+                config = getAccountConfigFromFile(PLUGIN_CONFIG_FILE);
+            }
+        }
+
+        return config;
+    }
+
+    void setConfig(PremiumAccount config) {
+        this.config = config;
+    }
+
+}

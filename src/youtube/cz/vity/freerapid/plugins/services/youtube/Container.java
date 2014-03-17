9fbd9bb9d3cd2e4166e189918d6e8e36a6417b6b
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 15:14:34 +0530

    SVN Update# 4695 by tong2shot: YouTube.com - minor fix.

--- src/youtube/cz/vity/freerapid/plugins/services/youtube/Container.java
+++ src/youtube/cz/vity/freerapid/plugins/services/youtube/Container.java
@@ -6,22 +6,22 @@ import java.util.Locale;
  * @author tong2shot
  */
 public enum Container {
-    Any,
-    mp4,
-    flv,
-    webm,
-    _3gp;
+    Any(0),
+    mp4(1),
+    flv(2),
+    webm(3),
+    _3gp(4);
 
     private final String name;
     private final String fileExt;
 
-    private Container() {
-        switch (this) {
-            case Any:
+    private Container(int index) {
+        switch (index) {
+            case 0:
                 name = "Any container";
                 fileExt = "Any";
                 break;
-            case _3gp:
+            case 4:
                 name = "3GP";
                 fileExt = ".3gp";
                 break;

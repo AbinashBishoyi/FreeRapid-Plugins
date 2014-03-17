214b8d96335b3f873ac5439fffb7d7122a4d85a6
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:53:50 +0530

    SVN Update# 2369 by translator: new plugin - TeraDepot.com

---
+++ src/teradepot/cz/vity/freerapid/plugins/services/teradepot/TeraDepotServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.teradepot;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author ntoskrnl
+ */
+public class TeraDepotServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "teradepot.com";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 9;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new TeraDepotFileRunner();
+    }
+
+}

9509567e135c5853fb59724af44689b9099c667c
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 09:01:22 +0530

    SVN Update# 2433 by ntoskrnl: new plugin - HellSpy.com

---
+++ src/hellspy/cz/vity/freerapid/plugins/services/hellspy/HellSpyServiceImpl.java
@@ -0,0 +1,56 @@
+package cz.vity.freerapid.plugins.services.hellspy;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author ntoskrnl
+ */
+public class HellSpyServiceImpl extends AbstractFileShareService {
+    private static final String PLUGIN_CONFIG_FILE = "plugin_HellSpy.xml";
+    private volatile PremiumAccount config;
+
+    @Override
+    public String getName() {
+        return "hellspy.com";
+    }
+
+    @Override
+    public int getMaxDownloadsFromOneIP() {
+        return 100;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new HellSpyFileRunner();
+    }
+
+    @Override
+    public void showOptions() throws Exception {
+        PremiumAccount pa = showConfigDialog();
+        if (pa != null) config = pa;
+    }
+
+    public PremiumAccount showConfigDialog() throws Exception {
+        return showAccountDialog(getConfig(), "HellSpy", PLUGIN_CONFIG_FILE);
+    }
+
+    PremiumAccount getConfig() throws Exception {
+        if (config == null) {
+            synchronized (HellSpyServiceImpl.class) {
+                config = getAccountConfigFromFile(PLUGIN_CONFIG_FILE);
+            }
+        }
+
+        return config;
+    }
+
+}

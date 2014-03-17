a05b006ae9d7d06f849fb8ca990193584d5736f4
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 12:03:12 +0530

    SVN Update# 3322 by JPEXS: [multishare_mms] FileSonic url handling fix

--- src/multishare_mms/cz/vity/freerapid/plugins/services/multishare_mms/MultiShareMMSFileRunner.java
+++ src/multishare_mms/cz/vity/freerapid/plugins/services/multishare_mms/MultiShareMMSFileRunner.java
@@ -36,7 +36,7 @@ class MultiShareMMSFileRunner extends AbstractRunner {
     private static String PHPSESSID = "";
     private static String versionUrl="http://www.multishare.cz/html/mms_support.php?version";
 
-    private static String version="1.1.2";
+    private static String version="1.1.3";
 
     @Override
     public void runCheck() throws Exception { //this method validates file

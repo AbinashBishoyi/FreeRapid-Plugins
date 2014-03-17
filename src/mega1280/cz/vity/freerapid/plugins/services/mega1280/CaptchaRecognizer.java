dc93ebf9de749eda735f08c80a052a3ad54df720
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:48:40 +0530

    SVN Update# 2323 by ntoskrnl: Mega.1280.com - new captcha recognition, not flawless though.

---
+++ src/mega1280/cz/vity/freerapid/plugins/services/mega1280/CaptchaRecognizer.java
@@ -0,0 +1,101 @@
+package cz.vity.freerapid.plugins.services.mega1280;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author Vity, ntoskrnl
+ */
+final class CaptchaRecognizer {
+    private final static char[] LETTERS = {'2', '3', '4', '5', '6', '7', '8', '9', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
+    private final Collection<Template> trainedSet = new ArrayList<Template>(LETTERS.length);
+
+    public CaptchaRecognizer() throws IOException {
+        //final BufferedImage image = ImageIO.read(new File("E:\\projects\\captchatest\\letters\\letters.png"));
+        final BufferedImage image = ImageIO.read((new CaptchaRecognizer()).getClass().getResourceAsStream("/resources/letters.png"));
+        int pos = 0;
+        for (final char c : LETTERS) {
+            final BufferedImage subimage = image.getSubimage(pos, 0, 20, 28);
+            trainedSet.add(new Template(c, imageToData(subimage)));
+            pos += 20;
+        }
+    }
+
+    public String recognize(final BufferedImage image) {
+        if (image.getType() != BufferedImage.TYPE_BYTE_BINARY)
+            throw new IllegalArgumentException("Image must be of type TYPE_BYTE_BINARY");
+
+        final StringBuilder builder = new StringBuilder(4);
+
+        for (int i = 0, x = 9; i < 5; i++, x += 28) {
+            final BufferedImage subimage = image.getSubimage(x, 0, 20, 28);
+            builder.append(findResult(subimage).ch);
+        }
+
+        return builder.toString();
+    }
+
+    private Template findResult(final BufferedImage image) {
+        final Template test = new Template('-', imageToData(image));
+        for (final Template template : trainedSet) {
+            template.countDistance(test);
+        }
+        return Collections.min(trainedSet);
+    }
+
+    private class Template implements Comparable<Template> {
+        private char ch;
+        private short[] data;
+        private long distance;
+
+        private Template(char ch, short[] data) {
+            this.ch = ch;
+            this.data = data;
+        }
+
+        public void countDistance(final Template otherTemplate) {
+            final short[] otherData = otherTemplate.data;
+            distance = 0;
+            for (int i = 0; i < data.length; i++) {
+                final int t = otherData[i] - data[i];
+                distance += t * t;
+            }
+        }
+
+        @Override
+        public int compareTo(final Template o) {
+            return new Long(distance).compareTo(o.distance);
+        }
+
+        public char getCh() {
+            return ch;
+        }
+
+        @Override
+        public String toString() {
+            return "'" + String.valueOf(ch) + "' - " + distance;
+        }
+    }
+
+    private short[] imageToData(final BufferedImage image) {
+        final int w = image.getWidth();
+        final int h = image.getHeight();
+
+        short[] data = new short[w * h];
+        int offset = 0;
+
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                data[offset++] = image.getRGB(x, y) == Color.WHITE.getRGB() ? (short) 255 : (short) 0;
+            }
+        }
+
+        return data;
+    }
+
+}
--- src/mega1280/cz/vity/freerapid/plugins/services/mega1280/Mega1280FileRunner.java
+++ src/mega1280/cz/vity/freerapid/plugins/services/mega1280/Mega1280FileRunner.java
@@ -1,6 +1,9 @@
 package cz.vity.freerapid.plugins.services.mega1280;
 
-import cz.vity.freerapid.plugins.exceptions.*;
+import cz.vity.freerapid.plugins.exceptions.CaptchaEntryInputMismatchException;
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.hoster.CaptchaSupport;
@@ -8,19 +11,21 @@ import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.Locale;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 /**
  * Class which contains main code
  *
- * @author Vity
+ * @author Vity, ntoskrnl
  */
 class Mega1280FileRunner extends AbstractRunner {
     private final static Logger logger = Logger.getLogger(Mega1280FileRunner.class.getName());
-    private int captchaCounter;
-
+    private final static int COLOR_LIMIT = 200;
+    private final int captchaMax = 6;
+    private int captchaCounter = 0;
 
     @Override
     public void runCheck() throws Exception { //this method validates file
@@ -29,8 +34,10 @@ class Mega1280FileRunner extends AbstractRunner {
         if (makeRedirectedRequest(getMethod)) {
             checkProblems();
             checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
-        } else
-            throw new PluginImplementationException();
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
     }
 
     private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
@@ -48,16 +55,14 @@ class Mega1280FileRunner extends AbstractRunner {
             final String contentAsString = getContentAsString();//check for response
             checkProblems();//check problems
             checkNameAndSize(contentAsString);//extract file name and size from the page
-            captchaCounter = 0;
             final String firstPage = "name=\"code_security\" id=\"code_security\"";
             while (getContentAsString().contains(firstPage)) {
                 final HttpMethod httpMethod = stepCaptcha();
-                //here is the download link extraction
                 if (!makeRedirectedRequest(httpMethod)) //reload captcha page or move to another page
-                    throw new PluginImplementationException();
+                    throw new ServiceConnectionProblemException();
             }
             if (!getContentAsString().contains("hddomainname"))
-                throw new YouHaveToWaitException("Invalid page content", 30);
+                throw new ServiceConnectionProblemException("Invalid page content");
             final String hddomainname = PlugUtils.getStringBetween(getContentAsString(), "hddomainname\" style=\"display:none\">", "</div>");
             final String hdfolder = PlugUtils.getStringBetween(getContentAsString(), "hdfolder\" style=\"display:none\">", "</div>");
             final String hdcode = PlugUtils.getStringBetween(getContentAsString(), "hdcode\" style=\"display:none\">", "</div>");
@@ -68,7 +73,7 @@ class Mega1280FileRunner extends AbstractRunner {
             if (!tryDownloadAndSaveFile(httpMethod)) {
                 checkProblems();//if downloading failed
                 logger.warning(getContentAsString());//log the info
-                throw new PluginImplementationException();//some unknown problem
+                throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
             }
         } else {
             checkProblems();
@@ -79,7 +84,7 @@ class Mega1280FileRunner extends AbstractRunner {
     private void checkProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
         if (contentAsString.contains("File Not Found") || contentAsString.contains("Li\u00EAn k\u1EBFt b\u1EA1n v\u1EEBa ch\u1ECDn kh\u00F4ng t\u1ED3n t\u1EA1i tr\u00EAn h\u1EC7 th\u1ED1ng") || contentAsString.contains("File not found")) {
-            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+            throw new URLNotAvailableAnymoreException("File not found"); //let user know in FRD
         }
         if (contentAsString.contains("Limit download xx !")) {
             throw new ServiceConnectionProblemException("Limit download xx ! - unknown error message from server");
@@ -87,25 +92,78 @@ class Mega1280FileRunner extends AbstractRunner {
     }
 
     private HttpMethod stepCaptcha() throws Exception {
-        CaptchaSupport captchaSupport = getCaptchaSupport();
+        final CaptchaSupport captchaSupport = getCaptchaSupport();
         final String captchaURL = "http://mega.1280.com/security_code.php";
         final String captcha;
-        if (captchaCounter < 6) {
+        if (captchaCounter < captchaMax) {
             ++captchaCounter;
-            final BufferedImage captchaImage = captchaSupport.getCaptchaImage(captchaURL);
-            captcha = PlugUtils.recognize(captchaImage, "-d -1 -C A-z-0-9");
-            logger.info("captcha = " + captcha);
+            final BufferedImage captchaImage = prepareCaptchaImage(captchaSupport.getCaptchaImage(captchaURL));
+            //captcha = PlugUtils.recognize(captchaImage, "-d -1 -C a-z-0-9");
+            captcha = new CaptchaRecognizer().recognize(captchaImage);
+            logger.info("Attempt " + captchaCounter + " of " + captchaMax + ", OCR recognized " + captcha);
         } else {
             captcha = captchaSupport.getCaptcha(captchaURL);
+            if (captcha == null) throw new CaptchaEntryInputMismatchException();
+            logger.info("Manual captcha " + captcha);
         }
 
-        if (captcha == null) {
-            throw new CaptchaEntryInputMismatchException();
-        } else {
-            return getMethodBuilder().setReferer(fileURL).setActionFromFormByName("frm_download", true).setAction(fileURL).setParameter("code_security", captcha.toLowerCase(Locale.ENGLISH)).toPostMethod();
+        return getMethodBuilder().setReferer(fileURL).setActionFromFormByName("frm_download", true).setAction(fileURL).setParameter("code_security", captcha).toPostMethod();
+    }
+
+    private BufferedImage prepareCaptchaImage(final BufferedImage input) {
+        final int w = input.getWidth();
+        final int h = input.getHeight();
+
+        //convert input image to greyscale
+        final BufferedImage greyScale = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
+        greyScale.getGraphics().drawImage(input, 0, 0, Color.WHITE, null);
+
+        //convert greyscale image to black and white according to COLOR_LIMIT
+        final BufferedImage blackAndWhite = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_BINARY);
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                final int red = new Color(greyScale.getRGB(x, y)).getRed();
+                final int color = red > COLOR_LIMIT ? Color.WHITE.getRGB() : Color.BLACK.getRGB();
+                blackAndWhite.setRGB(x, y, color);
+            }
         }
 
+        //remove the small distraction dots
+        final BufferedImage output = new BufferedImage(w - 2, h - 2, BufferedImage.TYPE_BYTE_BINARY);
+        output.getGraphics().drawImage(blackAndWhite.getSubimage(1, 1, w - 2, h - 2), 0, 0, Color.WHITE, null);
+        for (int y = 1; y < h - 1; y++) {
+            for (int x = 1; x < w - 1; x++) {
+                if (blackAndWhite.getRGB(x, y) == Color.BLACK.getRGB()) {
+                    final int color = getNeighborPixels(blackAndWhite, x, y).contains(Color.BLACK.getRGB()) ? Color.BLACK.getRGB() : Color.WHITE.getRGB();
+                    output.setRGB(x - 1, y - 1, color);
+                }
+            }
+        }
 
+        //JOptionPane.showConfirmDialog(null, new ImageIcon(output));
+
+        return output;
+    }
+
+    private ArrayList<Integer> getNeighborPixels(final BufferedImage img, final int x, final int y) {
+        if (img == null)
+            throw new NullPointerException("Image cannot be null");
+        final int w = img.getWidth();
+        final int h = img.getHeight();
+        if (x <= 0 || y <= 0 || x >= w || y >= h)
+            throw new IndexOutOfBoundsException();
+
+        final ArrayList<Integer> ret = new ArrayList<Integer>(8);
+        ret.add(img.getRGB(x - 1, y - 1));
+        ret.add(img.getRGB(x, y - 1));
+        ret.add(img.getRGB(x + 1, y - 1));
+        ret.add(img.getRGB(x - 1, y));
+        ret.add(img.getRGB(x + 1, y));
+        ret.add(img.getRGB(x - 1, y + 1));
+        ret.add(img.getRGB(x, y + 1));
+        ret.add(img.getRGB(x + 1, y + 1));
+
+        return ret;
     }
 
 }

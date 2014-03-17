7a2ea0085d6838ca1d3dddd79e22e579175d4a01
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:03:48 +0530

    SVN Update# 3647 by tong2shot: Speedyshare.com - fixed, added speedy.sh support

--- src/speedyshare/cz/vity/freerapid/plugins/services/speedyshare/SpeedyShareFileRunner.java
+++ src/speedyshare/cz/vity/freerapid/plugins/services/speedyshare/SpeedyShareFileRunner.java
@@ -5,6 +5,7 @@ import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import cz.vity.freerapid.utilities.LogUtils;
+import org.apache.commons.httpclient.Cookie;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 
@@ -22,10 +23,13 @@ import java.util.regex.Matcher;
  */
 class SpeedyShareFileRunner extends AbstractRunner {
     private final static Logger logger = Logger.getLogger(SpeedyShareFileRunner.class.getName());
+    private final static String SERVICE_COOKIE_DOMAIN = ".speedyshare.com";
 
     @Override
     public void runCheck() throws Exception {
         super.runCheck();
+        checkURL();
+        addCookie(new Cookie(SERVICE_COOKIE_DOMAIN, "trans", "en", "/", 86400, false));
         final GetMethod method = getGetMethod(fileURL);
         if (makeRedirectedRequest(method)) {
             checkProblems();
@@ -38,22 +42,29 @@ class SpeedyShareFileRunner extends AbstractRunner {
 
     private void checkNameAndSize() throws ErrorDuringDownloadingException {
         final String content = getContentAsString();
-        PlugUtils.checkName(httpFile, content, "<title>", "</title>");
-        PlugUtils.checkFileSize(httpFile, content, "File size ", ", ");
+        PlugUtils.checkName(httpFile, content, "Download File:", "\"");
+        PlugUtils.checkFileSize(httpFile, content, "<div class=sizetagtext>", "</div>");
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
     @Override
     public void run() throws Exception {
         super.run();
+        checkURL();
+        addCookie(new Cookie(SERVICE_COOKIE_DOMAIN, "trans", "en", "/", 86400, false));
         logger.info("Starting download in TASK " + fileURL);
-
         final GetMethod method = getGetMethod(fileURL);
         if (makeRedirectedRequest(method)) {
             checkProblems();
             checkNameAndSize();
 
-            final Matcher matcher = getMatcherAgainstContent("href=\"(/files/(\\d+?)/download/([^/<>\"]+?))\">");
+            final Matcher matcher;
+            if (fileURL.contains("speedyshare.com/files/")) {
+                matcher = getMatcherAgainstContent("href=[\"'](/files/(\\d+?)/download/([^/<>\"']+?))[\"']>");
+            } else {
+                matcher = getMatcherAgainstContent("href=[\"'](/([^/<>\"']+?)/download/([^/<>\"']+?))[\"']>");
+            }
+
             if (!matcher.find()) throw new PluginImplementationException("Download link not found");
 
             if (matcher.find(matcher.end())) { //multiple files
@@ -62,7 +73,12 @@ class SpeedyShareFileRunner extends AbstractRunner {
                 int start = 0;
                 final List<URI> uriList = new LinkedList<URI>();
                 while (matcher.find(start)) {
-                    String link = "http://www.speedyshare.com/files/" + matcher.group(2) + "/" + matcher.group(3);
+                    final String link;
+                    if (fileURL.contains("speedyshare.com/files/")) {
+                        link = "http://www.speedyshare.com/files/" + matcher.group(2) + "/" + matcher.group(3);
+                    } else {
+                        link = "http://www.speedyshare.com/" + matcher.group(2) + "/" + matcher.group(3);
+                    }
                     try {
                         uriList.add(new URI(link));
                     } catch (URISyntaxException e) {
@@ -87,9 +103,13 @@ class SpeedyShareFileRunner extends AbstractRunner {
         }
     }
 
+    private void checkURL() {
+        fileURL = fileURL.replaceFirst("speedy\\.sh", "speedyshare.com").replaceFirst("speedyshare.com/file/", "speedyshare.com/");
+    }
+
     private void checkProblems() throws ErrorDuringDownloadingException {
         final String content = getContentAsString();
-        if (content.contains("Not Found") || content.contains("Not valid anymore") || content.contains("The file has been deleted") || !content.contains("<span class=resultlink>")) {
+        if (content.contains("Not Found") || content.contains("Not valid anymore") || content.contains("The file has been deleted") || content.contains("File not found")) {
             throw new URLNotAvailableAnymoreException("File not found");
         }
         if (content.contains("The one-hour limit")) {
--- src/speedyshare/cz/vity/freerapid/plugins/services/speedyshare/TestApp.java
+++ src/speedyshare/cz/vity/freerapid/plugins/services/speedyshare/TestApp.java
@@ -16,8 +16,12 @@ public class TestApp extends PluginDevApplication {
         final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
         try {
             //we set file URL
-            httpFile.setNewURL(new URL("http://www.speedyshare.com/files/20762507/logovq.zip"));//single
+            //httpFile.setNewURL(new URL("http://www.speedyshare.com/files/20762507/logovq.zip"));//single
             //httpFile.setNewURL(new URL("http://www.speedyshare.com/712851541.html"));//multiple
+            //httpFile.setNewURL(new URL("http://www.speedyshare.com/w4EYf/DPnF-Phi-Brain-2-03-animelv.net.mp4"));
+            //httpFile.setNewURL(new URL("http://www.speedyshare.com/files/19332630/the_fish_that_saved_Pittsburgh.rar"));
+            //httpFile.setNewURL(new URL("http://speedy.sh/yBVJx/HimitSubs-Haiyore-Nyaruko-san-03-animelv.net.mp4"));
+            httpFile.setNewURL(new URL("http://www.speedyshare.com/file/tfV4v/SolSuite-2012-12.50-Setup.rar"));
             //the way we connect to the internet
             final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
             //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication

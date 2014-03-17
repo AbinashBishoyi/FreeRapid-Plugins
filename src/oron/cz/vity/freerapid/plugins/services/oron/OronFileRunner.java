9b533d51a087a4028e6a5e17c7e10c0d4b905e59
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:34:43 +0530

    SVN Update# 2198 by Vity: oron.com update

--- src/oron/cz/vity/freerapid/plugins/services/oron/OronFileRunner.java
+++ src/oron/cz/vity/freerapid/plugins/services/oron/OronFileRunner.java
@@ -19,115 +19,118 @@ import java.util.regex.Matcher;
  * @author Thumb
  */
 class OronFileRunner extends AbstractRunner {
-	private final static Logger logger = Logger.getLogger(OronFileRunner.class.getName());
+    private final static Logger logger = Logger.getLogger(OronFileRunner.class.getName());
 
 
-	@Override
-		public void runCheck() throws Exception { //this method validates file
-		super.runCheck();
-		final GetMethod getMethod = getGetMethod(fileURL);//make first request
-		if (makeRedirectedRequest(getMethod)) {
-			checkFileProblems();
-			checkNameAndSize();//ok let's extract file name and size from the page
-		} else {
-			checkFileProblems();
-			throw new ServiceConnectionProblemException();
-		}
-	}
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkFileProblems();
+            checkNameAndSize();//ok let's extract file name and size from the page
+        } else {
+            checkFileProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
 
-	/**
-	 * @throws PluginImplementationException
-	 *
-	 */
-	private void unimplemented() throws PluginImplementationException {
-		logger.warning(getContentAsString());//log the info
-		throw new PluginImplementationException();//some unknown problem
+    /**
+     * @throws PluginImplementationException
+     *
+     */
+    private void unimplemented() throws PluginImplementationException {
+        logger.warning(getContentAsString());//log the info
+        throw new PluginImplementationException();//some unknown problem
 
-	}
+    }
 
-	private void checkNameAndSize() throws ErrorDuringDownloadingException {
-		Matcher name_m=PlugUtils.matcher("Filename: <[^<>]*>([^<]+)<", getContentAsString());
-		if(!name_m.find())
-			unimplemented();
-		httpFile.setFileName(name_m.group(1));
+    private void checkNameAndSize() throws ErrorDuringDownloadingException {
+        Matcher name_m = PlugUtils.matcher("Filename: <[^<>]*>([^<]+)<", getContentAsString());
+        if (!name_m.find())
+            unimplemented();
+        httpFile.setFileName(name_m.group(1));
 
-		PlugUtils.checkFileSize(httpFile, getContentAsString(), "Size: ", "<");
-		httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
-	}
+        PlugUtils.checkFileSize(httpFile, getContentAsString(), "Size: ", "<");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
 
-	@Override
-		public void run() throws Exception {
-		super.run();
-		logger.info("Starting download in TASK " + fileURL);
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
 
-		runCheck();
-		checkDownloadProblems();
+        runCheck();
+        checkDownloadProblems();
 
-		final HttpMethod method= getMethodBuilder()
-			.setActionFromFormWhereTagContains("method_free", true)
-			.setAction(fileURL)
-			.setParameter("method_free", " Free Download ")
-			.toPostMethod();
+        final HttpMethod method = getMethodBuilder()
+                .setActionFromFormWhereTagContains("method_free", true)
+                .setAction(fileURL)
+                .setParameter("method_free", " Free Download ")
+                .toPostMethod();
 
-		if(!makeRequest(method))
-			throw new ServiceConnectionProblemException();
-		checkProblems();
+        if (!makeRequest(method))
+            throw new ServiceConnectionProblemException();
+        checkProblems();
 
-		MethodBuilder request2 = getMethodBuilder()
-			.setActionFromFormByName("F1", true)
-			.setAction(fileURL);
+        MethodBuilder request2 = getMethodBuilder()
+                .setActionFromFormByName("F1", true)
+                .setAction(fileURL);
 
-		ReCaptcha r=new ReCaptcha("6LdzWwYAAAAAAAzlssDhsnar3eAdtMBuV21rqH2N", client);
-		String imageURL=r.getImageURL();
-		CaptchaSupport cs=getCaptchaSupport();
-		r.setRecognized(cs.getCaptcha(imageURL));
+        ReCaptcha r = new ReCaptcha("6LdzWwYAAAAAAAzlssDhsnar3eAdtMBuV21rqH2N", client);
+        String imageURL = r.getImageURL();
+        CaptchaSupport cs = getCaptchaSupport();
+        r.setRecognized(cs.getCaptcha(imageURL));
 
-		final HttpMethod method2 = r.modifyResponseMethod(request2)
-			.toPostMethod();
+        final HttpMethod method2 = r.modifyResponseMethod(request2)
+                .toPostMethod();
 
-		if(!makeRequest(method2))
-			throw new ServiceConnectionProblemException();
-		checkProblems();
+        if (!makeRequest(method2))
+            throw new ServiceConnectionProblemException();
+        checkProblems();
 
-		final HttpMethod method3 = getMethodBuilder()
-			.setActionFromAHrefWhereATagContains("Download")
-			.toHttpMethod();
+        final HttpMethod method3 = getMethodBuilder()
+                .setActionFromAHrefWhereATagContains("Download")
+                .toHttpMethod();
 
-		//here is the download link extraction
-		if (!tryDownloadAndSaveFile(method3)) {
-			checkProblems();//if downloading failed
-			unimplemented();
-		}
-	}
+        //here is the download link extraction
+        if (!tryDownloadAndSaveFile(method3)) {
+            checkProblems();//if downloading failed
+            unimplemented();
+        }
+    }
 
-	private void checkFileProblems() throws ErrorDuringDownloadingException {
-		Matcher err_m=PlugUtils.matcher("<font class=\"err\">([^<>]+)<", getContentAsString());
-		if(err_m.find()) {
-			if(err_m.group(1).contains("No such file"))
-				throw new URLNotAvailableAnymoreException("File not found"); // we could as well repair the URL...
-		}
-		if(getContentAsString().contains("<b>File Not Found</b>")
-		   || getContentAsString().contains("<meta NAME=\"description\" CONTENT=\"ORON.com - File Not Found\">"))
-			throw new URLNotAvailableAnymoreException("File not found"); // we could as well repair the URL...
-	}
+    private void checkFileProblems() throws ErrorDuringDownloadingException {
+        if (getContentAsString().contains("Status: <b class=\"ok\">Premium Only</b><br>")) {
+            throw new URLNotAvailableAnymoreException("Premium only");
+        }
+        Matcher err_m = PlugUtils.matcher("<font class=\"err\">([^<>]+)<", getContentAsString());
+        if (err_m.find()) {
+            if (err_m.group(1).contains("No such file"))
+                throw new URLNotAvailableAnymoreException("File not found"); // we could as well repair the URL...
+        }
+        if (getContentAsString().contains("<b>File Not Found</b>")
+                || getContentAsString().contains("<meta NAME=\"description\" CONTENT=\"ORON.com - File Not Found\">"))
+            throw new URLNotAvailableAnymoreException("File not found"); // we could as well repair the URL...
+    }
 
-	private void checkDownloadProblems() throws ErrorDuringDownloadingException {
-		Matcher err_m=PlugUtils.matcher("<font class=\"err\">([^<>]+)<", getContentAsString());
-		if(err_m.find()) {
-			Matcher m=PlugUtils.matcher("You have to wait(?:.*?\\b([0-9]+) hour)?(?:.*?\\b([0-9]+) minute)?(?:.*\\b([0-9]+) second)?", err_m.group(1));
-			if(m.find())
-				throw new YouHaveToWaitException("You have to wait",
-					Integer.valueOf("0"+m.group(1))*3600+
-					Integer.valueOf("0"+m.group(2))*60+
-					Integer.valueOf("0"+m.group(3)));
-			if(getContentAsString().contains("Wrong captcha"))
-				throw new CaptchaEntryInputMismatchException();
-			unimplemented();
-		}
-	}
+    private void checkDownloadProblems() throws ErrorDuringDownloadingException {
+        Matcher err_m = PlugUtils.matcher("<font class=\"err\">([^<>]+)<", getContentAsString());
+        if (err_m.find()) {
+            Matcher m = PlugUtils.matcher("You have to wait(?:.*?\\b([0-9]+) hour)?(?:.*?\\b([0-9]+) minute)?(?:.*\\b([0-9]+) second)?", err_m.group(1));
+            if (m.find())
+                throw new YouHaveToWaitException("You have to wait",
+                        Integer.valueOf("0" + m.group(1)) * 3600 +
+                                Integer.valueOf("0" + m.group(2)) * 60 +
+                                Integer.valueOf("0" + m.group(3)));
+            if (getContentAsString().contains("Wrong captcha"))
+                throw new CaptchaEntryInputMismatchException();
+            unimplemented();
+        }
+    }
 
-	private void checkProblems() throws ErrorDuringDownloadingException {
-		checkFileProblems();
-		checkDownloadProblems();
-	}
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        checkFileProblems();
+        checkDownloadProblems();
+    }
 }
--- src/oron/cz/vity/freerapid/plugins/services/oron/TestApp.java
+++ src/oron/cz/vity/freerapid/plugins/services/oron/TestApp.java
@@ -16,7 +16,7 @@ public class TestApp extends PluginDevApplication {
         final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
         try {
             //we set file URL
-            httpFile.setNewURL(new URL("http://oron.com/w5y7z1wajovd/0859.rar.html"));
+            httpFile.setNewURL(new URL("http://oron.com/939dpmg1f6rf/2009-12-24-03.rar.html"));
             //the way we connect to the internet
             final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
             //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication

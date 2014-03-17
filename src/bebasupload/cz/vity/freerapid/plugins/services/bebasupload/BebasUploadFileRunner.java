61ade1e4a4e593d8bbe20f973bc4f52ec0a99ce8
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:25:36 +0530

    SVN Update# 2118 by Arthur: BebasUpload.com - Fixed captcha support

--- src/bebasupload/cz/vity/freerapid/plugins/services/bebasupload/BebasUploadFileRunner.java
+++ src/bebasupload/cz/vity/freerapid/plugins/services/bebasupload/BebasUploadFileRunner.java
@@ -1,19 +1,16 @@
 package cz.vity.freerapid.plugins.services.bebasupload;
 
-import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
-import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
-import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
-import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.exceptions.*;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.hoster.CaptchaSupport;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.Cookie;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 
 import java.util.logging.Logger;
-
-import org.apache.commons.httpclient.Cookie;
 
 /**
  * Class which contains main code
@@ -36,18 +33,17 @@ class BebasUploadFileRunner extends AbstractRunner {
     }
 
     private void Login() throws Exception {
-        PostMethod postMethod= getPostMethod("http://bebasupload.com/login.html");
+        PostMethod postMethod = getPostMethod("http://bebasupload.com/login.html");
 
-        postMethod.addParameter("op","login");
-        postMethod.addParameter("redirect","");
-        postMethod.addParameter("login","freerapid");
-        postMethod.addParameter("password","freerapid");
-
+        postMethod.addParameter("op", "login");
+        postMethod.addParameter("redirect", "");
+        postMethod.addParameter("login", "freerapid");
+        postMethod.addParameter("password", "freerapid");
 
 
         addCookie(new Cookie(".bebasupload.com", "login", "freerapid", "/", null, false));
         addCookie(new Cookie(".bebasupload.com", "xfss", "", "/", null, false));
-     
+
         makeRedirectedRequest(postMethod);
 
 
@@ -72,20 +68,21 @@ class BebasUploadFileRunner extends AbstractRunner {
             final String contentAsString = getContentAsString();//check for response
             checkProblems();//check problems
             checkNameAndSize(contentAsString);//extract file name and size from the page
-            HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).setBaseURL(fileURL).setReferer(fileURL).setActionFromFormByIndex(1, true).removeParameter("method_premium").setParameter("method_free","Free Download").toHttpMethod();//TODO
-            if(!makeRedirectedRequest(httpMethod)) {
-                   checkProblems();//if downloading failed
+            HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).setBaseURL(fileURL).setReferer(fileURL).setActionFromFormByIndex(1, true).removeParameter("method_premium").setParameter("method_free", "Free Download").toHttpMethod();//TODO
+            if (!makeRedirectedRequest(httpMethod)) {
+                checkProblems();//if downloading failed
                 logger.warning(getContentAsString());//log the info
                 throw new PluginImplementationException();//some unknown problem
             }
-            httpMethod = getMethodBuilder().setAction(fileURL).setBaseURL(fileURL).setReferer(fileURL).setActionFromFormByIndex(1, true).removeParameter("method_premium").setParameter("method_free","Free Download").toHttpMethod();//TODO
-            String stringWait = PlugUtils.getStringBetween(getContentAsString(),"<span id=\"countdown\">","</span>")    ;
+            httpMethod = getMethodBuilder().setAction(fileURL).setBaseURL(fileURL).setReferer(fileURL).setActionFromFormByIndex(1, true).removeParameter("method_premium").setParameter("method_free", "Free Download").toHttpMethod();//TODO
+            String stringWait = PlugUtils.getStringBetween(getContentAsString(), "<span id=\"countdown\">", "</span>");
             int wait = new Integer(stringWait);
             downloadTask.sleep(wait);
 
-            httpMethod = getMethodBuilder().setAction(fileURL).setBaseURL(fileURL).setReferer(fileURL).setActionFromFormByName("F1", true).removeParameter("method_premium").setParameter("method_free","Free Download").toHttpMethod();//TODO
+             HttpMethod finalMethod = stepCaptcha();
+            //httpMethod = getMethodBuilder().setAction(fileURL).setBaseURL(fileURL).setReferer(fileURL).setActionFromFormByName("F1", true).removeParameter("method_premium").setParameter("method_free", "Free Download").toHttpMethod();//TODO
             //here is the download link extraction
-            if (!tryDownloadAndSaveFile(httpMethod)) {
+            if (!tryDownloadAndSaveFile(finalMethod)) {
                 checkProblems();//if downloading failed
                 logger.warning(getContentAsString());//log the info
                 throw new PluginImplementationException();//some unknown problem
@@ -96,10 +93,30 @@ class BebasUploadFileRunner extends AbstractRunner {
         }
     }
 
+    private HttpMethod stepCaptcha() throws Exception {
+
+        CaptchaSupport captchaSupport = getCaptchaSupport();
+        String s = getMethodBuilder().setActionFromImgSrcWhereTagContains("captchas").getAction();
+        logger.info("Captcha URL " + s);
+        String captcha = captchaSupport.getCaptcha(s);
+        if (captcha == null) {
+            throw new CaptchaEntryInputMismatchException();
+        } else {
+            final HttpMethod postMethod = getMethodBuilder().setActionFromFormByName("F1", true).setAction(fileURL).
+                    setParameter("code", captcha).removeParameter("method_premium").toPostMethod();
+            return postMethod;
+
+        }
+    }
+
     private void checkProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
         if (contentAsString.contains("File Not Found")) {//TODO
             throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+        //Wrong captcha
+         if (contentAsString.contains("Wrong captcha")) {//TODO
+            throw new YouHaveToWaitException("Wrong captcha",1); //let to know user in FRD
         }
     }
 

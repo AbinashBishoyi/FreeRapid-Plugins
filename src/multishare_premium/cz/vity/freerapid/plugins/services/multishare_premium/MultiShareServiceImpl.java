0e14ea7da894bf123662bea80ad24f56428122b0
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:31:03 +0530

    SVN Update# 2166 by JPEXS: NEW: MultiShare premium plugin

---
+++ src/multishare_premium/cz/vity/freerapid/plugins/services/multishare_premium/MultiShareServiceImpl.java
@@ -0,0 +1,53 @@
+package cz.vity.freerapid.plugins.services.multishare_premium;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author JPEXS
+ */
+public class MultiShareServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "MultiShare.cz (premium)";
+    private static final String PLUGIN_CONFIG_FILE = "plugin_MultiSharePremium.xml";
+    private volatile PremiumAccount config;
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 9;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new MultiShareRunner();
+    }
+
+    @Override
+    public void showOptions() throws Exception {
+        PremiumAccount pa = showConfigDialog();
+        if (pa != null) config = pa;
+    }
+
+    public PremiumAccount showConfigDialog() throws Exception {
+        return showAccountDialog(getConfig(), "MultiShare", PLUGIN_CONFIG_FILE);
+    }
+
+    PremiumAccount getConfig() throws Exception {
+        if (config == null) {
+            synchronized (MultiShareServiceImpl.class) {
+                config = getAccountConfigFromFile(PLUGIN_CONFIG_FILE);
+            }
+        }
+
+        return config;
+    }
+
+}

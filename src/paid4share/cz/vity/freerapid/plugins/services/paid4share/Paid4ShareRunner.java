53a806e3371d1d0bf02bd9e97d76ea7451a93b53
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:31:48 +0530

    SVN Update# 1627 by Vity: paid4share + new plugin API

---
+++ src/paid4share/cz/vity/freerapid/plugins/services/paid4share/Paid4ShareRunner.java
@@ -0,0 +1,76 @@
+package cz.vity.freerapid.plugins.services.paid4share;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.InvalidURLOrServiceProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.exceptions.YouHaveToWaitException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * @author Vity
+ * @since 0.82
+ */
+class Paid4ShareRunner extends AbstractRunner {
+    private static final Logger logger = Logger.getLogger(Paid4ShareRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception {
+        super.runCheck();
+        final HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
+
+        if (makeRedirectedRequest(httpMethod)) {
+            checkProblems();
+            checkNameAndSize();
+        } else {
+            throw new InvalidURLOrServiceProblemException("Invalid URL or service problem");
+        }
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
+
+        if (makeRedirectedRequest(httpMethod)) {
+            checkProblems();
+            checkNameAndSize();
+            final HttpMethod m = getMethodBuilder().setReferer(fileURL).setActionFromAHrefWhereATagContains("Download file").toHttpMethod();
+
+            if (!tryDownloadAndSaveFile(m)) {
+                checkProblems();
+                logger.warning(getContentAsString());
+                throw new IOException("File input stream is empty");
+            }
+        } else {
+            throw new InvalidURLOrServiceProblemException("Invalid URL or service problem");
+        }
+    }
+
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+
+        final String contentAsString = getContentAsString();
+
+        if (contentAsString.contains("You have got max allowed download sessions from the same IP")) {
+            throw new YouHaveToWaitException("You have got max allowed download sessions from the same IP!", 60);
+        }
+
+        if (contentAsString.contains("was not found")) {
+            throw new URLNotAvailableAnymoreException("File was not found");
+        }
+    }
+
+    private void checkNameAndSize() throws ErrorDuringDownloadingException {
+        PlugUtils.checkName(httpFile, getContentAsString(), "<td align=left width=150px>", "</td>");
+        PlugUtils.checkFileSize(httpFile, getContentAsString(), "File size:</b></td>\n<td align=left>", "</td>");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+}

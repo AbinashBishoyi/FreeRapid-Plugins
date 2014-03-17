24128eeca968f7588d52341163e8582b18e0a927
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 12:26:12 +0530

    SVN Update# 3497 by ntoskrnl: new plugin - BitShare.com_premium

---
+++ src/bitshare_premium/cz/vity/freerapid/plugins/services/bitshare_premium/BitShareServiceImpl.java
@@ -0,0 +1,54 @@
+package cz.vity.freerapid.plugins.services.bitshare_premium;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.hoster.PremiumAccount;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author ntoskrnl
+ */
+public class BitShareServiceImpl extends AbstractFileShareService {
+    private static final String PLUGIN_CONFIG_FILE = "plugin_BitSharePremium.xml";
+    private volatile PremiumAccount config;
+
+    @Override
+    public String getName() {
+        return "bitshare.com_premium";
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new BitShareFileRunner();
+    }
+
+    @Override
+    public void showOptions() throws Exception {
+        PremiumAccount pa = showConfigDialog();
+        if (pa != null) config = pa;
+    }
+
+    PremiumAccount showConfigDialog() throws Exception {
+        return showAccountDialog(getConfig(), "BitShare", PLUGIN_CONFIG_FILE);
+    }
+
+    PremiumAccount getConfig() throws Exception {
+        synchronized (BitShareServiceImpl.class) {
+            if (config == null) {
+                config = getAccountConfigFromFile(PLUGIN_CONFIG_FILE);
+            }
+        }
+        return config;
+    }
+
+    void setConfig(final PremiumAccount config) {
+        this.config = config;
+    }
+
+}

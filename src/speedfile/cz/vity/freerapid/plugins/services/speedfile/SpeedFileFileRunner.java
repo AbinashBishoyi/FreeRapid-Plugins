3e7168c9685bc0091c7d00c3bd09d7a9a6214de5
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 11:46:03 +0530

    SVN Update# 3144 by Vity: new plugin bezvadata.cz, not finished speedfile.cz

---
+++ src/speedfile/cz/vity/freerapid/plugins/services/speedfile/SpeedFileFileRunner.java
@@ -0,0 +1,75 @@
+package cz.vity.freerapid.plugins.services.speedfile;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.util.logging.Logger;
+
+/**
+ * Class which contains main code
+ *
+ * @author Vity
+ */
+class SpeedFileFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(SpeedFileFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
+        PlugUtils.checkName(httpFile, content, "og:title\" content=\"", "\" />");
+        PlugUtils.checkFileSize(httpFile, content, "<big>", "|");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
+        if (makeRedirectedRequest(method)) { //we make the main request
+            final String contentAsString = getContentAsString();//check for response
+            checkProblems();//check problems
+            checkNameAndSize(contentAsString);//extract file name and size from the page
+            final HttpMethod httpMethod = getMethodBuilder().setBaseURL(fileURL).setReferer(fileURL).setActionFromAHrefWhereATagContains("<span>").toHttpMethod();
+            logger.info(httpMethod.getURI().toString());
+            makeRequest(httpMethod);
+            downloadTask.sleep(11);
+            //here is the download link extraction
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkProblems();//if downloading failed
+                throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("File Not Found")) {//TODO
+            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+        if (contentAsString.contains("chyba 404")) {
+            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+    }
+
+}

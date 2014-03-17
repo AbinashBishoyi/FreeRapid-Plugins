541396f68629841b98fbdc6c9f65bb4dbaeb0ed7
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 06:28:27 +0530

    SVN Update# 893 by Lukiz: aktualizace netload.in

--- src/netloadin/cz/vity/freerapid/plugins/services/netloadin/NetloadInRunner.java
+++ src/netloadin/cz/vity/freerapid/plugins/services/netloadin/NetloadInRunner.java
@@ -2,39 +2,50 @@ package cz.vity.freerapid.plugins.services.netloadin;
 
 import cz.vity.freerapid.plugins.exceptions.*;
 import cz.vity.freerapid.plugins.webclient.*;
-import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.params.HttpClientParams;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Ladislav Vitasek, Ludek Zika
  */
-class NetloadInRunner {
+class NetloadInRunner extends AbstractRunner {
     private final static Logger logger = Logger.getLogger(NetloadInRunner.class.getName());
-    private HttpDownloadClient client;
-    private HttpFileDownloader downloader;
     private static final String HTTP_NETLOAD = "http://netload.in";
 
     private String initURL;
     private String enterURL;
 
+    public void runCheck(HttpFileDownloader downloader) throws Exception {
+        super.runCheck(downloader);
+        final GetMethod getMethod = client.getGetMethod(fileURL);
+        getMethod.setFollowRedirects(true);
+        if (makeRequest(getMethod)) {
+            if (!client.getContentAsString().contains("Netload")) {
+                logger.warning(client.getContentAsString());
+                throw new InvalidURLOrServiceProblemException("Invalid URL or unindentified service");
+            }
+            Matcher matcher = PlugUtils.matcher("we don't host the requested file", client.getContentAsString());
+            if (matcher.find()) {
+                throw new URLNotAvailableAnymoreException(String.format("<b>Requested file isn't hosted. Probably was deleted.</b>"));
+            }
+           httpFile.setFileState(FileState.CHECKED_AND_EXISTING); 
+        } else
+            throw new PluginImplementationException("Problem with a connection to service.\nCannot find requested page content");
+    }
+
+
     public void run(HttpFileDownloader downloader) throws Exception {
-        this.downloader = downloader;
-        HttpFile httpFile = downloader.getDownloadFile();
-        client = downloader.getClient();
-        final String fileURL = httpFile.getFileUrl().toString();
+        super.run(downloader);
         initURL = fileURL;
         logger.info("Starting download in TASK " + fileURL);
         final GetMethod getMethod = client.getGetMethod(fileURL);
         getMethod.setFollowRedirects(true);
-        if (client.makeRequest(getMethod) == HttpStatus.SC_OK) {
+        if (makeRequest(getMethod)) {
             do {
                 stepEnterPage(client.getContentAsString());
                 if (!client.getContentAsString().contains("Please enter the Securitycode")) {
@@ -46,27 +57,27 @@ class NetloadInRunner {
 
             } while (client.getContentAsString().contains("You may forgot the security code or it might be wrong"));
 
-            Matcher matcher = Pattern.compile(">([0-9.]+ .B)</div>", Pattern.MULTILINE).matcher(client.getContentAsString());
+            Matcher matcher = PlugUtils.matcher(">([0-9.]+ .B)</div>", client.getContentAsString());
             if (matcher.find()) {
                 logger.info("File size " + matcher.group(1));
                 httpFile.setFileSize(PlugUtils.getFileSizeFromString(matcher.group(1)));
             }
             // download: JFC107.part1.rar
-            matcher = Pattern.compile("download:\\s*([^<]*)", Pattern.MULTILINE).matcher(client.getContentAsString());
+            matcher = PlugUtils.matcher("download:\\s*([^<]*)", client.getContentAsString());
             if (matcher.find()) {
                 final String fn = matcher.group(1);
                 logger.info("File name " + fn);
                 httpFile.setFileName(fn);
             } else logger.warning("File name was not found" + client.getContentAsString());
 
-            matcher = Pattern.compile("please wait.*countdown\\(([0-9]+)", Pattern.MULTILINE).matcher(client.getContentAsString());
+            matcher = PlugUtils.matcher("please wait.*countdown\\(([0-9]+)", client.getContentAsString());
             if (matcher.find()) {
                 int time = Integer.parseInt(matcher.group(1)) / 100;
                 downloader.sleep(time + 1);
                 if (downloader.isTerminated())
                     throw new InterruptedException();
             }
-            matcher = Pattern.compile("href=\"([^\"]*)\" >Click here for the download", Pattern.MULTILINE).matcher(client.getContentAsString());
+            matcher = PlugUtils.matcher("href=\"([^\"]*)\" >Click here for the download", client.getContentAsString());
             if (matcher.find()) {
                 String s = matcher.group(1);
                 logger.info("Found File URL - " + s);
@@ -74,19 +85,9 @@ class NetloadInRunner {
                     throw new InterruptedException();
                 httpFile.setState(DownloadState.GETTING);
                 final GetMethod method = client.getGetMethod(s);
-
-                try {
-                    final InputStream inputStream = client.makeFinalRequestForFile(method, httpFile);
-                    if (inputStream != null) {
-                        downloader.saveToFile(inputStream);
-                    } else {
-                        checkProblems();
-                        throw new IOException("File input stream is empty.");
-                    }
-
-                } finally {
-                    method.abort();
-                    method.releaseConnection();
+                if (!tryDownload(method)) {
+                    checkProblems();
+                    throw new IOException("File input stream is empty.");
                 }
             } else {
                 checkProblems();
@@ -104,7 +105,7 @@ class NetloadInRunner {
     }
 
     private boolean stepEnterPage(String contentAsString) throws Exception {
-        Matcher matcher = Pattern.compile("class=\"Free_dl\"><a href=\"([^\"]*)\"", Pattern.MULTILINE).matcher(client.getContentAsString());
+        Matcher matcher = PlugUtils.matcher("class=\"Free_dl\"><a href=\"([^\"]*)\"", contentAsString);
         if (!matcher.find()) {
             checkProblems();
             throw new InvalidURLOrServiceProblemException("Invalid URL or unindentified service");
@@ -117,9 +118,8 @@ class NetloadInRunner {
         enterURL = HTTP_NETLOAD + s;
         method1.setFollowRedirects(true);
         client.getHTTPClient().getParams().setBooleanParameter(HttpClientParams.ALLOW_CIRCULAR_REDIRECTS, true);
-        client.getHTTPClient().getParams().setParameter("http.protocol.single-cookie-header", true);
 
-        if (client.makeRequest(method1) != HttpStatus.SC_OK) {
+        if (!makeRequest(method1)) {
             throw new PluginImplementationException("Problem with a connection to service.\nCannot find requested page content");
         }
 
@@ -130,16 +130,16 @@ class NetloadInRunner {
     private boolean stepCaptcha(String contentAsString) throws Exception {
         if (contentAsString.contains("Please enter the Securitycode")) {
 
-            Matcher matcher = Pattern.compile("src=\"(share\\/includes\\/captcha.*?)\"", Pattern.MULTILINE).matcher(contentAsString);
+            Matcher matcher = PlugUtils.matcher("src=\"(share\\/includes\\/captcha.*?)\"", contentAsString);
             if (matcher.find()) {
                 String s = "/" + replaceEntities(matcher.group(1));
-                String captcha = downloader.getCaptcha(HTTP_NETLOAD + s);
+                String captcha = getCaptchaSupport().getCaptcha(HTTP_NETLOAD + s);
                 if (captcha == null) {
                     throw new CaptchaEntryInputMismatchException();
                 } else {
 
-                    String file_id = getParameter("file_id", contentAsString);
-                    matcher = Pattern.compile("form method\\=\"post\" action\\=\"([^\"]*)\"", Pattern.MULTILINE).matcher(contentAsString);
+                    String file_id = PlugUtils.getParameter("file_id", contentAsString);
+                    matcher = PlugUtils.matcher("form method\\=\"post\" action\\=\"([^\"]*)\"", contentAsString);
                     if (!matcher.find()) {
                         throw new PluginImplementationException("Captcha form action was not found");
                     }
@@ -150,7 +150,7 @@ class NetloadInRunner {
                     postMethod.addParameter("captcha_check", captcha);
                     postMethod.addParameter("start", "");
 
-                    if (client.makeRequest(postMethod) == HttpStatus.SC_OK) {
+                    if (makeRequest(postMethod)) {
 
                         return true;
                     }
@@ -164,22 +164,14 @@ class NetloadInRunner {
         return false;
     }
 
-    private String getParameter(String s, String contentAsString) throws PluginImplementationException {
-        Matcher matcher = Pattern.compile("name=\"" + s + "\"[^v>]*value=\"([^\"]*)\"", Pattern.MULTILINE).matcher(contentAsString);
-        if (matcher.find()) {
-            return matcher.group(1);
-        } else
-            throw new PluginImplementationException("Parameter " + s + " was not found");
-    }
-
     private void checkProblems() throws ServiceConnectionProblemException, YouHaveToWaitException, URLNotAvailableAnymoreException {
         Matcher matcher;
-        matcher = Pattern.compile("You could download your next file in.*countdown\\(([0-9]+)", Pattern.MULTILINE).matcher(client.getContentAsString());
+        matcher = PlugUtils.matcher("You could download your next file in.*countdown\\(([0-9]+)", client.getContentAsString());
         if (matcher.find()) {
             final int time = Integer.parseInt(matcher.group(1)) / 6000;
             throw new YouHaveToWaitException(String.format("<b> You could download your next file in %s minutes", time), time * 60);
         }
-        matcher = Pattern.compile("Sorry, we don't host the requested file", Pattern.MULTILINE).matcher(client.getContentAsString());
+        matcher = PlugUtils.matcher("Sorry, we don't host the requested file", client.getContentAsString());
         if (matcher.find()) {
             throw new URLNotAvailableAnymoreException(String.format("<b>Requested file isn't hosted. Probably was deleted.</b>"));
         }

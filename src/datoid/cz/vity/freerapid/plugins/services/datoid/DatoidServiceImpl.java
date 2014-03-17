e81649383d806d93e593591bdbafa713eb6f53f1
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 14:57:29 +0530

    SVN Update# 4572 by birchie: New Plugin:  Datoid.cz       Fixed:  WebShare.cz -- https urls recognised

---
+++ src/datoid/cz/vity/freerapid/plugins/services/datoid/DatoidServiceImpl.java
@@ -0,0 +1,28 @@
+package cz.vity.freerapid.plugins.services.datoid;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author birchie
+ */
+public class DatoidServiceImpl extends AbstractFileShareService {
+
+    @Override
+    public String getName() {
+        return "datoid.cz";
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new DatoidFileRunner();
+    }
+
+}

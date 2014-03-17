2f377ba7f248cdd27508243941b3b0586f5b19a6
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:37:34 +0530

    SVN Update# 2227 by RickCL: Implemented support to Crypt-It

---
+++ src/cryptit/cz/vity/freerapid/plugins/services/cryptit/CryptItServiceImpl.java
@@ -0,0 +1,32 @@
+package cz.vity.freerapid.plugins.services.cryptit;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author RickCL
+ */
+public class CryptItServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "cryptit.com";
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new CryptItRunner();
+    }
+
+    @Override
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return false;
+    }
+
+}
---
+++ src/cryptit/cz/vity/freerapid/plugins/services/cryptit/TestApp.java
@@ -0,0 +1,41 @@
+package cz.vity.freerapid.plugins.services.cryptit;
+
+import cz.vity.freerapid.plugins.dev.PluginDevApplication;
+import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import org.jdesktop.application.Application;
+
+import java.net.URL;
+
+/**
+ * @author RickCL
+ */
+public class TestApp extends PluginDevApplication {
+    @Override
+    protected void startup() {
+        final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
+        try {
+            //we set file URL
+            httpFile.setNewURL(new URL("http://crypt-it.com/s/HDB2ZD"));
+            //the way we connect to the internet
+            final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
+            //then we tries to download
+            final CryptItServiceImpl service = new CryptItServiceImpl(); //instance of service - of our plugin
+            testRun(service, httpFile, connectionSettings);//download file with service and its Runner
+            //all output goes to the console
+        } catch (Exception e) {//catch possible exception
+            e.printStackTrace(); //writes error output - stack trace to console
+        }
+        this.exit();//exit application
+    }
+
+    /**
+     * Main start method for running this application
+     * Called from IDE
+     *
+     * @param args arguments for application
+     */
+    public static void main(String[] args) {
+        Application.launch(TestApp.class, args);//starts the application - calls startup() internally
+    }
+}

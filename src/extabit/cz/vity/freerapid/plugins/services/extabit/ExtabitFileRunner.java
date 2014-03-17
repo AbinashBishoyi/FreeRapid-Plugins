6e1b35cde77f9d992c4ebb4d3e4fede6fe47a454
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 12:10:08 +0530

    SVN Update# 3380 by ntoskrnl: ExtaBit.com - fixed

--- src/extabit/cz/vity/freerapid/plugins/services/extabit/ExtabitFileRunner.java
+++ src/extabit/cz/vity/freerapid/plugins/services/extabit/ExtabitFileRunner.java
@@ -1,19 +1,17 @@
 package cz.vity.freerapid.plugins.services.extabit;
 
-import cz.vity.freerapid.plugins.exceptions.*;
+import cz.vity.freerapid.plugins.exceptions.CaptchaEntryInputMismatchException;
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
-import cz.vity.freerapid.plugins.webclient.hoster.CaptchaSupport;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
-import org.apache.commons.httpclient.Cookie;
 import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.methods.GetMethod;
 
-import java.io.IOException;
+import java.util.Random;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
-
-import javax.naming.ldap.Rdn;
 
 /**
  * Class which contains main code
@@ -24,43 +22,21 @@ class ExtabitFileRunner extends AbstractRunner {
     private final static Logger logger = Logger.getLogger(ExtabitFileRunner.class.getName());
 
     @Override
-    public void runCheck() throws Exception { //this method validates file
+    public void runCheck() throws Exception {
         super.runCheck();
-        if (makeFirstPossiblyRedirectedRequest()) {
-            checkFileProblems();
-            checkNameAndSize();//ok let's extract file name and size from the page
+        final HttpMethod method = getGetMethod(fileURL);
+        if (makeRedirectedRequest(method)) {
+            checkProblems();
+            checkNameAndSize();
         } else {
-            checkFileProblems();
+            checkProblems();
             throw new ServiceConnectionProblemException();
         }
     }
 
-    private boolean makeFirstPossiblyRedirectedRequest() throws IOException {
-        final GetMethod getMethod = getGetMethod(fileURL);//make first request
-        addCookie(new Cookie("extabit.com", "language", "en", "/", 86400, false));
-        if (!makeRequest(getMethod)) {
-            if (getMethod.getStatusCode() / 100 == 3) {
-                fileURL = getMethod.getResponseHeader("Location").getValue();
-                logger.info(String.format("Redirected, changing URL to %s", fileURL));
-                GetMethod m2 = getGetMethod(fileURL);
-                return makeRequest(m2);
-            }
-            return false;
-        }
-        return true;
-    }
-
     private void checkNameAndSize() throws ErrorDuringDownloadingException {
-        final Matcher nameMatcher = getMatcherAgainstContent("<div id=\"download_filename\"[^<>]*?>\\s*(.+?)\\s*</div>");
-        if (!nameMatcher.find())
-            unimplemented();
-        httpFile.setFileName(nameMatcher.group(1));
-
-        final Matcher sizeMatcher = getMatcherAgainstContent("<div class=\"download_filesize_en\">\\s*\\[(.+?)\\]\\s*</div>");
-        if (!sizeMatcher.find())
-            unimplemented();
-        httpFile.setFileSize(PlugUtils.getFileSizeFromString(sizeMatcher.group(1)));
-
+        PlugUtils.checkName(httpFile, getContentAsString(), "<title>", "download Extabit.com - file hosting</title>");
+        PlugUtils.checkFileSize(httpFile, getContentAsString(), "<th>Size:</th>\n<td class=\"col-fileinfo\">", "</td>");
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
@@ -68,84 +44,60 @@ class ExtabitFileRunner extends AbstractRunner {
     public void run() throws Exception {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
-        runCheck();
-        checkDownloadProblems();
-
-        while (getContentAsString().contains("cmn_form")) {
-            final HttpMethod req2 = getMethodBuilder()
-                    .setActionFromFormByName("cmn_form", true)
-                    .setParameter("capture", getCaptchaCode())
-                    .setBaseURL("http://extabit.com/")
-                    .toGetMethod(); // Post Method not work
-            if (!makeRequest(req2))
-                throw new ServiceConnectionProblemException();
+        HttpMethod method = getGetMethod(fileURL);
+        if (makeRedirectedRequest(method)) {
             checkProblems();
-        }
+            checkNameAndSize();
 
-        Matcher matcher = getMatcherAgainstContent("\\{\"ok\":true,\"href\":\"([^\"]*)\"\\}");
-        if( matcher.find() ) {
-            String url = Rdn.unescapeValue(matcher.group(1)).toString();
-            if( url.startsWith("?") )
-                url = fileURL + url;
-            final GetMethod req3 = getGetMethod( url );
-            if (!makeRequest(req3))
-                throw new ServiceConnectionProblemException();
-            checkProblems();
-        }
+            Matcher matcher;
+            final long startTime = System.currentTimeMillis();
+            do {
+                method = stepCaptcha();
+                final long toWait = startTime + 31000 - System.currentTimeMillis();
+                if (toWait > 0) {
+                    downloadTask.sleep((int) Math.ceil(toWait / 1000d));
+                }
+                if (!makeRedirectedRequest(method)) {
+                    checkProblems();
+                    throw new ServiceConnectionProblemException();
+                }
+            } while (!(matcher = getMatcherAgainstContent("\"href\"\\s*:\\s*\"(.+?)\"")).find());
 
-        final HttpMethod req4 = getMethodBuilder()
-                .setActionFromAHrefWhereATagContains("click here to download")
-                .toGetMethod();
-
-        if (!tryDownloadAndSaveFile(req4)) {
-            checkProblems();//if downloading failed
-            logger.warning(getContentAsString());//log the info
-
-            /* Since the server doesn't properly indicate
-             * multiple downloads condition, just assume it here
-             */
-            throw new ServiceConnectionProblemException("Unknown problem - will try again");
-        }
-    }
-
-    private String getCaptchaCode() throws ErrorDuringDownloadingException {
-        final CaptchaSupport cs = getCaptchaSupport();
-        final Matcher captchaMatcher = PlugUtils.matcher("<img src=\"(/cap[^\"]*)\"", getContentAsString());
-        if (!captchaMatcher.find())
-            unimplemented();
-
-        final String captcha = cs.getCaptcha("http://extabit.com" + captchaMatcher.group(1));
-        if (captcha == null)
-            throw new CaptchaEntryInputMismatchException();
-
-        return captcha;
-    }
-
-    private void unimplemented() throws PluginImplementationException {
-        //logger.warning(getContentAsString());//PluginImplementationException automatically logs the page content
-        throw new PluginImplementationException();//some unknown problem
-    }
-
-    private void checkFileProblems() throws ErrorDuringDownloadingException {
-        final String contentAsString = getContentAsString();
-        if (contentAsString.contains("<h1>File not found</h1>")) {
-            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
-        }
-        if (contentAsString.contains("\"err\"")) {
-            try {
-                makeFirstPossiblyRedirectedRequest();
-            }catch(IOException e) {
-                throw new PluginImplementationException(e);
+            method = getMethodBuilder()
+                    .setReferer(fileURL)
+                    .setAction(matcher.group(1).replace("\\/", "/"))
+                    .toGetMethod();
+            if (!tryDownloadAndSaveFile(method)) {
+                checkProblems();
+                throw new ServiceConnectionProblemException("Error starting download");
             }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
         }
-    }
-
-    private void checkDownloadProblems() {
     }
 
     private void checkProblems() throws ErrorDuringDownloadingException {
-        checkFileProblems();
-        checkDownloadProblems();
+        if (getContentAsString().contains("File not found")) {
+            throw new URLNotAvailableAnymoreException("File not found");
+        }
+        if (getContentAsString().contains("File is temporary unavailable")) {
+            throw new ServiceConnectionProblemException("File is temporarily unavailable");
+        }
+    }
+
+    private HttpMethod stepCaptcha() throws ErrorDuringDownloadingException {
+        final String captchaUrl = getMethodBuilder().setAction("/capture.gif?" + new Random().nextInt()).getEscapedURI();
+        final String captcha = getCaptchaSupport().getCaptcha(captchaUrl);
+        if (captcha == null) {
+            throw new CaptchaEntryInputMismatchException();
+        }
+        return getMethodBuilder()
+                .setReferer(fileURL)
+                .setAction(fileURL)
+                .setParameter("link", "1")
+                .setParameter("capture", captcha)
+                .toGetMethod();
     }
 
 }
--- src/extabit/cz/vity/freerapid/plugins/services/extabit/ExtabitServiceImpl.java
+++ src/extabit/cz/vity/freerapid/plugins/services/extabit/ExtabitServiceImpl.java
@@ -5,22 +5,19 @@ import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
 
 /**
  * Class that provides basic info about plugin
+ *
  * @author Thumb
  */
 public class ExtabitServiceImpl extends AbstractFileShareService {
 
+    @Override
     public String getName() {
         return "extabit.com";
     }
 
-    public int getMaxDownloadsFromOneIP() {
-        //TODO don't forget to update this value, in plugin.xml don't forget to update this value too
-        return 1;
-    }
-
     @Override
     public boolean supportsRunCheck() {
-        return true;//ok
+        return true;
     }
 
     @Override

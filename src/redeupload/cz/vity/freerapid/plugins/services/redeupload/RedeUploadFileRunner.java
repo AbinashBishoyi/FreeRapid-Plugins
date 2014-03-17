98af0461bad15971560c48e977ad7b0364f699db
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:38:41 +0530

    SVN Update# 3896 by birchie: New XFS Plugin : RedeUpload.com

---
+++ src/redeupload/cz/vity/freerapid/plugins/services/redeupload/RedeUploadFileRunner.java
@@ -0,0 +1,11 @@
+package cz.vity.freerapid.plugins.services.redeupload;
+
+import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+
+/**
+ * Class which contains main code
+ *
+ * @author birchie
+ */
+class RedeUploadFileRunner extends XFileSharingRunner {
+}

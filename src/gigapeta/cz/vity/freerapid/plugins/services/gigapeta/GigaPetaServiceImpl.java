02ab99ee7044df27693e05bfafd33947a93391af
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:05:12 +0530

    SVN Update# 1949 by wordrider: New plugin gigapeta.com

---
+++ src/gigapeta/cz/vity/freerapid/plugins/services/gigapeta/GigaPetaServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.gigapeta;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ * @author Thumb
+ */
+public class GigaPetaServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "gigapeta.com";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new GigaPetaFileRunner();
+    }
+
+}
---
+++ src/gigapeta/cz/vity/freerapid/plugins/services/gigapeta/TestApp.java
@@ -0,0 +1,45 @@
+package cz.vity.freerapid.plugins.services.gigapeta;
+
+import cz.vity.freerapid.plugins.dev.PluginDevApplication;
+import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
+import cz.vity.freerapid.plugins.webclient.interfaces.HttpFile;
+import org.jdesktop.application.Application;
+
+import java.net.URL;
+
+/**
+ * @author Thumb
+ */
+public class TestApp extends PluginDevApplication {
+    @Override
+    protected void startup() {
+        final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
+        try {
+            //we set file URL
+            httpFile.setNewURL(new URL(/*"http://gigapeta.com/dl/392427ad2d5f7"*/
+            		/*"http://gigapeta.com/dl/71696a9dfea1"*/
+            		"http://gigapeta.com/dl/401376a009f3a"));
+            //the way we connect to the internet
+            final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
+            //connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
+            //then we tries to download
+            final GigaPetaServiceImpl service = new GigaPetaServiceImpl(); //instance of service - of our plugin
+            //runcheck makes the validation
+            testRun(service, httpFile, connectionSettings);//download file with service and its Runner
+            //all output goes to the console
+        } catch (Exception e) {//catch possible exception
+            e.printStackTrace(); //writes error output - stack trace to console
+        }
+        this.exit();//exit application
+    }
+
+    /**
+     * Main start method for running this application
+     * Called from IDE
+     *
+     * @param args arguments for application
+     */
+    public static void main(String[] args) {
+        Application.launch(TestApp.class, args);//starts the application - calls startup() internally
+    }
+}

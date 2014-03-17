c22b1a5c6acb241aa736d2e62e26c2aad8e07c0a
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:32:55 +0530

    SVN Update# 1639 by Kajda: Plugin Web-Share.net

--- src/webshare/cz/vity/freerapid/plugins/services/webshare/WebShareFileRunner.java
+++ src/webshare/cz/vity/freerapid/plugins/services/webshare/WebShareFileRunner.java
@@ -1,9 +1,8 @@
-package cz.vity.freerapid.plugins.services.ziddu;
+package cz.vity.freerapid.plugins.services.webshare;
 
 import cz.vity.freerapid.plugins.exceptions.*;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
-import cz.vity.freerapid.plugins.webclient.MethodBuilder;
 import cz.vity.freerapid.plugins.webclient.hoster.CaptchaSupport;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.HttpMethod;
@@ -15,13 +14,14 @@ import java.util.logging.Logger;
  * @author Kajda
  * @since 0.82
  */
-class ZidduFileRunner extends AbstractRunner {
-    private static final Logger logger = Logger.getLogger(ZidduFileRunner.class.getName());
-    private static final String SERVICE_WEB = "http://downloads.ziddu.com";
+class WebShareFileRunner extends AbstractRunner {
+    private static final Logger logger = Logger.getLogger(WebShareFileRunner.class.getName());
+    private static final String SERVICE_WEB = "http://www.web-share.net";
 
     @Override
     public void runCheck() throws Exception {
         super.runCheck();
+        setPageEncoding("Windows-1250");
         final HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
 
         if (makeRedirectedRequest(httpMethod)) {
@@ -35,26 +35,19 @@ class ZidduFileRunner extends AbstractRunner {
     @Override
     public void run() throws Exception {
         super.run();
+        setPageEncoding("Windows-1250");
         logger.info("Starting download in TASK " + fileURL);
         HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
 
         if (makeRedirectedRequest(httpMethod)) {
             checkAllProblems();
             checkNameAndSize();
-            final MethodBuilder methodBuilder = getMethodBuilder();
-            httpMethod = methodBuilder.setReferer(fileURL).setActionFromFormByName("dfrm", true).toHttpMethod();
-            final String redirectURL = methodBuilder.getAction();
+            httpMethod = stepCaptcha();
 
-            if (makeRedirectedRequest(httpMethod)) {
-                httpMethod = stepCaptcha(redirectURL);
-
-                if (!tryDownloadAndSaveFile(httpMethod)) {
-                    checkAllProblems();
-                    logger.warning(getContentAsString());
-                    throw new IOException("File input stream is empty");
-                }
-            } else {
-                throw new ServiceConnectionProblemException();
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkAllProblems();
+                logger.warning(getContentAsString());
+                throw new IOException("File input stream is empty");
             }
         } else {
             throw new InvalidURLOrServiceProblemException("Invalid URL or service problem");
@@ -64,8 +57,8 @@ class ZidduFileRunner extends AbstractRunner {
     private void checkSeriousProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
 
-        if (contentAsString.contains("File not found")) {
-            throw new URLNotAvailableAnymoreException("File not found");
+        if (contentAsString.contains("Súbor sa nenašiel")) {
+            throw new URLNotAvailableAnymoreException("Súbor sa nenašiel");
         }
     }
 
@@ -73,28 +66,28 @@ class ZidduFileRunner extends AbstractRunner {
         checkSeriousProblems();
         final String contentAsString = getContentAsString();
 
-        if (contentAsString.contains("Please Enter Correct Verification Code")) {
-            throw new YouHaveToWaitException("Please Enter Correct Verification Code", 4);
+        if (contentAsString.contains("Zadali ste nesprávny overovací kód")) {
+            throw new YouHaveToWaitException("Zadali ste nesprávny overovací kód", 4);
         }
     }
 
     private void checkNameAndSize() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
-        PlugUtils.checkName(httpFile, contentAsString, "class=\"fontfamilyverdana textblue14\">", "<");
-        PlugUtils.checkFileSize(httpFile, contentAsString, "td height=\"18\" align=\"left\" class=\"fontfamilyverdana normal12blue\"><span class=\"fontfamilyverdana normal12black\">", "<");
+        PlugUtils.checkName(httpFile, contentAsString, "<br><h3>", "<");
+        PlugUtils.checkFileSize(httpFile, contentAsString, "Ve¾kos súboru: ", "<");
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
-    private HttpMethod stepCaptcha(String redirectURL) throws ErrorDuringDownloadingException {
+    private HttpMethod stepCaptcha() throws ErrorDuringDownloadingException {
         final CaptchaSupport captchaSupport = getCaptchaSupport();
-        final String captchaSrc = SERVICE_WEB + PlugUtils.getStringBetween(getContentAsString(), "\"", "\" align=\"absmiddle\" id=\"image\" name=\"image\"");
+        final String captchaSrc = SERVICE_WEB + PlugUtils.getStringBetween(getContentAsString(), "\"", "\" alt=\"code\" id=\"captcha_img\"");
         logger.info("Captcha URL " + captchaSrc);
         final String captcha = captchaSupport.getCaptcha(captchaSrc);
 
         if (captcha == null) {
             throw new CaptchaEntryInputMismatchException();
         } else {
-            return getMethodBuilder().setReferer(redirectURL).setActionFromFormByName("securefrm", true).setBaseURL(SERVICE_WEB).setParameter("securitycode", captcha).toHttpMethod();
+            return getMethodBuilder().setReferer(fileURL).setActionFromFormByName("fd", true).setBaseURL(SERVICE_WEB).setParameter("captcha", captcha).toHttpMethod();
         }
     }
 }
---
+++ src/webshare/cz/vity/freerapid/plugins/services/webshare/WebShareServiceImpl.java
@@ -0,0 +1,29 @@
+package cz.vity.freerapid.plugins.services.webshare;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Kajda
+ */
+public class WebShareServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "webshare.net";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 9;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new WebShareFileRunner();
+    }
+}

522864946a04616c019d2d451483233dc70a753c
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:03:15 +0530

    SVN Update# 1327 by Alex: Kewlshare +add new error handler

--- src/kewlshare/cz/vity/freerapid/plugins/services/kewlshare/KewlshareRunner.java
+++ src/kewlshare/cz/vity/freerapid/plugins/services/kewlshare/KewlshareRunner.java
@@ -25,11 +25,10 @@ class KewlshareRunner extends AbstractRunner {
         final GetMethod getMethod = getGetMethod(fileURL);
         if (makeRequest(getMethod)) {
             String checkcontent = getContentAsString();
-
             checkNameandSize(checkcontent);
-
-        } else
+        } else {
             throw new PluginImplementationException("Problem with a connection to service.\nCannot find requested page content");
+        }
     }
 
     @Override
@@ -37,36 +36,25 @@ class KewlshareRunner extends AbstractRunner {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
         final GetMethod getMethoda = getGetMethod(fileURL);
-
         if (makeRedirectedRequest(getMethoda)) {
             String contentAsString = getContentAsString();
             checkNameandSize(contentAsString);
-
-
-
-
-
             if (contentAsString.contains("name=\"selection")) {  //Confirm 1st Page
                 client.setReferer(fileURL);
                 final PostMethod page1 = getPostMethod(fileURL);
                 String[] parameters = new String[]{"nL", "cL", "selection"};
                 PlugUtils.addParameters(page1, contentAsString, parameters);
-
-
-                    if (makeRequest(page1)) { //Request page 2
-                        contentAsString = getContentAsString(); 
-                            if (contentAsString.contains("Please wait until the download begins")) { //Confirming Page 2
-                                
-                                downloadTask.sleep(5);
-                                client.setReferer(fileURL);
-                                final PostMethod page2 = getPostMethod(fileURL);
-                                String[] parameters2 = new String[]{"nL","cL", "act"};
-                                PlugUtils.addParameters(page2, contentAsString, parameters2);
-                                if (makeRequest(page2)) { //request page 3
-                                    contentAsString = getContentAsString();
-                                    if(contentAsString.contains("Download Now")) {  //Confirm page 3
-                                   
-
+                if (makeRequest(page1)) { //Request page 2
+                    contentAsString = getContentAsString();
+                    if (contentAsString.contains("Please wait until the download begins")) { //Confirming Page 2
+                        downloadTask.sleep(5);
+                        client.setReferer(fileURL);
+                        final PostMethod page2 = getPostMethod(fileURL);
+                        String[] parameters2 = new String[]{"nL", "cL", "act"};
+                        PlugUtils.addParameters(page2, contentAsString, parameters2);
+                        if (makeRequest(page2)) { //request page 3
+                            contentAsString = getContentAsString();
+                            if (contentAsString.contains("Download Now")) {  //Confirm page 3
                                 Matcher matcher = getMatcherAgainstContent("<form action=\"([^\"]+)");  //Find Action
                                 if (matcher.find()) { //find form action
                                     final PostMethod page3 = getPostMethod(matcher.group(1));
@@ -75,30 +63,27 @@ class KewlshareRunner extends AbstractRunner {
                                         logger.warning(getContentAsString());//something was really wrong, we will explore it from the logs :-)
                                         throw new IOException("File input stream is empty.");
                                     }
-
-
-                                } else throw new InvalidURLOrServiceProblemException("Can't find Page 3 Action");
-
-
-                            } else throw new InvalidURLOrServiceProblemException("Can't find Page 3");
-
-
-                        } else throw new InvalidURLOrServiceProblemException("Can't load Page 3");
-
-
-                    } else throw new InvalidURLOrServiceProblemException("Can't find Page 2"); //Can't find 2nd page
+                                } else {
+                                    throw new InvalidURLOrServiceProblemException("Can't find Page 3 Action");
+                                }
+                            } else {
+                                throw new InvalidURLOrServiceProblemException("Can't find Page 3");
+                            }
+                        } else {
+                            throw new InvalidURLOrServiceProblemException("Can't load Page 3");
+                        }
+                    } else {  //Can't find 2nd page
+                        checkProblems();
+                        throw new InvalidURLOrServiceProblemException("Can't find 2nd page");
+                    }
                 }
-
-
-            } else throw new InvalidURLOrServiceProblemException("Failed to requst Page 2"); //Can't request 2nd page
-
-
-        } else throw new InvalidURLOrServiceProblemException("Can't find 1st Page"); //Can't find first page
-
-
+            } else {
+                throw new InvalidURLOrServiceProblemException("Failed to requst Page 2"); //Can't request 2nd page
+            }
+        } else {
+            throw new InvalidURLOrServiceProblemException("Can't find 1st Page"); //Can't find first page
+        }
     }
-
-
 
 
     private void checkNameandSize(String content) throws Exception {
@@ -116,20 +101,24 @@ class KewlshareRunner extends AbstractRunner {
             logger.info("File name " + fileName);
             httpFile.setFileName(fileName);
 
-        } else logger.warning("File name was not found" + content);
+        } else {
+            logger.warning("File name was not found" + content);
+        }
 
-         Matcher smatcher = PlugUtils.matcher("(([0-9.]* .B))<", content);
+        Matcher smatcher = PlugUtils.matcher("(([0-9.]* .B))<", content);
         if (smatcher.find()) {
             final String fileSize = smatcher.group(1);
             logger.info("File size " + fileSize);
             httpFile.setFileSize(PlugUtils.getFileSizeFromString(fileSize));
 
-        } else logger.warning("File size was not found" + content);
+        } else {
+            logger.warning("File size was not found" + content);
+        }
 
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
 
     }
-   
+
 
     private void checkProblems() throws ServiceConnectionProblemException {
         if (getContentAsString().contains("already downloading")) {
@@ -138,6 +127,10 @@ class KewlshareRunner extends AbstractRunner {
         if (getContentAsString().contains("Currently a lot of users")) {
             throw new ServiceConnectionProblemException(String.format("<b>Kewlshare Error:</b><br>Currently a lot of users are downloading files."));
         }
+        if (getContentAsString().contains("We are forced to limit the access of this file")) {
+            throw new ServiceConnectionProblemException(String.format("<b>Kewlshare Error:</b><br>Currently a lot of users are downloading files."));
+        }
+        // We are forced to limit the access of this file
     }
 
 }

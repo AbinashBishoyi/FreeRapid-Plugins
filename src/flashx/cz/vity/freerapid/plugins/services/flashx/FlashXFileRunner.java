bf2c958d30113aa7e5bae174854fe99539fb74d7
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 14:34:07 +0530

    SVN Update# 4382 by ntoskrnl: new plugin - FlashX.tv

---
+++ src/flashx/cz/vity/freerapid/plugins/services/flashx/FlashXFileRunner.java
@@ -0,0 +1,80 @@
+package cz.vity.freerapid.plugins.services.flashx;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+
+import java.util.logging.Logger;
+
+/**
+ * Class which contains main code
+ *
+ * @author ntoskrnl
+ */
+class FlashXFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(FlashXFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception {
+        super.runCheck();
+        final HttpMethod method = getGetMethod(fileURL);
+        if (makeRedirectedRequest(method)) {
+            checkProblems();
+            checkNameAndSize();
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkNameAndSize() throws ErrorDuringDownloadingException {
+        final String name = PlugUtils.getStringBetween(getContentAsString(), "<div class=\"video_title\">", "</div>");
+        httpFile.setFileName(name + ".flv");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        HttpMethod method = getGetMethod(fileURL);
+        if (makeRedirectedRequest(method)) {
+            checkProblems();
+            checkNameAndSize();
+            method = getMethodBuilder().setActionFromIFrameSrcWhereTagContains("embed_player.php").toGetMethod();
+            if (!makeRedirectedRequest(method)) {
+                checkProblems();
+                throw new ServiceConnectionProblemException();
+            }
+            method = getMethodBuilder().setActionFromAHrefWhereATagContains("PLAY NOW").toGetMethod();
+            if (!makeRedirectedRequest(method)) {
+                checkProblems();
+                throw new ServiceConnectionProblemException();
+            }
+            method = getMethodBuilder().setActionFromTextBetween("config=", "\"").toGetMethod();
+            if (!makeRedirectedRequest(method)) {
+                checkProblems();
+                throw new ServiceConnectionProblemException();
+            }
+            method = getMethodBuilder().setActionFromTextBetween("<file>", "</file>").toGetMethod();
+            if (!tryDownloadAndSaveFile(method)) {
+                checkProblems();
+                throw new ServiceConnectionProblemException("Error starting download");
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        if (getContentAsString().contains("Video not found")) {
+            throw new URLNotAvailableAnymoreException("File not found");
+        }
+    }
+
+}

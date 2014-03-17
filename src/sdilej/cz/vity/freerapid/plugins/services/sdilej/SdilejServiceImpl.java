ce8ef0cca274c751095fead3db12fd9dd70e957e
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:37:53 +0530

    SVN Update# 1697 by Kajda: Plugin Sdilej.to

---
+++ src/sdilej/cz/vity/freerapid/plugins/services/sdilej/SdilejServiceImpl.java
@@ -0,0 +1,29 @@
+package cz.vity.freerapid.plugins.services.sdilej;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Kajda
+ */
+public class SdilejServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "sdilej.to";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new SdilejFileRunner();
+    }
+}

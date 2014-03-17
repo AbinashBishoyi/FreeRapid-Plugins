8f8e78b082cc4d7508aea65348819fea12498f0c
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 14:02:16 +0530

    SVN Update# 4090 by ntoskrnl: Stream.cz - fixed

--- src/streamcz/cz/vity/freerapid/plugins/services/streamcz/StreamCzRunner.java
+++ src/streamcz/cz/vity/freerapid/plugins/services/streamcz/StreamCzRunner.java
@@ -1,103 +1,72 @@
 package cz.vity.freerapid.plugins.services.streamcz;
 
-import cz.vity.freerapid.plugins.exceptions.*;
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
-import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.HttpMethod;
 
-import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 
 /**
- * @author Ladislav Vitasek, Ludek Zika
+ * @author Ladislav Vitasek
+ * @author Ludek Zika
+ * @author ntoskrnl
  */
 class StreamCzRunner extends AbstractRunner {
     private final static Logger logger = Logger.getLogger(StreamCzRunner.class.getName());
 
-    public StreamCzRunner() {
-        super();
-    }
-
     public void runCheck() throws Exception {
         super.runCheck();
-
-        final GetMethod getMethod = getGetMethod(fileURL);
-        if (makeRedirectedRequest(getMethod)) {
-            checkName(getContentAsString());
-        } else
-            throw new PluginImplementationException();
+        final HttpMethod method = getGetMethod(fileURL);
+        if (makeRedirectedRequest(method)) {
+            checkProblems();
+            checkName();
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
     }
 
     public void run() throws Exception {
         super.run();
-
-        final GetMethod getMethod = getGetMethod(fileURL);
-        getMethod.setFollowRedirects(true);
-        if (makeRedirectedRequest(getMethod)) {
-            if (getContentAsString().contains("stream.cz")) {
-                checkName(getContentAsString());
-
-                Matcher matcher = getMatcherAgainstContent("cdnID=([^&]*)&");
-                if (!matcher.find()) {
-                    checkProblems();
-                    logger.warning(getContentAsString());
-                    throw new IOException("Id parameter not found.");
-                }
-
-                String flvId = matcher.group(1);
-                String finalURL = "http://cdn-dispatcher.stream.cz/?id=" + flvId;
-                GetMethod finalMethod = client.getGetMethod(finalURL);
-
-                if (!tryDownloadAndSaveFile(finalMethod)) {
-                    checkProblems();
-                    logger.warning(getContentAsString());
-                    throw new IOException("File input stream is empty.");
-                }
-
-            } else {
+        logger.info("Starting download in TASK " + fileURL);
+        HttpMethod method = getGetMethod(fileURL);
+        if (makeRedirectedRequest(method)) {
+            checkProblems();
+            checkName();
+            method = getGetMethod("http://cdn-dispatcher.stream.cz/?id=" + getId());
+            if (!tryDownloadAndSaveFile(method)) {
                 checkProblems();
-                logger.info(getContentAsString());
-                throw new PluginImplementationException();
+                throw new ServiceConnectionProblemException("Error starting download");
             }
-        } else
-            throw new PluginImplementationException();
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
     }
 
-    private String sicherName(String s) {
-        Matcher matcher = PlugUtils.matcher("/[0-9]+-([^/]*)", s);
-        if (matcher.find()) {
-            return matcher.group(1);
+    private String getId() throws ErrorDuringDownloadingException {
+        final Matcher matcher = PlugUtils.matcher("/(\\d+)", fileURL);
+        if (!matcher.find()) {
+            throw new PluginImplementationException("Error parsing file URL");
         }
-        return "file01.flv";
+        return matcher.group(1);
     }
 
-    private void checkName(String content) throws Exception {
-
-        if (!content.contains("stream.cz")) {
-            logger.warning(getContentAsString());
-            throw new InvalidURLOrServiceProblemException("Invalid URL or unindentified service");
-        }
-        if (getContentAsString().contains("404 nenalezeno")) {
-            throw new URLNotAvailableAnymoreException("<b>404 nenalezeno.</b><br>");
-        }
-
-        Matcher matcher = PlugUtils.matcher("filename=([^&]*)&", content);
-        // odebiram jmeno
-        String fn;
-        if (matcher.find()) {
-            fn = matcher.group(1);
-        } else fn = sicherName(fileURL);
-        logger.info("File name " + fn);
-        httpFile.setFileName(fn);
-        // konec odebirani jmena
-
-
+    private void checkName() throws Exception {
+        final String name = PlugUtils.getStringBetween(getContentAsString(), "<h1 class=\"fn\">", "</h1>");
+        httpFile.setFileName(name + ".flv");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
-    private void checkProblems() throws ServiceConnectionProblemException, YouHaveToWaitException, URLNotAvailableAnymoreException {
-        if (getContentAsString().contains("404 nenalezeno")) {
-            throw new URLNotAvailableAnymoreException("<b>404 nenalezeno.</b><br>");
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        if (getContentAsString().contains("Stránku nebylo možné nalézt")) {
+            throw new URLNotAvailableAnymoreException("File not found");
         }
     }
 

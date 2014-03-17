718d99cfd449d447a481e197109f48df14196b1a
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:09:04 +0530

    SVN Update# 1389 by Vity: yourfiles.biz plugin

---
+++ src/yourfiles/cz/vity/freerapid/plugins/services/yourfiles/YourFilesServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.yourfiles;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Vity
+ *         Class that provides basic info about plugin
+ */
+public class YourFilesServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "yourfiles.biz";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;//i checked that before
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new YourFilesFileRunner();
+    }
+
+}

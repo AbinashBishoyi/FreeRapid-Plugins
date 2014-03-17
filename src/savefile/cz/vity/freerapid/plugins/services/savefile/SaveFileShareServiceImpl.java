ad4c6179c3963a210ec6010b2211be1464d7fab6
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 06:36:57 +0530

    SVN Update# 994 by Vity: savefile from Alex

---
+++ src/savefile/cz/vity/freerapid/plugins/services/savefile/SaveFileShareServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.savefile;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Ladislav Vitasek
+ */
+public class SaveFileShareServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "savefile.com";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new SaveFileRunner();
+    }
+
+}

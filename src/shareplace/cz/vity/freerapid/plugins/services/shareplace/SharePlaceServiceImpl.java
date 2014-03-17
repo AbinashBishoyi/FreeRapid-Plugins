f1bf385f9b15f2943c506b1ef3f6621a24be4cf2
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:21:44 +0530

    SVN Update# 2085 by wordrider: new plugin - SharePlace.com

---
+++ src/shareplace/cz/vity/freerapid/plugins/services/shareplace/SharePlaceServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.shareplace;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author ntoskrnl
+ */
+public class SharePlaceServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "shareplace.com";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 4;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new SharePlaceFileRunner();
+    }
+
+}

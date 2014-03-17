9c1d9bba131fb6b7801d757f31ccd555417eb99e
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 06:43:43 +0530

    SVN Update# 1071 by Alex: code cleaning

--- src/kewlshare/cz/vity/freerapid/plugins/services/kewlshare/KewlshareRunner.java
+++ src/kewlshare/cz/vity/freerapid/plugins/services/kewlshare/KewlshareRunner.java
@@ -24,11 +24,10 @@ class KewlshareRunner extends AbstractRunner {
         super.runCheck();
         final GetMethod getMethod = getGetMethod(fileURL);
         if (makeRequest(getMethod)) {
-            //checkNameandSize(getContentAsString());
             String checkcontent = getContentAsString();
 
-            checkName(checkcontent);
-            checkSize(checkcontent);
+            checkNameandSize(checkcontent);
+
         } else
             throw new PluginImplementationException("Problem with a connection to service.\nCannot find requested page content");
     }
@@ -38,26 +37,14 @@ class KewlshareRunner extends AbstractRunner {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
         final GetMethod getMethoda = getGetMethod(fileURL);
-        //getMethoda.setFollowRedirects(true);
-        //you can now use makeRedirectedRequest() for working with redirects
+
         if (makeRedirectedRequest(getMethoda)) {
             String contentAsString = getContentAsString();
-            //checkNameandSize(contentAsString);
-            checkName(contentAsString);
-            checkSize(contentAsString);
+            checkNameandSize(contentAsString);
 
-//            client.setReferer(fileURL);//referer
-//            final PostMethod method = getPostMethod(fileURL);//file url
-//            String[] parameters = new String[]{"nL", "cL", "selection"}; //array of parameter names for parsing
-//            PlugUtils.addParameters(method, contentAsString, parameters);
 
-            //1 Page - Free/Premium
-            //      <form action="" method="post">
-            //		<input type="hidden" name="nL" value="BDI=" />
-            //		<input type="hidden" name="cL" value="AjU=" />
-            //		<input type="hidden" name="selection" value="Free" />
-            //		<input  id="imageInput" type="image" border="0"  value="Free User" src="http://kewlshare.com/button/free.gif"  />
-            //		</form>
+
+
 
             if (contentAsString.contains("name=\"selection")) {  //Confirm 1st Page
                 client.setReferer(fileURL);
@@ -65,34 +52,20 @@ class KewlshareRunner extends AbstractRunner {
                 String[] parameters = new String[]{"nL", "cL", "selection"};
                 PlugUtils.addParameters(page1, contentAsString, parameters);
 
-                if (makeRequest(page1)) { //Request page 2
-                    contentAsString = getContentAsString();
-                    if (contentAsString.contains("Please wait until the download begins")) { //Confirming Page 2
-                        // Find wait timer var intervel=5;
-                        //2 Page -
 
-                        //<form action="" method="post">
-                        //<input type="hidden" name="nL" value="V2E=" />
-                        //<input type="hidden" name="cL" value="BDI=" />
-                        //<input type="hidden" name="act" value="captcha" />
-                        //<input id="imageInput" type="image" border="0" src="http://kewlshare.com/img/pod.gif"  value="Enter" /></div></div>
-                        //</form>
-
-                        //Matcher matcher = getMatcherAgainstContent("var interval=([0-9]+);");
-                        //String s = matcher.group(1);
-                        //int seconds = new Integer(s);
-                        downloadTask.sleep(5);
-                        client.setReferer(fileURL);
-                        final PostMethod page2 = getPostMethod(fileURL);
-                        String[] parameters2 = new String[]{"nL", "cL", "act"};
-                        PlugUtils.addParameters(page2, contentAsString, parameters2);
-                        if (makeRequest(page2)) { //request page 3
-                            contentAsString = getContentAsString();
-                            if (contentAsString.contains("Download Now")) {  //Confirm page 3
-                                //3 Page - Download now
-
-                                //<form action="http://lucky.kewlshare.com/dl.php/a09e4b6e7a335ddabf241bb91afbf9c9/Harry.Potter.And.The.Goblet.Of.Fire.2005.720p.nHD.x264.AAC.NhaNc3__mininova_.torrent" method="posT" name="post" target="_blank" onsubmit="downloadMe();" >
-                                //<input id="imageInput" type="image" border="0" value="Download  Harry.Potter.And.The.Goblet.Of.Fire.2005.720p.nHD.x264.AAC.NhaNc3__mininova_.torrent" src="http://kewlshare.com/img/down.gif"  /></form>
+                    if (makeRequest(page1)) { //Request page 2
+                        contentAsString = getContentAsString(); 
+                            if (contentAsString.contains("Please wait until the download begins")) { //Confirming Page 2
+                                
+                                downloadTask.sleep(5);
+                                client.setReferer(fileURL);
+                                final PostMethod page2 = getPostMethod(fileURL);
+                                String[] parameters2 = new String[]{"nL","cL", "act"};
+                                PlugUtils.addParameters(page2, contentAsString, parameters2);
+                                if (makeRequest(page2)) { //request page 3
+                                    contentAsString = getContentAsString();
+                                    if(contentAsString.contains("Download Now")) {  //Confirm page 3
+                                   
 
                                 Matcher matcher = getMatcherAgainstContent("<form action=\"([^\"]+)");  //Find Action
                                 if (matcher.find()) { //find form action
@@ -126,7 +99,17 @@ class KewlshareRunner extends AbstractRunner {
     }
 
 
-    private void checkName(String content) throws Exception {
+
+
+    private void checkNameandSize(String content) throws Exception {
+
+        if (!content.contains("kewlshare.com")) {
+            logger.warning(getContentAsString());
+            throw new InvalidURLOrServiceProblemException("Invalid URL or unindentified service");
+        }
+        if (content.contains("File doesn")) {
+            throw new InvalidURLOrServiceProblemException("<b>Kewlshare error:</b><br>File doesn't exist");
+        }
         Matcher xmatcher = PlugUtils.matcher("<!-- <title>: ([^<]+)", content);
         if (xmatcher.find()) {
             final String fileName = xmatcher.group(1).trim(); //method trim removes white characters from both sides of string
@@ -134,50 +117,19 @@ class KewlshareRunner extends AbstractRunner {
             httpFile.setFileName(fileName);
 
         } else logger.warning("File name was not found" + content);
-    }
 
-    private void checkSize(String content) throws Exception {
-        Matcher smatcher = PlugUtils.matcher("(([0-9.]* .B))<", content);
+         Matcher smatcher = PlugUtils.matcher("(([0-9.]* .B))<", content);
         if (smatcher.find()) {
             final String fileSize = smatcher.group(1);
             logger.info("File size " + fileSize);
             httpFile.setFileSize(PlugUtils.getFileSizeFromString(fileSize));
-            httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+
         } else logger.warning("File size was not found" + content);
 
-    }
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
 
-//    private void checkNameandSize(String content) throws Exception {
-//
-//        if (!content.contains("kewlshare.com")) {
-//            logger.warning(getContentAsString());
-//            throw new InvalidURLOrServiceProblemException("Invalid URL or unindentified service");
-//        }
-//        if (content.contains("File doesn")) {
-//            throw new URLNotAvailableAnymoreException("<b>Kewlshare error:</b><br>File doesn't exist");
-//        }
-//
-//        Matcher xmatcher = PlugUtils.matcher("File Name : <strong>([^|]+) || ([^<]+)", content);
-//        if (xmatcher.find()) {
-//            final String fileSize = xmatcher.group(2);
-//            logger.info("File size " + fileSize);
-//            httpFile.setFileSize(PlugUtils.getFileSizeFromString(fileSize));
-//
-//        } else logger.warning("File size was not found" + content);
-//
-//        xmatcher = PlugUtils.matcher("<!-- <title>: ([^<]+)", content);
-//        if (xmatcher.find()) {
-//            final String fileName = xmatcher.group(1).trim(); //method trim removes white characters from both sides of string
-//            logger.info("File name " + fileName);
-//            httpFile.setFileName(fileName);
-//
-//        } else logger.warning("File name was not found" + content);
-//
-//
-//        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
-//
-//    }
-//
+    }
+   
 
     private void checkProblems() throws ServiceConnectionProblemException {
         if (getContentAsString().contains("already downloading")) {

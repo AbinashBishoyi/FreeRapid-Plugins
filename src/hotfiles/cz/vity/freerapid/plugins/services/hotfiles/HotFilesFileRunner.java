60147a1df5c985213ded10b7cc9e240ad84615f6
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 14:12:40 +0530

    SVN Update# 4210 by ntoskrnl: new XFS plugin - HotFiles.ws

---
+++ src/hotfiles/cz/vity/freerapid/plugins/services/hotfiles/HotFilesFileRunner.java
@@ -0,0 +1,11 @@
+package cz.vity.freerapid.plugins.services.hotfiles;
+
+import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
+
+/**
+ * Class which contains main code
+ *
+ * @author ntoskrnl
+ */
+class HotFilesFileRunner extends XFileSharingRunner {
+}

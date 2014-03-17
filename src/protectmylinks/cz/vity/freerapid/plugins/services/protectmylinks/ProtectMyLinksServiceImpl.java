351209da85d83735ce710ae45b3bdf8d07f979f8
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:51:35 +0530

    SVN Update# 2349 by ntoskrnl: new plugin - Protect-My-Links.com

---
+++ src/protectmylinks/cz/vity/freerapid/plugins/services/protectmylinks/ProtectMyLinksServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.protectmylinks;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author ntoskrnl
+ */
+public class ProtectMyLinksServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "protect-my-links.com";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 9;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new ProtectMyLinksFileRunner();
+    }
+
+}

e3dabf7b677ad77e3aff9ac6927235f735a9c21f
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 15:12:50 +0530

    SVN Update# 4681 by tong2shot: JunoCloud.me - disable direct download handling.

--- src/junocloud/cz/vity/freerapid/plugins/services/junocloud/JunoCloudFileRunner.java
+++ src/junocloud/cz/vity/freerapid/plugins/services/junocloud/JunoCloudFileRunner.java
@@ -1,7 +1,9 @@
 package cz.vity.freerapid.plugins.services.junocloud;
 
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
 import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
 import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileSizeHandler;
+import org.apache.commons.httpclient.HttpMethod;
 
 import java.util.List;
 
@@ -18,4 +20,16 @@ class JunoCloudFileRunner extends XFileSharingRunner {
         fileSizeHandlers.add(new JunoCloudFileSizeHandler());
         return fileSizeHandlers;
     }
+
+    @Override
+    protected boolean handleDirectDownload(final HttpMethod method) throws Exception {
+        HttpMethod redirMethod = redirectToLocation(method);
+        if (!makeRedirectedRequest(redirMethod)) {
+            checkFileProblems();
+            throw new ServiceConnectionProblemException();
+        }
+        fileURL = redirMethod.getURI().toString();
+        return false;
+    }
+
 }

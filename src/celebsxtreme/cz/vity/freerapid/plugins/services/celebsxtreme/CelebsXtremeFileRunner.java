01100b7f1562d2b953df699fda5bba692552776a
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 12:11:05 +0530

    SVN Update# 3387 by tong2shot:

---
+++ src/celebsxtreme/cz/vity/freerapid/plugins/services/celebsxtreme/CelebsXtremeFileRunner.java
@@ -0,0 +1,89 @@
+package cz.vity.freerapid.plugins.services.celebsxtreme;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+
+/**
+ * Class which contains main code
+ *
+ * @author tong2shot
+ */
+class CelebsXtremeFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(CelebsXtremeFileRunner.class.getName());
+
+    /*
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
+        PlugUtils.checkName(httpFile, content, "FileNameLEFT", "FileNameRIGHT");//TODO
+        PlugUtils.checkFileSize(httpFile, content, "FileSizeLEFT", "FileSizeRIGHT");//TODO
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+    */
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
+        Matcher matcher = PlugUtils.matcher("/([^/]+\\.\\w+$)", fileURL);
+        if (!matcher.find()) {
+            logger.warning("File not found");
+            throw new URLNotAvailableAnymoreException("File not found");
+        }
+        httpFile.setFileName(matcher.group(1));
+
+        if (!tryDownloadAndSaveFile(method)) {
+            checkProblems();//if downloading failed
+            logger.warning(getContentAsString());//log the info
+            throw new PluginImplementationException();//some unknown problem
+        }
+        /*
+        if (makeRedirectedRequest(method)) { //we make the main request
+            final String contentAsString = getContentAsString();//check for response
+            checkProblems();//check problems
+            //checkNameAndSize(contentAsString);//extract file name and size from the page
+            final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setActionFromFormWhereTagContains("Download!", true).toHttpMethod();//TODO
+
+            //here is the download link extraction
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkProblems();//if downloading failed
+                logger.warning(getContentAsString());//log the info
+                throw new PluginImplementationException();//some unknown problem
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+        */
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("Page not found")) {
+            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+    }
+
+}

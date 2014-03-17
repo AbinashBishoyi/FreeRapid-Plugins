8d8226260c50c022f1a09423e31156f27d80d9eb
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:39:04 +0530

    SVN Update# 2241 by RickCL: Implemented support to HotFile Folder

---
+++ src/hotfilefolder/cz/vity/freerapid/plugins/services/hotfilefolder/HotFileFolderServiceImpl.java
@@ -0,0 +1,32 @@
+package cz.vity.freerapid.plugins.services.hotfilefolder;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author RickCL
+ */
+public class HotFileFolderServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "hotfilefolder";
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new HotFileFolderRunner();
+    }
+
+    @Override
+    public int getMaxDownloadsFromOneIP() {
+        return 1;
+    }
+
+    @Override
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return false;
+    }
+
+}

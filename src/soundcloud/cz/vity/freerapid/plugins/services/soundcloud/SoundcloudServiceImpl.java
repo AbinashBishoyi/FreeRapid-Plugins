edb3a3cf927bed366ea3394e7f63c0584d6a4367
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 09:22:36 +0530

    SVN Update# 2641 by Vity: added soundcloud plugin support

---
+++ src/soundcloud/cz/vity/freerapid/plugins/services/soundcloud/SoundcloudServiceImpl.java
@@ -0,0 +1,32 @@
+package cz.vity.freerapid.plugins.services.soundcloud;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ * @author Vity
+ */
+public class SoundcloudServiceImpl extends AbstractFileShareService {
+
+    @Override
+    public String getName() {
+        return "soundcloud.com";
+    }
+
+    @Override
+    public int getMaxDownloadsFromOneIP() {
+        return 10;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new SoundcloudFileRunner();
+    }
+
+}

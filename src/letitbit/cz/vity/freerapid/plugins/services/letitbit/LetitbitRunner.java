1e08e314782f161f122368b690de916cb01af8f8
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:42:30 +0530

    SVN Update# 1723 by Lukiz: update Letitbit

--- src/letitbit/cz/vity/freerapid/plugins/services/letitbit/LetitbitRunner.java
+++ src/letitbit/cz/vity/freerapid/plugins/services/letitbit/LetitbitRunner.java
@@ -3,16 +3,13 @@ package cz.vity.freerapid.plugins.services.letitbit;
 import cz.vity.freerapid.plugins.exceptions.*;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.DownloadState;
-import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.HttpMethod;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Ladislav Vitasek, Ludek Zika
@@ -23,8 +20,9 @@ class LetitbitRunner extends AbstractRunner {
     @Override
     public void runCheck() throws Exception {
         super.runCheck();
-        final GetMethod getMethod = getGetMethod(fileURL);
-        if (makeRequest(getMethod)) {
+        final HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
+
+        if (makeRequest(httpMethod)) {
             checkNameAndSize(getContentAsString());
         } else
             throw new PluginImplementationException();
@@ -39,43 +37,35 @@ class LetitbitRunner extends AbstractRunner {
             throw new URLNotAvailableAnymoreException(String.format("The requested file was not found"));
 
         }
-        Matcher matcher = PlugUtils.matcher("span> (.*? .b)</h1>", contentAsString);
-        if (matcher.find()) {
-            logger.info("File size " + matcher.group(1));
-            httpFile.setFileSize(PlugUtils.getFileSizeFromString(matcher.group(1)));
-        }
-        matcher = PlugUtils.matcher("File::</span>\\s*([^<]*)", contentAsString);
-        if (matcher.find()) {
-            final String fn = matcher.group(1);
-            logger.info("File name " + fn);
-            httpFile.setFileName(fn);
-            httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
-        } else logger.warning("File name was not found" + contentAsString);
+        PlugUtils.checkFileSize(httpFile, contentAsString, "File size::</span>", "</h1>");
+        PlugUtils.checkName(httpFile, contentAsString, "File::</span>", "</h1>");
+
     }
 
     @Override
     public void run() throws Exception {
         super.run();
         logger.info("Starting download in TASK " + fileURL);
-
-        final GetMethod getMethod = getGetMethod(fileURL);
-        getMethod.setFollowRedirects(true);
-        if (makeRequest(getMethod)) {
+        client.getHTTPClient().getParams().setBooleanParameter("dontUseHeaderFilename", true);
+        final HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
+        if (makeRedirectedRequest(httpMethod)) {
             checkNameAndSize(getContentAsString());
             boolean useOCR = true;
             while (true) {
                 stepCaptcha(useOCR);
                 Matcher matcher;
+
                 matcher = PlugUtils.matcher("src=\"([^?]*)\\?link=([^\"]*)\"", getContentAsString());
                 if (matcher.find()) {
-                    String t = matcher.group(2);
-                    logger.info("Download URL: " + t);
-                    downloadTask.sleep(4);
+                    String actionURL = matcher.group(2);
+                    String referer = matcher.group(1) + "?link=" + actionURL;
+                    final HttpMethod method = getMethodBuilder().
+                            setMethodAction(actionURL).
+                            setReferer(referer).toGetMethod();
+                    logger.info("Download URL: " + actionURL);
                     logger.info(getContentAsString());
+                    downloadTask.sleep(4);
                     httpFile.setState(DownloadState.GETTING);
-                    client.setReferer(matcher.group(1) + "?link=" + t);
-                    final GetMethod method = getGetMethod(t);
-                    method.setFollowRedirects(true);
                     if (!tryDownloadAndSaveFile(method)) {
                         checkProblems();
                         logger.info(getContentAsString());
@@ -87,7 +77,7 @@ class LetitbitRunner extends AbstractRunner {
                     if (getContentAsString().contains("javascript:history.go(-1);")) {
                         logger.info("Wrong captcha");
                         if (useOCR) useOCR = false;
-                        makeRequest(getMethod);
+                        makeRedirectedRequest(httpMethod);
                         continue;
                     }
                     checkProblems();
@@ -100,51 +90,46 @@ class LetitbitRunner extends AbstractRunner {
     }
 
     private void stepCaptcha(boolean useOCR) throws Exception {
-        Matcher matcher = Pattern.compile("form action=\"([^\"]*download[^\"]*)\"(.*)</form>", Pattern.MULTILINE | Pattern.DOTALL).matcher(getContentAsString());
-        if (!matcher.find()) {
+        try {
+            String captcha;
+            if (useOCR) captcha = readCaptchaImage();
+            else captcha = getCaptchaSupport().getCaptcha(getCaptchaImageURL());
+            if (captcha == null) throw new CaptchaEntryInputMismatchException();
+            downloadTask.sleep(4);
+            HttpMethod postMethod = getMethodBuilder().
+                    setActionFromFormWhereActionContains("download", true).
+                    setReferer(fileURL).setParameter("cap", captcha).setParameter("fix", "1").
+                    toPostMethod();
+
+            if (!makeRequest(postMethod)) {
+                logger.info(getContentAsString());
+                throw new PluginImplementationException();
+            }
+        } catch (BuildMethodException e) {
             checkProblems();
-            logger.warning(getContentAsString());
+            logger.warning(e.getMessage());
             throw new InvalidURLOrServiceProblemException("Free download link not found");
         }
-        String s = matcher.group(1);
-        String form = matcher.group(2);
-        logger.info("Submit form to - " + s);
-        client.setReferer(fileURL);
-        final PostMethod postMethod = getPostMethod(s);
-        PlugUtils.addParameters(postMethod, form, new String[]{"uid", "frameset", "uid2"});
-        String captcha;
-        if (useOCR) captcha = readCaptchaImage();
-         else captcha = getCaptchaSupport().getCaptcha(getCaptchaImageURL());
-        if (captcha == null)  throw new CaptchaEntryInputMismatchException(); 
-        postMethod.addParameter("cap", captcha);
-        postMethod.addParameter("fix", "1");
-        downloadTask.sleep(4);
-        if (!makeRequest(postMethod)) {
-            logger.info(getContentAsString());
-            throw new PluginImplementationException();
-        }
+
     }
 
     private String getCaptchaImageURL() throws Exception {
-          Matcher matcher = getMatcherAgainstContent("src='(.*?/cap.php[^']*)'");
-        if (matcher.find()) {
-            String s = PlugUtils.replaceEntities(matcher.group(1));
-            logger.info("Captcha - image " + s);
-          return s;
-     } else throw new PluginImplementationException("Captcha URL not found");
+        String s = getMethodBuilder().setActionFromImgSrcWhereTagContains("cap.php").getAction();
+        logger.info("Captcha - image " + s);
+        return s;
+    }
 
-   }
     private String readCaptchaImage() throws Exception {
-            String s = getCaptchaImageURL();
-            String captcha;
-            final BufferedImage captchaImage = getCaptchaSupport().getCaptchaImage(s);
-            final BufferedImage croppedCaptchaImage = captchaImage.getSubimage(1, 1, captchaImage.getWidth() - 2, captchaImage.getHeight() - 2);
-            captcha = PlugUtils.recognize(croppedCaptchaImage, "-C A-z-0-9");
-            if (captcha != null) {
-                logger.info("Captcha - OCR recognized " + captcha);
-            } else captcha = "";
-            captchaImage.flush();//askForCaptcha uvolnuje ten obrazek, takze tady to udelame rucne
-            return captcha;
+        String s = getCaptchaImageURL();
+        String captcha;
+        final BufferedImage captchaImage = getCaptchaSupport().getCaptchaImage(s);
+        final BufferedImage croppedCaptchaImage = captchaImage.getSubimage(1, 1, captchaImage.getWidth() - 2, captchaImage.getHeight() - 2);
+        captcha = PlugUtils.recognize(croppedCaptchaImage, "-C A-z-0-9");
+        if (captcha != null) {
+            logger.info("Captcha - OCR recognized " + captcha);
+        } else captcha = "";
+        captchaImage.flush();//askForCaptcha uvolnuje ten obrazek, takze tady to udelame rucne
+        return captcha;
 
     }
 

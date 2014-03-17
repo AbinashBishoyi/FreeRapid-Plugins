db7aed35c4e761eb519f4533dc1892203fca1033
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 07:34:44 +0530

    SVN Update# 1659 by Vity: wikiupload plugin fixed

--- src/wikiupload/cz/vity/freerapid/plugins/services/wikiupload/WikiuploadFileRunner.java
+++ src/wikiupload/cz/vity/freerapid/plugins/services/wikiupload/WikiuploadFileRunner.java
@@ -1,22 +1,17 @@
 package cz.vity.freerapid.plugins.services.wikiupload;
 
-import cz.vity.freerapid.plugins.exceptions.*;
+import cz.vity.freerapid.plugins.exceptions.CaptchaEntryInputMismatchException;
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
-import cz.vity.freerapid.plugins.webclient.MethodBuilder;
 import cz.vity.freerapid.plugins.webclient.hoster.CaptchaSupport;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.logging.Logger;
-import java.io.IOException;
-
-import org.apache.commons.httpclient.HttpMethod;
-
-import java.io.IOException;
 
 /**
  * Class which contains main code
@@ -27,33 +22,27 @@ class WikiuploadFileRunner extends AbstractRunner {
     private final static Logger logger = Logger.getLogger(WikiuploadFileRunner.class.getName());
     private static final String SERVICE_WEB = "http://www.wikiupload.com/";
 
-    private String fileCommentURL;
-    private String lName;
-    private String ID;
-
-
     @Override
     public void runCheck() throws Exception { //this method validates file
         super.runCheck();
-
-        fileURL = CommonURL(fileURL);
-
 
         checkName();//ok let's extract file name and size from the page
 
     }
 
+    //Java methods always start with lower capital
+    private String commonURL(String cURL) throws ErrorDuringDownloadingException {
 
-    private String CommonURL(String cURL) throws ErrorDuringDownloadingException {
-     
-        if (cURL.contains("wikiupload.com/download_page.php")) {
-            return cURL;
+        //notice that contains() method is case sensitive
+        final String lowercased = cURL.toLowerCase();
+        if (lowercased.contains("wikiupload.com/download_page.php")) {
+            return lowercased;
 
-        } else if (cURL.contains("wikiupload.com/middle_page.php")) {
-            return cURL.replaceFirst("wikiupload.com/middle_page.php", "wikiupload.com/download_page.php");
+        } else if (lowercased.contains("wikiupload.com/middle_page.php")) {
+            return lowercased.replaceFirst("wikiupload.com/middle_page.php", "wikiupload.com/download_page.php");
 
-        } else if (cURL.contains("wikiupload.com/comment.php")) {
-            return cURL.replaceFirst("wikiupload.com/comment.php", "wikiupload.com/download_page.php");
+        } else if (lowercased.contains("wikiupload.com/comment.php")) {
+            return lowercased.replaceFirst("wikiupload.com/comment.php", "wikiupload.com/download_page.php");
         } else {
             throw new PluginImplementationException("Invalid URL");
             //return null;
@@ -64,57 +53,48 @@ class WikiuploadFileRunner extends AbstractRunner {
 
 
     private void checkName() throws Exception {
-        fileCommentURL = fileURL;
-        fileCommentURL = fileCommentURL.replaceFirst("download_page.php", "comment.php");
+        final String lowercased = commonURL(fileURL).toLowerCase();
+        String fileCommentURL = lowercased.replaceFirst("download_page.php", "comment.php");
         final GetMethod getMethod = getGetMethod(fileCommentURL);
         if (makeRedirectedRequest(getMethod)) {
-            lName = fileURL.substring(fileURL.lastIndexOf("download_page.php"));
-            lName = "<td><a href='" + lName + "'><span class=\"title_6\">";
-            PlugUtils.checkName(httpFile, getContentAsString(), lName, "</span></a></td>");//TODO
+            String lName = "<span class=\"title_6\">"; //checkName takes always first match, this can simplified
+            PlugUtils.checkName(httpFile, getContentAsString(), lName, "</span></a></td>");
+            httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
         } else {
             throw new PluginImplementationException();
         }
-        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
     @Override
     public void run() throws Exception {
         super.run();
-        fileURL = CommonURL(fileURL);
+        fileURL = commonURL(fileURL);
         logger.info("Starting download in TASK " + fileURL);
         checkName();
-        HttpMethod httpMethod = getMethodBuilder().setAction(fileURL).toHttpMethod();
 
-        if (makeRedirectedRequest(httpMethod)) {
-            String contentAsString = getContentAsString();
-            ID = PlugUtils.getParameter("id", contentAsString);
-
-            final MethodBuilder methodBuilder = getMethodBuilder();
-            final String redirectURL = fileURL;
-
-            httpMethod = stepCaptcha(redirectURL);
-
-            if (!tryDownloadAndSaveFile(httpMethod)) {
-                //checkAllProblems();
-                logger.warning(getContentAsString());
-                throw new IOException("File input stream is empty");
-            }
-
-        } else {
-            throw new InvalidURLOrServiceProblemException("Invalid URL or service problem");
+        if (makeRedirectedRequest(getMethodBuilder().setAction(fileURL).toHttpMethod())) {
+            boolean succesful;
+            do {
+                succesful = true;
+                if (!tryDownloadAndSaveFile(stepCaptcha())) {
+                    checkProblems();
+                    succesful = false;
+                    logger.warning(getContentAsString());
+                }
+            } while (!succesful);
         }
     }
 
     private void checkProblems() throws ErrorDuringDownloadingException {
-        final String contentAsString = getContentAsString();
-        if (contentAsString.contains("File Not Found")) {//TODO
+        if ("".equals(getContentAsString())) {
             throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
         }
     }
 
-    private HttpMethod stepCaptcha(String redirectURL) throws ErrorDuringDownloadingException {
+    private HttpMethod stepCaptcha
+            () throws ErrorDuringDownloadingException {
         final CaptchaSupport captchaSupport = getCaptchaSupport();
-        final String captchaSrc = SERVICE_WEB + "turing_image.php";//PlugUtils.getStringBetween(getContentAsString(), "\"", "\" align=\"absmiddle\" id=\"image\" name=\"image\"");
+        final String captchaSrc = SERVICE_WEB + "turing_image.php";
         logger.info("Captcha URL " + captchaSrc);
         final String captcha = captchaSupport.getCaptcha(captchaSrc);
 
@@ -123,10 +103,8 @@ class WikiuploadFileRunner extends AbstractRunner {
         } else {
             //return getMethodBuilder().setReferer(fileURL).setAction(fileURL).setParameter("code", captcha).setParameter("id", ID).toHttpMethod();
             //logger.info("!!!!!!!!!!!!!!!! LAST CONTENT !!!!!!!!!!!!!!!!!!!" + getContentAsString());
-            
-            //return getMethodBuilder().setReferer(fileURL).setActionFromFormByIndex(1, true).setAction(fileURL).setParameter("code", captcha).toHttpMethod();
-            return getMethodBuilder().setActionFromImgSrcWhereTagContains("turing_image.php").setAction(fileURL).setParameter("code", captcha).toHttpMethod();
-            
+
+            return getMethodBuilder().setReferer(fileURL).setActionFromFormByIndex(1, true).setAction(fileURL).setAndEncodeParameter("code", captcha).toHttpMethod();
         }
 
 

28c25ee7e1f23e04b626f8d88a5820a8d5c3b826
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 06:42:35 +0530

    SVN Update# 1059 by Vity: user cash plugin added

---
+++ src/usercash/cz/vity/freerapid/plugins/services/usercash/UserCashRunner.java
@@ -0,0 +1,55 @@
+package cz.vity.freerapid.plugins.services.usercash;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.DownloadState;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+
+/**
+ * @author Ladislav Vitasek
+ */
+class UserCashRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(UserCashRunner.class.getName());
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL);
+        if (makeRedirectedRequest(method)) {
+            final Matcher matcher = getMatcherAgainstContent("<TITLE>(.+?)</TITLE>");
+            if (matcher.find()) {
+                final String s = matcher.group(1);
+                try {
+                    this.httpFile.setNewURL(new URL(s));
+                    this.httpFile.setPluginID("");
+                    this.httpFile.setState(DownloadState.QUEUED);
+                } catch (MalformedURLException e) {
+                    throw new URLNotAvailableAnymoreException("Invalid URL");
+                }
+            } else {
+                checkProblems();
+                throw new PluginImplementationException();
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("was not found")) {
+            throw new URLNotAvailableAnymoreException("Súbor bol zmazaný");
+        }
+    }
+
+}
---
+++ src/usercash/cz/vity/freerapid/plugins/services/usercash/UserCashServiceImpl.java
@@ -0,0 +1,30 @@
+package cz.vity.freerapid.plugins.services.usercash;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * @author Ladislav Vitasek
+ */
+public class UserCashServiceImpl extends AbstractFileShareService {
+    private static final String SERVICE_NAME = "usercash.com";
+
+    public String getName() {
+        return SERVICE_NAME;
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 10;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return false;
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new UserCashRunner();
+    }
+
+}

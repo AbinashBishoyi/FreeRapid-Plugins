658b7a9a3a5554b0b1e671150f1a1d5dc8be5948
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 09:06:30 +0530

    SVN Update# 2474 by ntoskrnl: FileFactory.com - fixed

--- src/filefactory/cz/vity/freerapid/plugins/services/filefactory/FileFactoryRunner.java
+++ src/filefactory/cz/vity/freerapid/plugins/services/filefactory/FileFactoryRunner.java
@@ -1,8 +1,10 @@
 package cz.vity.freerapid.plugins.services.filefactory;
 
 import cz.vity.freerapid.plugins.exceptions.*;
+import cz.vity.freerapid.plugins.services.recaptcha.ReCaptcha;
 import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.hoster.CaptchaSupport;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -17,6 +19,8 @@ import java.util.regex.Matcher;
 class FileFactoryFileRunner extends AbstractRunner {
     private static final Logger logger = Logger.getLogger(FileFactoryFileRunner.class.getName());
     private static final String SERVICE_WEB = "http://www.filefactory.com";
+    private String reCaptchaKey;
+    private String captchaCheck;
 
     @Override
     public void runCheck() throws Exception {
@@ -50,7 +54,13 @@ class FileFactoryFileRunner extends AbstractRunner {
                 makeRedirectedRequest(getMethod);
             }
 
-            final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setAction(getLink()).toGetMethod();
+            do {
+                if (!makeRedirectedRequest(stepCaptcha())) {
+                    throw new ServiceConnectionProblemException();
+                }
+            } while (!getContentAsString().contains("status:\"ok\""));
+
+            final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setActionFromTextBetween("path:\"", "\"").toGetMethod();
             if (makeRedirectedRequest(httpMethod)) {
                 checkAllProblems();
                 final String content = getContentAsString();
@@ -73,43 +83,43 @@ class FileFactoryFileRunner extends AbstractRunner {
         }
     }
 
+    private void checkNameAndSize(final String content) throws ErrorDuringDownloadingException {
+        PlugUtils.checkName(httpFile, content, "class=\"last\">", "</span");
+        PlugUtils.checkFileSize(httpFile, content, "<span>", "file uploaded");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
     private void checkSeriousProblems() throws ErrorDuringDownloadingException {
         final String contentAsString = getContentAsString();
-
         if (contentAsString.contains("Sorry, this file is no longer available")) {
             throw new URLNotAvailableAnymoreException("Sorry, this file is no longer available. It may have been deleted by the uploader, or has expired");
         }
-
         if (contentAsString.contains("What is FileFactory?")) {
             throw new URLNotAvailableAnymoreException("Page not found");
         }
-
         if (contentAsString.contains("Sorry, there are currently no free download slots available on this server")) {
-            throw new YouHaveToWaitException("Sorry, there are currently no free download slots available on this server", 60);
+            throw new ServiceConnectionProblemException("Sorry, there are currently no free download slots available on this server");
+        }
+        if (contentAsString.contains("All of the available ")) {
+            throw new ServiceConnectionProblemException("All of the available free download slots on this server are in use");
         }
     }
 
     private void checkAllProblems() throws ErrorDuringDownloadingException {
         checkSeriousProblems();
+
         final String contentAsString = getContentAsString();
-
         if (contentAsString.contains("Your download slot has expired")) {
-            throw new YouHaveToWaitException("Your download slot has expired.  Please try again", 60);
+            throw new ServiceConnectionProblemException("Your download slot has expired. Please try again");
         }
-
         if (contentAsString.contains("You are currently downloading too many files at once")) {
-            throw new YouHaveToWaitException("You are currently downloading too many files at once. Multiple simultaneous downloads are only permitted for Premium Members", 60);
+            throw new ServiceConnectionProblemException("You are currently downloading too many files at once. Multiple simultaneous downloads are only permitted for Premium Members");
         }
-
         Matcher matcher = getMatcherAgainstContent("You(?:r IP)? \\((.+?)\\) (?:has|have) exceeded the download limit for free users");
-
         if (matcher.find()) {
             final String userIP = matcher.group(1);
-
             matcher = getMatcherAgainstContent("Please wait (.+?) (.+?) to download more files");
-
             int waitSeconds = 2 * 60;
-
             if (matcher.find()) {
                 if (matcher.group(2).equals("minutes")) {
                     waitSeconds = 60 * Integer.parseInt(matcher.group(1));
@@ -117,15 +127,8 @@ class FileFactoryFileRunner extends AbstractRunner {
                     waitSeconds = Integer.parseInt(matcher.group(1));
                 }
             }
-
             throw new YouHaveToWaitException(String.format("You (%s) have exceeded the download limit for free users", userIP), waitSeconds);
         }
-    }
-
-    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
-        PlugUtils.checkName(httpFile, content, "class=\"last\">", "</span");
-        PlugUtils.checkFileSize(httpFile, content, "<span>", "file uploaded");
-        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
 
     private String getLink() throws Exception {
@@ -173,6 +176,30 @@ class FileFactoryFileRunner extends AbstractRunner {
         }
 
         throw new PluginImplementationException("Error parsing JavaScript: Variable '" + s + "' not found");
+    }
+
+    private HttpMethod stepCaptcha() throws Exception {
+        if (reCaptchaKey == null)
+            reCaptchaKey = PlugUtils.getStringBetween(getContentAsString(), "Recaptcha.create(\"", "\"");
+        if (captchaCheck == null) captchaCheck = PlugUtils.getStringBetween(getContentAsString(), "check:'", "'");
+        final ReCaptcha r = new ReCaptcha(reCaptchaKey, client);
+        final CaptchaSupport captchaSupport = getCaptchaSupport();
+
+        final String captchaURL = r.getImageURL();
+        logger.info("Captcha URL " + captchaURL);
+
+        final String captcha = captchaSupport.getCaptcha(captchaURL);
+        if (captcha == null) throw new CaptchaEntryInputMismatchException();
+        r.setRecognized(captcha);
+
+        final HttpMethod httpMethod = r.modifyResponseMethod(
+                getMethodBuilder()
+                        .setReferer(fileURL)
+                        .setAction("http://www.filefactory.com/file/checkCaptcha.php")
+                        .setParameter("check", captchaCheck)
+        ).toPostMethod();
+        httpMethod.addRequestHeader("X-Requested-With", "XMLHttpRequest");//use AJAX
+        return httpMethod;
     }
 
     @Override

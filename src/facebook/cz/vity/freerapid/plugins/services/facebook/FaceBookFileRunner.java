41b40d3ef07cb19e924fab0ae0a61df7a24b984e
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 11:43:40 +0530

    SVN Update# 3118 by Vity: facebook unicode file names update

--- src/facebook/cz/vity/freerapid/plugins/services/facebook/FaceBookFileRunner.java
+++ src/facebook/cz/vity/freerapid/plugins/services/facebook/FaceBookFileRunner.java
@@ -59,8 +59,8 @@ class FaceBookFileRunner extends AbstractRunner {
     }
 
     private void checkNameAndSize(final String content) throws Exception {
-        final String name = PlugUtils.getStringBetween(content, "\"video_title\":\"", "\"");
-        httpFile.setFileName(URLDecoder.decode(name, "UTF-8").replace(" [HQ]", "") + ".mp4");
+        final String name = fromLiteral(PlugUtils.getStringBetween(content, "\"video_title\":\"", "\""));
+        httpFile.setFileName(name.replace(" [HQ]", "") + ".mp4");
         url = PlugUtils.getStringBetween(content, "\"video_src\":\"", "\"").replace("\\/", "/");
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
     }
@@ -140,5 +140,130 @@ class FaceBookFileRunner extends AbstractRunner {
         final Header h = method.getResponseHeader("Content-Encoding");
         return h != null && "gzip".equalsIgnoreCase(h.getValue());
     }
+
+    //taken from StringUtil in JLibs
+    //TODO remove when 0.86 released with PlugUtils.unescapeUnicode
+    private static String fromLiteral(String str) {
+        StringBuilder buf = new StringBuilder();
+        int i = 0;
+        for (int len = str.length(); i < len; i++) {
+            char c = str.charAt(i);
+            label0:
+            switch (c) {
+                case 92: // '\\'
+                    if (i == str.length() - 1) {
+                        buf.append('\\');
+                        break;
+                    }
+                    c = str.charAt(++i);
+                    switch (c) {
+                        case 110: // 'n'
+                            buf.append('\n');
+                            break label0;
+
+                        case 116: // 't'
+                            buf.append('\t');
+                            break label0;
+
+                        case 114: // 'r'
+                            buf.append('\r');
+                            break label0;
+
+                        case 117: // 'u'
+                            int value = 0;
+                            for (int j = 0; j < 4; j++) {
+                                c = str.charAt(++i);
+                                switch (c) {
+                                    case 48: // '0'
+                                    case 49: // '1'
+                                    case 50: // '2'
+                                    case 51: // '3'
+                                    case 52: // '4'
+                                    case 53: // '5'
+                                    case 54: // '6'
+                                    case 55: // '7'
+                                    case 56: // '8'
+                                    case 57: // '9'
+                                        value = ((value << 4) + c) - 48;
+                                        break;
+
+                                    case 97: // 'a'
+                                    case 98: // 'b'
+                                    case 99: // 'c'
+                                    case 100: // 'd'
+                                    case 101: // 'e'
+                                    case 102: // 'f'
+                                        value = ((value << 4) + 10 + c) - 97;
+                                        break;
+
+                                    case 65: // 'A'
+                                    case 66: // 'B'
+                                    case 67: // 'C'
+                                    case 68: // 'D'
+                                    case 69: // 'E'
+                                    case 70: // 'F'
+                                        value = ((value << 4) + 10 + c) - 65;
+                                        break;
+
+                                    case 58: // ':'
+                                    case 59: // ';'
+                                    case 60: // '<'
+                                    case 61: // '='
+                                    case 62: // '>'
+                                    case 63: // '?'
+                                    case 64: // '@'
+                                    case 71: // 'G'
+                                    case 72: // 'H'
+                                    case 73: // 'I'
+                                    case 74: // 'J'
+                                    case 75: // 'K'
+                                    case 76: // 'L'
+                                    case 77: // 'M'
+                                    case 78: // 'N'
+                                    case 79: // 'O'
+                                    case 80: // 'P'
+                                    case 81: // 'Q'
+                                    case 82: // 'R'
+                                    case 83: // 'S'
+                                    case 84: // 'T'
+                                    case 85: // 'U'
+                                    case 86: // 'V'
+                                    case 87: // 'W'
+                                    case 88: // 'X'
+                                    case 89: // 'Y'
+                                    case 90: // 'Z'
+                                    case 91: // '['
+                                    case 92: // '\\'
+                                    case 93: // ']'
+                                    case 94: // '^'
+                                    case 95: // '_'
+                                    case 96: // '`'
+                                    default:
+                                        throw new IllegalArgumentException("Malformed \\uxxxx encoding.");
+                                }
+                            }
+
+                            buf.append((char) value);
+                            break;
+
+                        case 111: // 'o'
+                        case 112: // 'p'
+                        case 113: // 'q'
+                        case 115: // 's'
+                        default:
+                            buf.append(c);
+                            break;
+                    }
+                    break;
+
+                default:
+                    buf.append(c);
+                    break;
+            }
+        }
+
+        return buf.toString();
+    }
+
 
 }
--- src/facebook/cz/vity/freerapid/plugins/services/facebook/TestApp.java
+++ src/facebook/cz/vity/freerapid/plugins/services/facebook/TestApp.java
@@ -17,7 +17,8 @@ public class TestApp extends PluginDevApplication {
         final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
         try {
             //we set file URL
-            httpFile.setNewURL(new URL("http://www.facebook.com/video/video.php?v=10150223847135034"));//no login
+            // httpFile.setNewURL(new URL("http://www.facebook.com/video/video.php?v=10150223847135034"));//no login
+            httpFile.setNewURL(new URL("http://www.facebook.com/video/video.php?v=117907328260060&comments"));//no login
             //httpFile.setNewURL(new URL("http://www.facebook.com/video/video.php?v=1603402488187"));//requires login
             //the way we connect to the internet
             final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection

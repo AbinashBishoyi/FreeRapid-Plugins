d451ecdf024159fe3287e580328b1be25718a16a
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 06:54:57 +0530

    SVN Update# 1223 by Alex: tinyurl plugins

--- src/tinyurl/cz/vity/freerapid/plugins/services/tinyurl/TestApp.java
+++ src/tinyurl/cz/vity/freerapid/plugins/services/tinyurl/TestApp.java
@@ -1,4 +1,4 @@
-package cz.vity.freerapid.plugins.services.yourfilehost;
+package cz.vity.freerapid.plugins.services.tinyurl;
 
 import cz.vity.freerapid.plugins.dev.PluginDevApplication;
 import cz.vity.freerapid.plugins.webclient.ConnectionSettings;
@@ -8,8 +8,6 @@ import org.jdesktop.application.Application;
 import java.net.URL;
 
 /**
- * Test application for yourfilehost.com
- *
  * @author Alex
  */
 public class TestApp extends PluginDevApplication {
@@ -18,13 +16,12 @@ public class TestApp extends PluginDevApplication {
         final HttpFile httpFile = getHttpFile(); //creates new test instance of HttpFile
         try {
             //we set file URL
-            httpFile.setNewURL(new URL("http://www.yourfilehost.com/media.php?cat=video&file=bush_shoe_attack.flv"));
-
+            httpFile.setNewURL(new URL("http://tinyurl.com/82tfes"));
             //the way we connect to the internet
             final ConnectionSettings connectionSettings = new ConnectionSettings();// creates default connection
             connectionSettings.setProxy("localhost", 8081); //eg we can use local proxy to sniff HTTP communication
             //then we tries to download
-            final YourFileHostServiceImpl service = new YourFileHostServiceImpl(); //instance of service - of our plugin
+            final TinyUrlServiceImpl service = new TinyUrlServiceImpl(); //instance of service - of our plugin
             testRun(service, httpFile, connectionSettings);//download file with service and its Runner
             //all output goes to the console
         } catch (Exception e) {//catch possible exception

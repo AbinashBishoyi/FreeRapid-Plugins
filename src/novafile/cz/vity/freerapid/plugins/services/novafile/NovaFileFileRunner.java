275bb11248476c5369a7caf137f68e768e5bfad9
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 13:23:42 +0530

    SVN Update# 3787 by tong2shot: NovaFile.com - fixed

--- src/novafile/cz/vity/freerapid/plugins/services/novafile/NovaFileFileRunner.java
+++ src/novafile/cz/vity/freerapid/plugins/services/novafile/NovaFileFileRunner.java
@@ -1,9 +1,13 @@
 package cz.vity.freerapid.plugins.services.novafile;
 
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.YouHaveToWaitException;
 import cz.vity.freerapid.plugins.services.xfilesharing.XFileSharingRunner;
 import cz.vity.freerapid.plugins.services.xfilesharing.nameandsize.FileSizeHandler;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
 
 import java.util.List;
+import java.util.regex.Matcher;
 
 /**
  * Class which contains main code
@@ -17,5 +21,25 @@ class NovaFileFileRunner extends XFileSharingRunner {
         final List<FileSizeHandler> fileSizeHandlers = super.getFileSizeHandlers();
         fileSizeHandlers.add(0, new NovaFileFileSizeHandler());
         return fileSizeHandlers;
+    }
+
+    @Override
+    protected void checkDownloadProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("until the next download becomes available")) {
+            String regexRule = "(?:([0-9]+) hours?, )?(?:([0-9]+) minutes?, )?(?:([0-9]+) seconds?)";
+            Matcher matcher = PlugUtils.matcher(regexRule, contentAsString);
+            int waitHours = 0, waitMinutes = 0, waitSeconds = 0, waitTime;
+            if (matcher.find()) {
+                if (matcher.group(1) != null)
+                    waitHours = Integer.parseInt(matcher.group(1));
+                if (matcher.group(2) != null)
+                    waitMinutes = Integer.parseInt(matcher.group(2));
+                waitSeconds = Integer.parseInt(matcher.group(3));
+            }
+            waitTime = (waitHours * 60 * 60) + (waitMinutes * 60) + waitSeconds;
+            throw new YouHaveToWaitException("You have to wait " + waitTime + " seconds", waitTime);
+        }
+        super.checkDownloadProblems();
     }
 }

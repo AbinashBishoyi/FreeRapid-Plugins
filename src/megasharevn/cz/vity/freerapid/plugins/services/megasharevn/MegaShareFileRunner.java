1c22044529b03c12532f1ed79045d707801970d7
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-17 08:14:37 +0530

    SVN Update# 2025 by Vity: megashare.vn

---
+++ src/megasharevn/cz/vity/freerapid/plugins/services/megasharevn/MegaShareFileRunner.java
@@ -0,0 +1,71 @@
+package cz.vity.freerapid.plugins.services.megasharevn;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.util.logging.Logger;
+
+/**
+ * Class which contains main code
+ *
+ * @author Vity
+ */
+class MegaShareFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(MegaShareFileRunner.class.getName());
+
+
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
+        } else
+            throw new PluginImplementationException();
+    }
+
+    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
+        PlugUtils.checkName(httpFile, content, "file:</strong></td>\n<td width=\"79%\">", "</td>");
+        PlugUtils.checkFileSize(httpFile, content, "ng:</strong></td>\n<td>", "</td>");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
+        if (makeRedirectedRequest(method)) { //we make the main request
+            final String contentAsString = getContentAsString();//check for response
+            checkProblems();//check problems
+            checkNameAndSize(contentAsString);//extract file name and size from the page
+
+            client.getHTTPClient().getParams().setParameter("considerAsStream", "text/plain; charset=UTF-8");
+            final HttpMethod httpMethod = getMethodBuilder().setReferer(fileURL).setBaseURL("http://megashare.vn").setActionFromTextBetween("<a id=\\\"button-large\\\" a href=\\\"", "\\\">").toHttpMethod();
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkProblems();//if downloading failed
+                logger.warning(getContentAsString());//log the info
+                throw new PluginImplementationException();//some unknown problem
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("File not found")) {
+            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+    }
+
+}
---
+++ src/megasharevn/cz/vity/freerapid/plugins/services/megasharevn/MegaShareServiceImpl.java
@@ -0,0 +1,31 @@
+package cz.vity.freerapid.plugins.services.megasharevn;
+
+import cz.vity.freerapid.plugins.webclient.AbstractFileShareService;
+import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
+
+/**
+ * Class that provides basic info about plugin
+ *
+ * @author Vity
+ */
+public class MegaShareServiceImpl extends AbstractFileShareService {
+
+    public String getName() {
+        return "megashare.vn";
+    }
+
+    public int getMaxDownloadsFromOneIP() {
+        return 3;
+    }
+
+    @Override
+    public boolean supportsRunCheck() {
+        return true;//ok
+    }
+
+    @Override
+    protected PluginRunner getPluginRunnerInstance() {
+        return new MegaShareFileRunner();
+    }
+
+}

69307871274936c002a1d581cd737e982af49d35
Abinash Bishoyi <abinash.bishoyi@gmail.com>
2014-03-18 10:01:32 +0530

    SVN Update# 4901 by birchie: New Plugin:  DivxStream.net                       Fixed:  ZippyShare.com

---
+++ src/divxstream/cz/vity/freerapid/plugins/services/divxstream/DivxStreamFileRunner.java
@@ -0,0 +1,78 @@
+package cz.vity.freerapid.plugins.services.divxstream;
+
+import cz.vity.freerapid.plugins.exceptions.ErrorDuringDownloadingException;
+import cz.vity.freerapid.plugins.exceptions.PluginImplementationException;
+import cz.vity.freerapid.plugins.exceptions.ServiceConnectionProblemException;
+import cz.vity.freerapid.plugins.exceptions.URLNotAvailableAnymoreException;
+import cz.vity.freerapid.plugins.webclient.AbstractRunner;
+import cz.vity.freerapid.plugins.webclient.FileState;
+import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+
+/**
+ * Class which contains main code
+ *
+ * @author birchie
+ */
+class DivxStreamFileRunner extends AbstractRunner {
+    private final static Logger logger = Logger.getLogger(DivxStreamFileRunner.class.getName());
+
+    @Override
+    public void runCheck() throws Exception { //this method validates file
+        super.runCheck();
+        final GetMethod getMethod = getGetMethod(fileURL);//make first request
+        if (makeRedirectedRequest(getMethod)) {
+            checkProblems();
+            checkNameAndSize(getContentAsString());//ok let's extract file name and size from the page
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkNameAndSize(String content) throws ErrorDuringDownloadingException {
+        final Matcher match = PlugUtils.matcher("<div align=\"center\".+?>(.+?)</div>", content);
+        if (!match.find())
+            throw new PluginImplementationException("File name not found");
+        httpFile.setFileName(match.group(1).trim() + ".flv");
+        httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+        logger.info("Starting download in TASK " + fileURL);
+        final GetMethod method = getGetMethod(fileURL); //create GET request
+        if (makeRedirectedRequest(method)) { //we make the main request
+            final String contentAsString = getContentAsString();//check for response
+            checkProblems();//check problems
+            checkNameAndSize(contentAsString);//extract file name and size from the page
+            final Matcher match1 = PlugUtils.matcher("type\\|(\\d+?)\\|(\\d+?)\\|(\\d+?)\\|(\\d+?)\\|\\|(\\d+?)\\|player", contentAsString);
+            final Matcher match2 = PlugUtils.matcher("provider\\|([^\\|]+?)\\|([^\\|]+?)\\|file", contentAsString);
+            if (!match1.find()) throw new PluginImplementationException("Download url matcher1 failed");
+            if (!match2.find()) throw new PluginImplementationException("Download url matcher2 failed");
+            final String dlUrl = "http://" + match1.group(5) + "." + match1.group(4) + "." + match1.group(3) + "." +
+                    match1.group(2) + ":" + match1.group(1) + "/" + match2.group(2) + "/v." + match2.group(1);
+            final HttpMethod httpMethod = getGetMethod(dlUrl);
+            if (!tryDownloadAndSaveFile(httpMethod)) {
+                checkProblems();//if downloading failed
+                throw new ServiceConnectionProblemException("Error starting download");//some unknown problem
+            }
+        } else {
+            checkProblems();
+            throw new ServiceConnectionProblemException();
+        }
+    }
+
+    private void checkProblems() throws ErrorDuringDownloadingException {
+        final String contentAsString = getContentAsString();
+        if (contentAsString.contains("404 Not Found")) {
+            throw new URLNotAvailableAnymoreException("File not found"); //let to know user in FRD
+        }
+    }
+
+}
--- src/zippyshare/cz/vity/freerapid/plugins/services/zippyshare/ZippyShareFileRunner.java
+++ src/zippyshare/cz/vity/freerapid/plugins/services/zippyshare/ZippyShareFileRunner.java
@@ -50,8 +50,8 @@ class ZippyShareFileRunner extends AbstractRunner {
             final String url;
             Matcher matcher = getMatcherAgainstContent("<script[^<>]*?>([^<>]*?document\\.getElementById\\('dlbutton'\\)\\.href\\s*=\\s*[^<>]+?)</script>");
             if (matcher.find()) {
-                final String function = PlugUtils.getStringBetween(matcher.group(1), "function", "{");
-                final String script = matcher.group(1) + ";\n" + function + ";\ndocument.getElementById('dlbutton').href";
+                final String function = PlugUtils.getStringBetween(matcher.group(1), "var", "= function").trim();
+                final String script = matcher.group(1) + ";\n" + function + "();\ndocument.getElementById('dlbutton').href";
                 logger.info("Evaluating script:\n" + script);
                 final Context context = Context.enter();
                 try {

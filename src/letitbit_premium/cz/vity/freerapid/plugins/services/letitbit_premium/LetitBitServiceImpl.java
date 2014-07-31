83ecfd7fc476fd2621dedc1ad651d70f83431091
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-08-01 02:50:05 +0530

    SVN Update# 5055 by ntoskrnl: remove silly workaround for FRD v0.85

--- src/letitbit_premium/cz/vity/freerapid/plugins/services/letitbit_premium/LetitBitServiceImpl.java
+++ src/letitbit_premium/cz/vity/freerapid/plugins/services/letitbit_premium/LetitBitServiceImpl.java
@@ -11,39 +11,6 @@ import cz.vity.freerapid.plugins.webclient.interfaces.PluginRunner;
  */
 public class LetitBitServiceImpl extends AbstractFileShareService {
 
-    static {
-        if (!"yes".equals(System.getProperty("cz.vity.freerapid.updatechecked", null))) {
-            try {
-                System.setProperty("cz.vity.freerapid.updatechecked", "yes");
-                @SuppressWarnings("unchecked")
-                final Class<org.jdesktop.application.Application> mainApp =
-                        (Class<org.jdesktop.application.Application>) Class.forName("cz.vity.freerapid.core.MainApp");
-                try {
-                    //make sure that version < 0.85u1
-                    Class.forName("cz.vity.freerapid.plugins.webclient.utils.ScriptUtils");
-                } catch (ClassNotFoundException e) {
-                    {
-                        final java.io.File file = new java.io.File(cz.vity.freerapid.utilities.Utils.getAppPath(), "startup.properties");
-                        String s = cz.vity.freerapid.utilities.Utils.loadFile(file, "UTF-8");
-                        s = s.replaceAll("(?m)^\\-\\-debug$", "#--debug");
-                        if (file.delete()) {
-                            final java.io.OutputStream os = new java.io.BufferedOutputStream(new java.io.FileOutputStream(file));
-                            os.write(s.getBytes("UTF-8"));
-                            os.close();
-                        }
-                    }
-                    {
-                        final java.lang.reflect.Method startCheckNewVersion = mainApp.getDeclaredMethod("startCheckNewVersion");
-                        startCheckNewVersion.setAccessible(true);
-                        startCheckNewVersion.invoke(org.jdesktop.application.Application.getInstance(mainApp));
-                    }
-                }
-            } catch (Throwable t) {
-                //ignore
-            }
-        }
-    }
-
     private static final String PLUGIN_CONFIG_FILE = "plugin_LetitBitPremium.xml";
     private volatile PremiumAccount config;
 

9a9a01c63d846659713bf0f3912f9f14dc90f71c
AbinashBishoyi <abinash.bishoyi@gmail.com>
2014-10-20 23:52:30 +0530

    SVN Update# 5129 by ntoskrnl: ZippyShare.com - fixed

--- src/zippyshare/cz/vity/freerapid/plugins/services/zippyshare/ZippyShareFileRunner.java
+++ src/zippyshare/cz/vity/freerapid/plugins/services/zippyshare/ZippyShareFileRunner.java
@@ -6,15 +6,12 @@ import cz.vity.freerapid.plugins.webclient.AbstractRunner;
 import cz.vity.freerapid.plugins.webclient.DownloadClientConsts;
 import cz.vity.freerapid.plugins.webclient.FileState;
 import cz.vity.freerapid.plugins.webclient.utils.PlugUtils;
+import cz.vity.freerapid.plugins.webclient.utils.ScriptUtils;
 import cz.vity.freerapid.utilities.LogUtils;
 import org.apache.commons.httpclient.HttpMethod;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.URLDecoder;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -49,30 +46,11 @@ class ZippyShareFileRunner extends AbstractRunner {
             checkProblems();
             checkNameAndSize();
             final String url;
-            Matcher matcher = getMatcherAgainstContent("(?s)<body>(.+?)</body>");
-            if (!matcher.find()) {
-                throw new PluginImplementationException("Script not found (1)");
-            }
-            matcher = PlugUtils.matcher("<script[^<>]*?>([^<>]+?)</script>", matcher.group(1));
+            Matcher matcher = getMatcherAgainstContent("document\\.getElementById\\([^\\(\\)]*?\\)\\.href[^<>]+=([^<>]+?/[^<>]+?\";)");
             if (matcher.find()) {
-                final Matcher buttonId = getMatcherAgainstContent("<a\\b[^<>]*?\\bid=\"([\\w\\-]+?)\"[^<>]*?>.*?alt=\"Download\"");
-                if (!buttonId.find()) {
-                    throw new PluginImplementationException("Download button ID not found");
-                }
-                final ScriptEngine engine = initScriptEngine();
-                try {
-                    do {
-                        final String script = matcher.group(1);
-                        if (script.contains("tumblr") || script.contains("swfobject")) {
-                            continue;
-                        }
-                        logger.info("Evaluating script:\n" + script);
-                        engine.eval(script);
-                    } while (matcher.find());
-                    url = engine.eval("document.getElementById('" + buttonId.group(1) + "').href").toString();
-                } catch (final Exception e) {
-                    throw new PluginImplementationException("Script execution failed", e);
-                }
+                final String script = matcher.group(1);
+                logger.info("Evaluating script:\n" + script);
+                url = "/d/" + String.valueOf(ScriptUtils.evaluateJavaScript(script));
             } else if (getContentAsString().contains("Recaptcha.create(")) {
                 url = PlugUtils.getStringBetween(getContentAsString(), "document.location = '", "';");
                 final String rcKey = PlugUtils.getStringBetween(getContentAsString(), "Recaptcha.create(\"", "\"");
@@ -98,6 +76,7 @@ class ZippyShareFileRunner extends AbstractRunner {
             if (url == null) {
                 throw new PluginImplementationException("Download URL not found");
             }
+            logger.info("Download URL: " + url);
             httpMethod = getMethodBuilder().setReferer(fileURL).setAction(url).toGetMethod();
             setClientParameter(DownloadClientConsts.DONT_USE_HEADER_FILENAME, true);
             if (!tryDownloadAndSaveFile(httpMethod)) {
@@ -137,20 +116,6 @@ class ZippyShareFileRunner extends AbstractRunner {
             logger.info("File size not found");
         }
         httpFile.setFileState(FileState.CHECKED_AND_EXISTING);
-    }
-
-    private ScriptEngine initScriptEngine() throws Exception {
-        final ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
-        if (engine == null) {
-            throw new RuntimeException("JavaScript engine not found");
-        }
-        final Reader reader = new InputStreamReader(ZippyShareFileRunner.class.getResourceAsStream("zippy.js"), "UTF-8");
-        try {
-            engine.eval(reader);
-        } finally {
-            reader.close();
-        }
-        return engine;
     }
 
     private HttpMethod stepCaptcha(final String rcKey, final String shortencode) throws Exception {
